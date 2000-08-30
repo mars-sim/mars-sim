d1ce11f075512b555fd33cdfeca75f98d0e8d1a7
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2000-08-30 12:47:12 +0000

    Fully implemented UI for vehicle maintenance.

--- MaintenanceGarageFacilityPanel.java
+++ MaintenanceGarageFacilityPanel.java
@@ -1,17 +1,28 @@
 //************************** Maintenance Garage Facility Panel **************************
-// Last Modified: 5/22/00
+// Last Modified: 8/29/00
 
 // The MaintenanceGarageFacilityPanel class displays information about a settlement's maintenance garage facility in the user interface.
 
 import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 import javax.swing.*;
 import javax.swing.border.*;
 
-public class MaintenanceGarageFacilityPanel extends FacilityPanel {
+public class MaintenanceGarageFacilityPanel extends FacilityPanel implements ActionListener {
 
 	// Data members
 	
 	private MaintenanceGarageFacility maintenanceGarage;  // The maintenance garage facility this panel displays.
+	private Vector parkedVehicles;       // A vector of vehicles currently in the garage.
+	private Vector vehicleButtons;       // A vector of vehicle buttons.
+	private Vector vehicleProgressBars;  // A vector of vehicle progress bars.
+	private JPanel vehicleGridPane;      // A UI panel that contains the vehicle buttons and progress bars.
+	private JLabel sizeTotalLabel;       // A label showing the current total of the vehicles sizes in the garage.
+	
+	// Cached garage data
+	
+	private int currentTotalSize;        // The current total size of vehicles in the garage.
 	
 	// Constructor
 	
@@ -33,7 +44,7 @@ public class MaintenanceGarageFacilityPanel extends FacilityPanel {
 		// Prepare content pane
 		
 		JPanel contentPane = new JPanel(new BorderLayout(0, 5));
-		add(contentPane, "North");
+		add(contentPane, "Center");
 		
 		// Prepare name label
 		
@@ -54,19 +65,20 @@ public class MaintenanceGarageFacilityPanel extends FacilityPanel {
 		
 		// Prepare max size label
 		
-		JLabel maxSizeLabel = new JLabel("Maximum Vehicle Size Allowable: " + maintenanceGarage.getMaxVehicleSize(), JLabel.CENTER);
+		JLabel maxSizeLabel = new JLabel("Maximum Vehicle Size Allowed: " + maintenanceGarage.getMaxVehicleSize(), JLabel.CENTER);
 		maxSizeLabel.setForeground(Color.black);
 		labelPane.add(maxSizeLabel);
 		
 		// Prepare size capacity label
 		
-		JLabel sizeCapacityLabel = new JLabel("Vehicle Size Total Capacity: " + maintenanceGarage.getMaxSizeCapacity(), JLabel.CENTER);
+		JLabel sizeCapacityLabel = new JLabel("Total Vehicle Size Capacity: " + maintenanceGarage.getMaxSizeCapacity(), JLabel.CENTER);
 		sizeCapacityLabel.setForeground(Color.black);
 		labelPane.add(sizeCapacityLabel);
 		
 		// Prepare size total label
 		
-		JLabel sizeTotalLabel = new JLabel("Vehicle Size Total Currently: " + maintenanceGarage.getTotalSize(), JLabel.CENTER);
+		currentTotalSize = maintenanceGarage.getTotalSize();
+		sizeTotalLabel = new JLabel("Current Total Vehicle Size: " + currentTotalSize, JLabel.CENTER);
 		sizeTotalLabel.setForeground(Color.black);
 		labelPane.add(sizeTotalLabel);
 		
@@ -78,36 +90,161 @@ public class MaintenanceGarageFacilityPanel extends FacilityPanel {
 		
 		// Prepare parked name label
 		
-		JLabel parkedVehiclesLabel = new JLabel("Vehicles Parked In Garage:", JLabel.CENTER);
+		JLabel parkedVehiclesLabel = new JLabel("Vehicles Undergoing Maintenance:", JLabel.CENTER);
 		parkedVehiclesLabel.setForeground(Color.black);
 		parkedVehiclesPane.add(parkedVehiclesLabel, "North");
 		
 		// Prepare vehicle list pane
 		
-		JPanel vehicleListPane = new JPanel();
-		parkedVehiclesPane.add(vehicleListPane, "Center");
+		JPanel vehicleListPane = new JPanel(new BorderLayout());
+		parkedVehiclesPane.add(new JScrollPane(vehicleListPane), "Center");
+	
+		// Create parked vehicles vector and vehicle buttons vector.
 		
-		// Prepare inner vehicle list pane
+		parkedVehicles = new Vector();
+		Vehicle[] parkedVehicleArray = maintenanceGarage.getVehicles();
+		for (int x=0; x < parkedVehicleArray.length; x++) parkedVehicles.addElement(parkedVehicleArray[x]);
 		
-		JPanel innerVehicleListPane = new JPanel(new BorderLayout());
-		innerVehicleListPane.setPreferredSize(new Dimension(150, 100));
-		vehicleListPane.add(innerVehicleListPane);
+		// Prepare vehicle grid pane.
 		
-		// Prepare vehicle list
+		vehicleGridPane = new JPanel(new GridLayout(parkedVehicles.size(), 1, 0, 5));
+		vehicleListPane.add(vehicleGridPane, "North");
+	
+		// Add vehicles.
 		
-		String[] vehicleNames = maintenanceGarage.getVehicleNames();
-		DefaultListModel vehicleListModel = new DefaultListModel();
-		for (int x=0; x < vehicleNames.length; x++) vehicleListModel.addElement(vehicleNames[x]);
-		JList vehicleList = new JList(vehicleListModel);
-		vehicleList.setVisibleRowCount(6);
-		// vehicleList.addMouseListener(this);
-		innerVehicleListPane.add(new JScrollPane(vehicleList), "Center");
+		vehicleButtons = new Vector();
+		vehicleProgressBars = new Vector();
+		for (int x=0; x < parkedVehicles.size(); x++) {
+			
+			Vehicle vehicle = (Vehicle) parkedVehicles.elementAt(x);
+			
+			JPanel vehiclePane = new JPanel(new BorderLayout());
+			vehicleGridPane.add(vehiclePane);
+			
+			JButton vehicleButton = new JButton(vehicle.getName());
+			vehicleButton.addActionListener(this);
+			vehiclePane.add(vehicleButton, "West");
+			vehicleButtons.addElement(vehicleButton);
+			
+			JProgressBar vehicleProgressBar = new JProgressBar();
+			vehicleProgressBar.setStringPainted(true);
+			vehicleProgressBar.setPreferredSize(new Dimension(100, 0));
+			
+			int maintenanceProgress = 0;
+			float currentWork = (float) vehicle.getCurrentMaintenanceWork();
+			float totalWork = (float) vehicle.getTotalMaintenanceWork();
+			maintenanceProgress = (int) (100F * (currentWork / totalWork));
+			
+			vehicleProgressBar.setValue(maintenanceProgress);
+			vehiclePane.add(vehicleProgressBar, "East");
+			vehicleProgressBars.addElement(vehicleProgressBar);
+		}
+	}
+	
+	// ActionListener method overriden
+	
+	public void actionPerformed(ActionEvent event) {
+		
+		Object button = event.getSource();
+			
+		// Check to see if button is a vehicle button.
+		
+		for (int x=0; x < parkedVehicles.size(); x++) {
+			if (button == vehicleButtons.elementAt(x)) {
+				Vehicle vehicle = (Vehicle) parkedVehicles.elementAt(x);
+				desktop.openUnitWindow(vehicle.getID());
+			}
+		}
 	}
 	
 	// Updates the facility panel's information
 	
 	public void updateInfo() { 
-		// Implement later	
+		
+		// Change vehicle size total currently if necessary.
+		
+		if (currentTotalSize != maintenanceGarage.getTotalSize()) {
+			currentTotalSize = maintenanceGarage.getTotalSize();
+			sizeTotalLabel.setText("Current Total Vehicle Size: " + currentTotalSize);
+		}
+		
+		// Get vehicles in garage array.
+		
+		Vehicle[] parkedVehicleArray = maintenanceGarage.getVehicles();
+		
+		// Check if array matches parkedVehicles vector.
+		
+		boolean match = false;
+		if (parkedVehicleArray.length == parkedVehicles.size()) {
+			match = true;
+			for (int x=0; x < parkedVehicleArray.length; x++) 
+				if ((Vehicle) parkedVehicles.elementAt(x) != parkedVehicleArray[x]) match = false;
+		}
+		
+		// If a match, update vehicle progress bars.
+		
+		if (match) {
+			for (int x=0; x < parkedVehicles.size(); x++) {
+				
+				Vehicle vehicle = (Vehicle) parkedVehicles.elementAt(x);
+				
+				int maintenanceProgress = 0;
+				float currentWork = (float) vehicle.getCurrentMaintenanceWork();
+				float totalWork = (float) vehicle.getTotalMaintenanceWork();
+				maintenanceProgress = (int) (100F * (currentWork / totalWork));
+				
+				((JProgressBar) vehicleProgressBars.elementAt(x)).setValue(maintenanceProgress);
+			}
+		}
+		else {
+			// If not a match, update parkedVehicles vector and UI.
+			// Clear vectors.
+			
+			parkedVehicles.removeAllElements();
+			vehicleButtons.removeAllElements();
+			vehicleProgressBars.removeAllElements();
+			
+			// Populate parkedVehicles vector.
+			
+			for (int x=0; x < parkedVehicleArray.length; x++) parkedVehicles.addElement(parkedVehicleArray[x]);
+			
+			// Set up vehicle grid pane layout.
+			
+			vehicleGridPane.removeAll();
+			vehicleGridPane.setLayout(new GridLayout(parkedVehicles.size(), 1, 0, 5));
+			
+			// Add vehicle UI's
+			
+			for (int x=0; x < parkedVehicles.size(); x++) {
+				
+				Vehicle vehicle = (Vehicle) parkedVehicles.elementAt(x);
+				
+				JPanel vehiclePane = new JPanel(new BorderLayout());
+				vehicleGridPane.add(vehiclePane);
+			
+				JButton vehicleButton = new JButton(vehicle.getName());
+				vehicleButton.addActionListener(this);
+				vehiclePane.add(vehicleButton, "West");
+				vehicleButtons.addElement(vehicleButton);
+				
+				JProgressBar vehicleProgressBar = new JProgressBar();
+				vehicleProgressBar.setStringPainted(true);
+				vehicleProgressBar.setPreferredSize(new Dimension(100, 0));
+				
+				int maintenanceProgress = 0;
+				float currentWork = (float) vehicle.getCurrentMaintenanceWork();
+				float totalWork = (float) vehicle.getTotalMaintenanceWork();
+				maintenanceProgress = (int) (100F * (currentWork / totalWork));
+				
+				vehicleProgressBar.setValue(maintenanceProgress);
+				vehiclePane.add(vehicleProgressBar, "East");
+				vehicleProgressBars.addElement(vehicleProgressBar);
+			}
+			
+			// Validate panel
+			
+			getParent().validate();
+		}
 	}
 }	
 
