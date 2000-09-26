f34f0703875214c844839ffe105a2f32460cd41d
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2000-09-26 05:02:32 +0000

    Beatification and small changes.

--- MarsProject.java
+++ MarsProject.java
@@ -1,12 +1,12 @@
 /**
  * Mars Simulation Project
  * MarsProject.java
- * @version 2.71 2000-09-21
+ * @version 2.71 2000-09-25
  * @author Scott Davis
  */
- 
+
 import org.mars_sim.msp.simulation.VirtualMars;
-import org.mars_sim.msp.ui.standard.*; 
+import org.mars_sim.msp.ui.standard.*;
 import java.io.*;
 import javax.swing.plaf.metal.*;
 import javax.swing.*;
@@ -19,36 +19,35 @@ public class MarsProject {
 
     public MarsProject() {
 
-	// prepare custom UI color theme
-	MetalLookAndFeel.setCurrentTheme(new MarsTheme());
-		
-	// create a splash window
-	SplashWindow splashWindow = new SplashWindow();
-		
-	// create virtual mars
-	VirtualMars mars = new VirtualMars();
-	
-	// create main desktop window
-	MainWindow window = new MainWindow(mars);
+        // prepare custom UI color theme
+        MetalLookAndFeel.setCurrentTheme(new MarsTheme());
 
-	// dispose splash window
-	splashWindow.dispose();
-	splashWindow = null;
+        // create a splash window
+        SplashWindow splashWindow = new SplashWindow();
+
+        // create virtual mars
+        VirtualMars mars = new VirtualMars();
+
+        // create main desktop window
+        MainWindow window = new MainWindow(mars);
+
+        // dispose splash window
+        splashWindow.dispose();
+        splashWindow = null;
     }
 
     /** Set error output to a text file (for debugging). */
-    public void setDebugMode() {
-	try {
-	    FileOutputStream errFileStream = new FileOutputStream("err.txt");
-	    System.setErr(new PrintStream(errFileStream));
-	}
-	catch(FileNotFoundException e) {
-	    System.out.println("err.txt could not be opened");
-	}
+    private void setDebugMode() {
+        try {
+            FileOutputStream errFileStream = new FileOutputStream("err.txt");
+            System.setErr(new PrintStream(errFileStream));
+        } catch (FileNotFoundException e) {
+            System.out.println("err.txt could not be opened");
+        }
     }
 
     /** The starting method for the application */
     public static void main(String args[]) {
-	MarsProject marsSim = new MarsProject();
+        MarsProject marsSim = new MarsProject();
     }
 }
