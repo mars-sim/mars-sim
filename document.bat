fce81274d487ac32fc341dbb0c748f52915cde73
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2001-01-30 00:47:18 +0000

    Updated the javadoc documentation script.

--- document.bat
+++ document.bat
@@ -1,2 +1,1 @@
-javadoc -d c:\java\msp_stuff\javadoc -overview overview.html -use -author -windowtitle "Mars Simulation Project v 2.71 API Specification" -doctitle  "Mars Simulation Project v 2.71<BR>API Specification" -header "<b>Mars Simulation Project</b><br><font size="-1">v2.71</font>" MarsProject.java Installer.java org.mars_sim.msp.simulation org.mars_sim.msp.ui.standard
-
+javadoc -d docs/javadoc -overview overview.html -use -author -windowtitle "Mars Simulation Project v 2.71 API Specification" -doctitle "Mars Simulation Project v 2.71<BR>API Specification" -header "<b>Mars Simulation Project</b><br><font size=\"-1\">v2.71</font>" org.mars_sim.msp.simulation org.mars_sim.msp.ui.standard MarsProject 
