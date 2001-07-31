266769e648ee65e11061780cc230cd401f035b87
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2001-07-31 12:47:27 +0000

    Adding EatMeal and Sleep tasks

---
+++ org/mars_sim/msp/simulation/task/EatMeal.java
@@ -0,0 +1,64 @@
+/**
+ * Mars Simulation Project
+ * EatMeal.java
+ * @version 2.72 2001-07-30
+ * @author Scott Davis
+ */
+
+package org.mars_sim.msp.simulation.task;
+
+import org.mars_sim.msp.simulation.*;
+
+/** The EatMail class is a task for eating a meal.
+ *  The duration of the task is 20 millisols.
+ *
+ *  Note: Eating a meal reduces hunger
+ */
+class EatMeal extends Task {
+
+    // Data members
+    private double duration = 20D; // The predetermined duration of task in millisols
+
+    /** Constructs a EatMeal object
+     *  @param person the person to perform the task
+     *  @param mars the virtual Mars
+     */
+    public EatMeal(Person person, VirtualMars mars) {
+        super("Eating a meal", person, mars);
+        
+        // System.out.println(person.getName() + " is eating with " + person.getHunger() + " hunger.");
+    }
+
+    /** Returns the weighted probability that a person might perform this task.
+     *
+     *  @param person the person to perform the task
+     *  @param mars the virtual Mars
+     *  @return the weighted probability that a person might perform this task
+     */
+    public static double getProbability(Person person, VirtualMars mars) {
+
+        double result = person.getHunger() - 250D;
+        if (result < 0) result = 0;
+        
+        return result;
+    }
+
+    /** This task allows the person to eat for the duration. 
+     *  @param time the amount of time to perform this task (in millisols)
+     *  @return amount of time remaining after finishing with task (in millisols)
+     */
+    double doTask(double time) {
+        double timeLeft = super.doTask(time);
+        if (subTask != null) return timeLeft;
+
+        person.setHunger(0D);
+        timeCompleted += time;
+        if (timeCompleted > duration) {
+            person.consumeFood(1D / 3D);
+            isDone = true;
+            return timeCompleted - duration;
+        }
+        else return 0;
+    }
+}
+
---
+++ org/mars_sim/msp/simulation/task/Sleep.java
@@ -0,0 +1,69 @@
+/**
+ * Mars Simulation Project
+ * Sleep.java
+ * @version 2.72 2001-07-28
+ * @author Scott Davis
+ */
+
+package org.mars_sim.msp.simulation.task;
+
+import org.mars_sim.msp.simulation.*;
+
+/** The Sleep class is a task for sleeping.
+ *  The duration of the task is by default chosen randomly, between 250 - 350 millisols.
+ *
+ *  Note: Sleeping reduces fatigue.
+ */
+class Sleep extends Task {
+
+    // Data members
+    private double duration; // The predetermined duration of task in millisols
+
+    /** Constructs a Sleep object
+     *  @param person the person to perform the task
+     *  @param mars the virtual Mars
+     */
+    public Sleep(Person person, VirtualMars mars) {
+        super("Sleeping", person, mars);
+
+        duration = 250D + RandomUtil.getRandomInt(100);
+        // System.out.println(person.getName() + " sleeping with " + person.getFatigue() + " fatigue and visibility: " + mars.getSurfaceFeatures().getSurfaceSunlight(person.getCoordinates()));
+    }
+
+    /** Returns the weighted probability that a person might perform this task.
+     *  Returns 10 if person's fatigue is over 750.
+     *  Returns an additional 50 if it is night time. 
+     *  @param person the person to perform the task
+     *  @param mars the virtual Mars
+     *  @return the weighted probability that a person might perform this task
+     */
+    public static double getProbability(Person person, VirtualMars mars) {
+        double result = 0D;
+
+        if (person.getFatigue() > 750D) {
+            result = 25D;
+            if (mars.getSurfaceFeatures().getSurfaceSunlight(person.getCoordinates()) == 0)
+                result += 50D;
+        }
+
+        return result;
+    }
+
+    /** This task allows the person to sleep for the duration. 
+     *  @param time the amount of time to perform this task (in millisols)
+     *  @return amount of time remaining after finishing with task (in millisols)
+     */
+    double doTask(double time) {
+        double timeLeft = super.doTask(time);
+        if (subTask != null) return timeLeft;
+
+        person.setFatigue(0D);
+        timeCompleted += time;
+        if (timeCompleted > duration) {
+            isDone = true;
+            return timeCompleted - duration;
+        }
+        else return 0;
+    }
+}
+
