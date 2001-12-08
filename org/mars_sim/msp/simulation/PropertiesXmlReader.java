18973edabfc08db551fbee8163d7a559a7cf7e60
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2001-12-08 07:22:32 +0000

    Modified hieretical structure of properties.xml file

--- org/mars_sim/msp/simulation/PropertiesXmlReader.java
+++ org/mars_sim/msp/simulation/PropertiesXmlReader.java
@@ -1,7 +1,7 @@
 /**
  * Mars Simulation Project
  * PropertiesXmlReader.java
- * @version 2.73 2001-12-06
+ * @version 2.73 2001-12-07
  * @author Scott Davis
  */
 
@@ -19,21 +19,21 @@ class PropertiesXmlReader extends MspXmlReader {
     // XML element types
     private static int PROPERTY_LIST = 0;
     private static int TIME_RATIO = 1;
-    private static int PERSON_OXYGEN_CONSUMPTION = 2;
-    private static int PERSON_WATER_CONSUMPTION = 3;
-    private static int PERSON_FOOD_CONSUMPTION = 4;
-    private static int ROVER_OXYGEN_STORAGE_CAPACITY = 5;
-    private static int ROVER_WATER_STORAGE_CAPACITY = 6;
-    private static int ROVER_FOOD_STORAGE_CAPACITY = 7;
-    private static int ROVER_FUEL_STORAGE_CAPACITY = 8;
-    private static int ROVER_FUEL_EFFICIENCY = 9;
-    private static int SETTLEMENT_OXYGEN_STORAGE_CAPACITY = 10;
-    private static int SETTLEMENT_WATER_STORAGE_CAPACITY = 11;
-    private static int SETTLEMENT_FOOD_STORAGE_CAPACITY = 12;
-    private static int SETTLEMENT_FUEL_STORAGE_CAPACITY = 13;
+    private static int PERSON_PROPERTIES = 2;
+    private static int OXYGEN_CONSUMPTION = 3;
+    private static int WATER_CONSUMPTION = 4;
+    private static int FOOD_CONSUMPTION = 5;
+    private static int ROVER_PROPERTIES = 6;
+    private static int OXYGEN_STORAGE_CAPACITY = 7;
+    private static int WATER_STORAGE_CAPACITY = 8;
+    private static int FOOD_STORAGE_CAPACITY = 9;
+    private static int FUEL_STORAGE_CAPACITY = 10;
+    private static int FUEL_EFFICIENCY = 11;
+    private static int SETTLEMENT_PROPERTIES = 12;
 
     // Data members
     private int elementType; // The current element type being parsed
+    private int propertyCatagory; // The property catagory
     private double timeRatio; // The time ratio property
     private double personOxygenConsumption; // The person oxygen consumption property
     private double personWaterConsumption; // The person water consumption property
@@ -67,41 +67,41 @@ class PropertiesXmlReader extends MspXmlReader {
         if (name.equals("TIME_RATIO")) {
             elementType = TIME_RATIO;
         }
-        if (name.equals("PERSON_OXYGEN_CONSUMPTION")) {
-            elementType = PERSON_OXYGEN_CONSUMPTION;
+        if (name.equals("PERSON_PROPERTIES")) {
+            elementType = PERSON_PROPERTIES;
+            propertyCatagory = PERSON_PROPERTIES;
         }
-        if (name.equals("PERSON_WATER_CONSUMPTION")) {
-            elementType = PERSON_WATER_CONSUMPTION;
+        if (name.equals("OXYGEN_CONSUMPTION")) {
+            elementType = OXYGEN_CONSUMPTION;
         }
-        if (name.equals("PERSON_FOOD_CONSUMPTION")) {
-            elementType = PERSON_FOOD_CONSUMPTION;
+        if (name.equals("WATER_CONSUMPTION")) {
+            elementType = WATER_CONSUMPTION;
         }
-        if (name.equals("ROVER_OXYGEN_STORAGE_CAPACITY")) {
-            elementType = ROVER_OXYGEN_STORAGE_CAPACITY;
+        if (name.equals("FOOD_CONSUMPTION")) {
+            elementType = FOOD_CONSUMPTION;
         }
-        if (name.equals("ROVER_WATER_STORAGE_CAPACITY")) {
-            elementType = ROVER_WATER_STORAGE_CAPACITY;
+        if (name.equals("ROVER_PROPERTIES")) {
+            elementType = ROVER_PROPERTIES;
+            propertyCatagory = ROVER_PROPERTIES;
         }
-        if (name.equals("ROVER_FOOD_STORAGE_CAPACITY")) {
-            elementType = ROVER_FOOD_STORAGE_CAPACITY;
+        if (name.equals("OXYGEN_STORAGE_CAPACITY")) {
+            elementType = OXYGEN_STORAGE_CAPACITY;
         }
-        if (name.equals("ROVER_FUEL_STORAGE_CAPACITY")) {
-            elementType = ROVER_FUEL_STORAGE_CAPACITY;
+        if (name.equals("WATER_STORAGE_CAPACITY")) {
+            elementType = WATER_STORAGE_CAPACITY;
         }
-        if (name.equals("ROVER_FUEL_EFFICIENCY")) {
-            elementType = ROVER_FUEL_EFFICIENCY;
+        if (name.equals("FOOD_STORAGE_CAPACITY")) {
+            elementType = FOOD_STORAGE_CAPACITY;
         }
-        if (name.equals("SETTLEMENT_OXYGEN_STORAGE_CAPACITY")) {
-            elementType = SETTLEMENT_OXYGEN_STORAGE_CAPACITY;
+        if (name.equals("FUEL_STORAGE_CAPACITY")) {
+            elementType = FUEL_STORAGE_CAPACITY;
         }
-        if (name.equals("SETTLEMENT_WATER_STORAGE_CAPACITY")) {
-            elementType = SETTLEMENT_WATER_STORAGE_CAPACITY;
+        if (name.equals("FUEL_EFFICIENCY")) {
+            elementType = FUEL_EFFICIENCY;
         }
-        if (name.equals("SETTLEMENT_FOOD_STORAGE_CAPACITY")) {
-            elementType = SETTLEMENT_FOOD_STORAGE_CAPACITY;
-        }
-        if (name.equals("SETTLEMENT_FUEL_STORAGE_CAPACITY")) {
-            elementType = SETTLEMENT_FUEL_STORAGE_CAPACITY;
+        if (name.equals("SETTLEMENT_PROPERTIES")) {
+            elementType = SETTLEMENT_PROPERTIES;
+            propertyCatagory = SETTLEMENT_PROPERTIES;
         }
     }
 
@@ -117,52 +117,51 @@ class PropertiesXmlReader extends MspXmlReader {
             elementType = PROPERTY_LIST;
             return;
         }
-        if (elementType == PERSON_OXYGEN_CONSUMPTION) {
+        if (elementType == PERSON_PROPERTIES) {
             elementType = PROPERTY_LIST;
+            propertyCatagory = -1;
+            return;
+        }        
+        if (elementType == OXYGEN_CONSUMPTION) {
+            elementType = PERSON_PROPERTIES;
             return;
         }
-        if (elementType == PERSON_WATER_CONSUMPTION) {
-            elementType = PROPERTY_LIST;
+        if (elementType == WATER_CONSUMPTION) {
+            elementType = PERSON_PROPERTIES;
             return;
         }
-        if (elementType == PERSON_FOOD_CONSUMPTION) {
-            elementType = PROPERTY_LIST;
+        if (elementType == FOOD_CONSUMPTION) {
+            elementType = PERSON_PROPERTIES;
             return;
         }
-        if (elementType == ROVER_OXYGEN_STORAGE_CAPACITY) {
+        if (elementType == ROVER_PROPERTIES) {
             elementType = PROPERTY_LIST;
+            propertyCatagory = -1;
             return;
         }
-        if (elementType == ROVER_WATER_STORAGE_CAPACITY) {
-            elementType = PROPERTY_LIST;
+        if (elementType == OXYGEN_STORAGE_CAPACITY) {
+            elementType = propertyCatagory;
             return;
         }
-        if (elementType == ROVER_FOOD_STORAGE_CAPACITY) {
-            elementType = PROPERTY_LIST;
+        if (elementType == WATER_STORAGE_CAPACITY) {
+            elementType = propertyCatagory;
             return;
         }
-        if (elementType == ROVER_FUEL_STORAGE_CAPACITY) {
-            elementType = PROPERTY_LIST;
+        if (elementType == FOOD_STORAGE_CAPACITY) {
+            elementType = propertyCatagory;
             return;
         }
-        if (elementType == ROVER_FUEL_EFFICIENCY) {
-            elementType = PROPERTY_LIST;
+        if (elementType == FUEL_STORAGE_CAPACITY) {
+            elementType = propertyCatagory;
             return;
         }
-        if (elementType == SETTLEMENT_OXYGEN_STORAGE_CAPACITY) {
-            elementType = PROPERTY_LIST;
+        if (elementType == FUEL_EFFICIENCY) {
+            elementType = ROVER_PROPERTIES;
             return;
         }
-        if (elementType == SETTLEMENT_WATER_STORAGE_CAPACITY) {
+        if (elementType == SETTLEMENT_PROPERTIES) {
             elementType = PROPERTY_LIST;
-            return;
-        }
-        if (elementType == SETTLEMENT_FOOD_STORAGE_CAPACITY) {
-            elementType = PROPERTY_LIST;
-            return;
-        }
-        if (elementType == SETTLEMENT_FUEL_STORAGE_CAPACITY) {
-            elementType = PROPERTY_LIST;
+            propertyCatagory = -1;
             return;
         }
     }
@@ -178,41 +177,37 @@ class PropertiesXmlReader extends MspXmlReader {
         if (elementType == TIME_RATIO) {
             timeRatio = Double.parseDouble(data);
         }
-        if (elementType == PERSON_OXYGEN_CONSUMPTION) {
+        if (elementType == OXYGEN_CONSUMPTION) {
             personOxygenConsumption = Double.parseDouble(data);
         }
-        if (elementType == PERSON_WATER_CONSUMPTION) {
+        if (elementType == WATER_CONSUMPTION) {
             personWaterConsumption = Double.parseDouble(data);
         }
-        if (elementType == PERSON_FOOD_CONSUMPTION) {
+        if (elementType == FOOD_CONSUMPTION) {
             personFoodConsumption = Double.parseDouble(data);
         }
-        if (elementType == ROVER_OXYGEN_STORAGE_CAPACITY) {
-            roverOxygenStorageCapacity = Double.parseDouble(data);
+        if (elementType == OXYGEN_STORAGE_CAPACITY) {
+            double value = Double.parseDouble(data);
+            if (propertyCatagory == ROVER_PROPERTIES) roverOxygenStorageCapacity = value;
+            if (propertyCatagory == SETTLEMENT_PROPERTIES) settlementOxygenStorageCapacity = value;
         }
-        if (elementType == ROVER_WATER_STORAGE_CAPACITY) {
-            roverWaterStorageCapacity = Double.parseDouble(data);
+        if (elementType == WATER_STORAGE_CAPACITY) {
+            double value = Double.parseDouble(data);
+            if (propertyCatagory == ROVER_PROPERTIES) roverWaterStorageCapacity = value;
+            if (propertyCatagory == SETTLEMENT_PROPERTIES) settlementWaterStorageCapacity = value;
         }
-        if (elementType == ROVER_FOOD_STORAGE_CAPACITY) {
-            roverFoodStorageCapacity = Double.parseDouble(data);
+        if (elementType == FOOD_STORAGE_CAPACITY) {
+            double value = Double.parseDouble(data);
+            if (propertyCatagory == ROVER_PROPERTIES) roverFoodStorageCapacity = value;
+            if (propertyCatagory == SETTLEMENT_PROPERTIES) settlementFoodStorageCapacity = value;
         }
-        if (elementType == ROVER_FUEL_STORAGE_CAPACITY) {
-            roverFuelStorageCapacity = Double.parseDouble(data);
+        if (elementType == FUEL_STORAGE_CAPACITY) {
+            double value = Double.parseDouble(data);
+            if (propertyCatagory == ROVER_PROPERTIES) roverFuelStorageCapacity = value;
+            if (propertyCatagory == SETTLEMENT_PROPERTIES) settlementFuelStorageCapacity = value;
         }
-        if (elementType == ROVER_FUEL_EFFICIENCY) {
+        if (elementType == FUEL_EFFICIENCY) {
             roverFuelEfficiency = Double.parseDouble(data);
-        }
-        if (elementType == SETTLEMENT_OXYGEN_STORAGE_CAPACITY) {
-            settlementOxygenStorageCapacity = Double.parseDouble(data);
-        }
-        if (elementType == SETTLEMENT_WATER_STORAGE_CAPACITY) {
-            settlementWaterStorageCapacity = Double.parseDouble(data);
-        }
-        if (elementType == SETTLEMENT_FOOD_STORAGE_CAPACITY) {
-            settlementFoodStorageCapacity = Double.parseDouble(data);
-        }
-        if (elementType == SETTLEMENT_FUEL_STORAGE_CAPACITY) {
-            settlementFuelStorageCapacity = Double.parseDouble(data);
         }
     }
 
