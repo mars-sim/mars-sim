6e7e0c03ee2f8899318596ad10dc9632a05109c2
barry-evans <barry-evans@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2002-01-23 09:06:15 +0000

    Added a probability rating

--- org/mars_sim/msp/simulation/person/MedicalComplaint.java
+++ org/mars_sim/msp/simulation/person/MedicalComplaint.java
@@ -8,6 +8,7 @@ package org.mars_sim.msp.simulation.person;
 
 import java.util.HashMap;
 import java.io.Serializable;
+import org.mars_sim.msp.simulation.MarsClock;
 
 /**
  * This class represents the definition of a specific Medical Complaint that can
@@ -19,12 +20,19 @@ import java.io.Serializable;
  */
 public class MedicalComplaint implements Serializable {
 
+    /**
+     * The maximum probability rating. This allows the complaint to be specifed
+     * to 1/100 th of a percentage.
+     */
+    public final static int MAXPROBABILITY = 100000;
+
 
     private String name;                // Identifying name
     private int seriousness;            // Seriousness of this illness
     private double degradePeriod;       // Time before complaint degrades
     private double recoveryPeriod;      // Time before Person recovers
-    private int performanceFactor;      // Factor effecting Person performance
+    private int probability;            // Probability of occuring
+    private double performanceFactor;   // Factor effecting Person performance
     MedicalComplaint nextPhase;         // Next phase of this illness
 
     /**
@@ -33,21 +41,28 @@ public class MedicalComplaint implements Serializable {
      * @param name Name of complaint.
      * @param seriousness How serious is this complaint.
      * @param degrade The time it takes before this complaint advances, if this
-     * value is zero, then the Person can shelf heel themselves.
+     * value is zero, then the Person can shelf heel themselves. This value is
+     * in Earth minutes.
      * @param recovery The time is takes for a Person to recover. If this value
-     * is zero it means the complaint results in death unless treated.
+     * is zero it means the complaint results in death unless treated. This
+     * value is in Earth minutes.
+     * @param probability The probability of this illness occuring, this can be
+     * between 0 and MAXPROBABILITY.
      * @param performance The percentage that a Persons performance is decreased.
      * @param next The complaint that this degrades into unless checked.
      */
     MedicalComplaint(String name, int seriousness,
                              double degrade, double recovery,
+                             int probability,
                              int performance, MedicalComplaint next) {
         this.name = name;
         this.seriousness = seriousness;
-        this.degradePeriod = degrade;
-        this.recoveryPeriod = recovery;
-        this.performanceFactor = performance;
+        // Convert from minutes into Millisols
+        this.degradePeriod = MarsClock.convertSecondsToMillisols(degrade * 60D);
+        this.recoveryPeriod = MarsClock.convertSecondsToMillisols(recovery * 60D);
+        this.performanceFactor = (performance / 100D);
         this.nextPhase = next;
+        this.probability = probability;
     }
 
     /**
@@ -76,10 +91,26 @@ public class MedicalComplaint implements Serializable {
 
     /**
      * Get the performance factor that effect Person with the complaint.
-     * @return Percentage value representing the effect.
+     * @return The value is between 0 -> 1.
      */
-    public int getPerformanceFactor() {
+    public double getPerformanceFactor() {
         return performanceFactor;
+    }
+
+    /**
+     * Get the probabity of this complaint.
+     * @return Probabity from 0 to 1.
+     */
+    public int getProbability() {
+        return probability;
+    }
+
+    /**
+     * Get the recover period.
+     * @return Double value representing a duration.
+     */
+    public double getRecoveryPeriod() {
+        return recoveryPeriod;
     }
 
     /**
@@ -91,10 +122,10 @@ public class MedicalComplaint implements Serializable {
     }
 
     /**
-     * Get the recover period.
-     * @return Double value representing a duration.
+     * Get a string respresentation.
+     * @return The name of the complaint.
      */
-    public double getRecoveryPeriod() {
-        return recoveryPeriod;
+    public String toString() {
+        return name;
     }
 }
