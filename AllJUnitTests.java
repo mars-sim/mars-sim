3bd1f1aa2227d6f81734eaa91e4368f852a5a08f
krose <krose@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2003-01-22 02:37:32 +0000

    Added a suite of JUnit tests. Each package has a JUnitTests class. AllJUnitTests in the default package runs all unit tests. A simple example is in org.mars_sim.msp.simulation.person.JUnitTests. See http://www.junit.org for details.

---
+++ AllJUnitTests.java
@@ -0,0 +1,53 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+// include any ONE of the following...
+//import junit.textui.TestRunner;
+//import junit.awtui.TestRunner;
+import junit.swingui.TestRunner;
+
+/**
+ * JUnit test suite
+ */
+public class AllJUnitTests extends TestCase {
+
+	private static final Class thisClass = AllJUnitTests.class;
+
+	/**
+	 * Run all JUnit tests.
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(thisClass);
+	}
+	
+	/**
+	 * Collection of external test suites to be included in current testing.
+	 */
+	public static Test suite() {
+		TestSuite suite = new TestSuite(thisClass);
+
+		suite.addTestSuite(org.mars_sim.msp.simulation.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.simulation.equipment.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.simulation.events.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.simulation.malfunction.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.simulation.person.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.simulation.person.ai.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.simulation.person.medical.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.simulation.structure.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.simulation.structure.building.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.simulation.structure.template.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.simulation.vehicle.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.ui.standard.JUnitTests.class);
+		suite.addTestSuite(org.mars_sim.msp.ui.standard.monitor.JUnitTests.class);
+
+		return suite;
+	}
+	
+	/**
+	 * Every JUnit test suite needs at least one test.This one obviously does nothing.
+	 * Any others begining with "test..." will be automatically included as well.
+	 */
+	public void testNothing() {
+	}
+}
---
+++ org/mars_sim/msp/simulation/person/JUnitTests.java
@@ -0,0 +1,98 @@
+package org.mars_sim.msp.simulation.person;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+// include any ONE of the following...
+//import junit.textui.TestRunner;
+//import junit.awtui.TestRunner;
+import junit.swingui.TestRunner;
+
+import org.mars_sim.msp.simulation.Mars;
+import org.mars_sim.msp.simulation.SimulationProperties;
+import org.mars_sim.msp.simulation.Coordinates;
+import org.mars_sim.msp.simulation.structure.Settlement;
+import org.mars_sim.msp.simulation.structure.building.LanderHab;
+
+/**
+ * JUnit test suite
+ */
+public class JUnitTests extends TestCase {
+
+	private static final Class thisClass = JUnitTests.class;
+
+	// Get test properites.
+	private static final java.util.Properties properties =
+		System.getProperties();
+	{
+		try {
+			properties.load(
+				new java.io.BufferedInputStream(
+					new java.io.FileInputStream(
+						thisClass.getName() + ".properties")));
+		}
+		catch (java.io.IOException e) {
+			// do nothing
+		}
+	}
+
+	/**
+	 * Any initialization necessary for all tests.
+	 */
+	public JUnitTests() {
+		super();
+	}
+
+	/**
+	 * Run all JUnit tests.
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(thisClass);
+	}
+
+	/**
+	 * Collection of external test suites to be included in current testing.
+	 */
+	public static Test suite() {
+		return new TestSuite(thisClass);
+	}
+
+	/**
+	 * Every JUnit test suite needs at least one test.This one obviously does nothing.
+	 * Any others begining with "test..." will be automatically included as well.
+	 */
+	public void testNothing() {
+	}
+
+	public void testCreatePerson1() throws Exception {
+
+		Mars mars = new Mars(new SimulationProperties());
+		String personName = "Person Name";
+		String settlementName = "Settlement Name";
+		Coordinates coords = new Coordinates(0, 0);
+		Settlement settlement = new Settlement(settlementName, coords, mars);
+
+		Person person = new Person(personName, settlement, mars);
+		
+		assertEquals("Name: ", personName, person.getName());
+		assertEquals("Coords: ", coords, person.getCoordinates());
+	}
+
+	public void testCreatePerson2() throws Exception {
+
+		Mars mars = new Mars(new SimulationProperties());
+		String personName = "Person Name";
+		String settlementName = "Settlement Name";
+		Coordinates coords = new Coordinates(0, 0);
+		Settlement settlement = new Settlement(settlementName, coords, mars);
+		settlement.getBuildingManager().addBuilding(
+			new LanderHab(settlement.getBuildingManager()));
+		mars.getUnitManager().addUnit(settlement);
+
+		Person person = new Person(personName, mars, mars.getUnitManager());
+		
+		assertEquals("Name: ", personName, person.getName());
+		assertEquals("Coords: ", coords, person.getCoordinates());
+	}
+}
