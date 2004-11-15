8322c0ed5fbc6a63e4b3a51a724c8ac347d810fc
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2004-11-15 06:19:38 +0000

    More work on cooking

--- org/mars_sim/msp/simulation/structure/building/function/Cooking.java
+++ org/mars_sim/msp/simulation/structure/building/function/Cooking.java
@@ -1,17 +1,22 @@
 /**
  * Mars Simulation Project
  * Cooking.java
- * @version 2.78 2004-11-10
+ * @version 2.78 2004-11-12
  * @author Scott Davis
  */
 package org.mars_sim.msp.simulation.structure.building.function;
 
 import java.io.Serializable;
+import java.util.*;
+import org.mars_sim.msp.simulation.Resource;
 import org.mars_sim.msp.simulation.Simulation;
 import org.mars_sim.msp.simulation.SimulationConfig;
 import org.mars_sim.msp.simulation.structure.building.*;
+import org.mars_sim.msp.simulation.time.MarsClock;
 
-
+/**
+ * The Cooking class is a building function for cooking meals.
+ */
 public class Cooking extends Function implements Serializable {
 
 	public static final String NAME = "Cooking";
@@ -19,6 +24,7 @@ public class Cooking extends Function implements Serializable {
 	// Data members
 	private int numCooks;
 	private int cookCapacity;
+	private List meals;
 	
 	/**
 	 * Constructor
@@ -30,6 +36,7 @@ public class Cooking extends Function implements Serializable {
 		super(NAME, building);
 		
 		numCooks = 0;
+		meals = new ArrayList();
 		
 		SimulationConfig simConfig = Simulation.instance().getSimConfig();
 		BuildingConfig config = simConfig.getBuildingConfiguration();
@@ -51,6 +58,21 @@ public class Cooking extends Function implements Serializable {
 	}
 	
 	/**
+	 * Adds a cook to the facility.
+	 */
+	public void addCook() {
+		if (numCooks < cookCapacity) numCooks++;
+	}
+	
+	/**
+	 * Removes a cook from the facility.
+	 *
+	 */
+	public void removeCook() {
+		if (numCooks > 0) numCooks--;
+	}
+	
+	/**
 	 * Get the current number of cooks using this facility.
 	 * @return number of cooks
 	 */
@@ -59,12 +81,66 @@ public class Cooking extends Function implements Serializable {
 	}
 	
 	/**
+	 * Gets the number of cooked meals at this facility.
+	 * @return number of meals
+	 */
+	public int getNumberOfMeals() {
+		return meals.size();
+	}
+	
+	/**
+	 * Checks if there are any cooked meals in this facility.
+	 * @return true if cooked meals
+	 */
+	public boolean hasCookedMeal() {
+		return (meals.size() > 0);
+	}
+	
+	/**
+	 * Gets the number of cooked meals in this facility.
+	 * @return number of meals
+	 */
+	public int getNumberOfCookedMeals() {
+		return meals.size();
+	}
+	
+	/**
+	 * Gets a cooked meal from this facility.
+	 * @return
+	 */
+	public CookedMeal getCookedMeal() {
+		CookedMeal bestMeal = null;
+		int bestQuality = -1;
+		Iterator i = meals.iterator();
+		while (i.hasNext()) {
+			CookedMeal meal = (CookedMeal) i.next();
+			if (meal.getQuality() > bestQuality) {
+				bestQuality = meal.getQuality();
+				bestMeal = meal;
+			}
+		}
+		
+		if (bestMeal != null) meals.remove(bestMeal);
+		
+		return bestMeal;
+	}
+	
+	/**
 	 * Time passing for the building.
 	 * @param time amount of time passing (in millisols)
 	 * @throws BuildingException if error occurs.
 	 */
 	public void timePassing(double time) throws BuildingException {
-
+		// Move expired meals back to food again (refrigerate leftovers).
+		Iterator i = meals.iterator();
+		while (i.hasNext()) {
+			CookedMeal meal = (CookedMeal) i.next();
+			MarsClock currentTime = Simulation.instance().getMasterClock().getMarsClock();
+			if (MarsClock.getTimeDiff(meal.getExpirationTime(), currentTime) < 0D) {
+				getBuilding().getInventory().addResource(Resource.FOOD, 1D);
+				i.remove();
+			}
+		}
 	}
 
 	/**
