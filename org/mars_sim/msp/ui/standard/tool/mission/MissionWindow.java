19218226515d73760f463c7fe500d0e7b0ee0411
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2007-01-15 05:16:27 +0000

    Adding create new mission and edit mission buttons with stubbed implementation.

--- org/mars_sim/msp/ui/standard/tool/mission/MissionWindow.java
+++ org/mars_sim/msp/ui/standard/tool/mission/MissionWindow.java
@@ -9,12 +9,19 @@ package org.mars_sim.msp.ui.standard.tool.mission;
 
 import java.awt.Dimension;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import org.mars_sim.msp.simulation.person.ai.mission.Mission;
 import org.mars_sim.msp.ui.standard.MainDesktopPane;
@@ -67,6 +74,35 @@ public class MissionWindow extends ToolWindow {
         missionList.addListSelectionListener(navpointPane);
         infoPane.add("Navpoints", navpointPane);
         
+        JPanel buttonPane = new JPanel(new FlowLayout());
+        mainPane.add(buttonPane, BorderLayout.SOUTH);
+        
+        JButton createButton = new JButton("Create New Mission");
+        createButton.addActionListener( 
+        		new ActionListener() {
+        			public void actionPerformed(ActionEvent e) {
+        				createNewMission();
+        			}
+        		});
+        buttonPane.add(createButton);
+        
+        final JButton editButton = new JButton("Edit Mission");
+        editButton.setEnabled(false);
+        editButton.addActionListener(
+        		new ActionListener() {
+        			public void actionPerformed(ActionEvent e) {
+        				Mission mission = (Mission) missionList.getSelectedValue();
+        				if (mission != null) editMission(mission);
+        			}
+        		});
+        missionList.addListSelectionListener(
+            	new ListSelectionListener() {
+            		public void valueChanged(ListSelectionEvent e) {
+            			editButton.setEnabled(missionList.getSelectedValue() != null);
+            		}
+            	});
+        buttonPane.add(editButton);
+        
         // Pack window
         pack();
 	}
@@ -78,6 +114,21 @@ public class MissionWindow extends ToolWindow {
 	public void selectMission(Mission mission) {
 		MissionListModel model = (MissionListModel) missionList.getModel();
 		if (model.containsMission(mission)) missionList.setSelectedValue(mission, true);
+	}
+	
+	/**
+	 * Open wizard to create a new mission.
+	 */
+	private void createNewMission() {
+		System.out.println("Creating new mission.");
+	}
+	
+	/**
+	 * Open wizard to edit a mission.
+	 * @param mission the mission to edit.
+	 */
+	private void editMission(Mission mission) {
+		System.out.println("Editing mission: " + mission.getName());
 	}
 	
 	/**
