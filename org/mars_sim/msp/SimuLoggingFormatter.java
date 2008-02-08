58216f772d0ec25394b5ac6f76b1c438e51f98be
sebturku <sebturku@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2008-02-08 13:43:52 +0000

    changed licensing message

--- org/mars_sim/msp/SimuLoggingFormatter.java
+++ org/mars_sim/msp/SimuLoggingFormatter.java
@@ -1,16 +1,9 @@
-/***********************************************************************
-*This program/file is free software: you can redistribute it and/or modify
-*it under the terms of the GNU General Public License as published by
-*the Free Software Foundation, either version 3 of the License, or
-*(at your option) any later version.
-*This program/file is distributed in the hope that it will be useful,
-*but WITHOUT ANY WARRANTY; without even the implied warranty of
-*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*GNU General Public License for more details.
-*You should have received a copy of the GNU General Public License
-*along with this program/file.  If not, see <http://www.gnu.org/licenses/>.
-* 
-***********************************************************************/
+/**
+ * Mars Simulation Project
+ * SimuLoggingFormatter.java
+ * @version 2.83 2008-08-08
+ * @author Sebastien Venot
+ */
 package org.mars_sim.msp;
 
 import java.text.DateFormat;
@@ -18,11 +11,8 @@ import java.util.Date;
 import java.util.logging.Formatter;
 import java.util.logging.LogRecord;
 
-/**
- * @author Sebastien Venot
- * @version 1.0
- * @date 8.2.2008
- */
+
+
 public class SimuLoggingFormatter extends Formatter {
     
     public final static String LINEFEED =
