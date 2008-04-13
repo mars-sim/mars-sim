23a0ea81e6748b2d32f4900d554363e310d0f6ba
sebturku <sebturku@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2008-04-13 13:20:34 +0000

    added maximum size for cache

--- org/mars_sim/msp/ui/standard/sound/SoundConstants.java
+++ org/mars_sim/msp/ui/standard/sound/SoundConstants.java
@@ -15,7 +15,7 @@ public interface SoundConstants {
 	public final static String SOUNDS_ROOT_PATH = "sounds/";
 	
 	// Unit window sounds for rovers.
-	public final static String SND_ROVER_MOVING = "rover_moving.ogg";
+    public final static String SND_ROVER_MOVING = "rover_moving.ogg";
     public final static String SND_ROVER_MALFUNCTION = "rover_malfunction.ogg";
     public final static String SND_ROVER_MAINTENANCE = "rover_maintenance.ogg";
     public final static String SND_ROVER_PARKED = "";
@@ -34,7 +34,11 @@ public interface SoundConstants {
     public final static String SND_EQUIPMENT = "";
     
     // Supported sound formats
-    public final static String SND_FORMAT_WAV = ".wav";
-    public final static String SND_FORMAT_MP3 = ".mp3";
-    public final static String SND_FORMAT_OGG = ".ogg";
+    public final static String SND_FORMAT_WAV  = ".wav";
+    public final static String SND_FORMAT_MP3  = ".mp3";
+    public final static String SND_FORMAT_OGG  = ".ogg";
+    public final static String SND_FORMAT_MIDI = ".mid";
+    
+    //maximum amount of clips in cache
+    public final static int MAX_CACHE_SIZE = 5;
 }
