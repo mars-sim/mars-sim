7d972a7403e9b7f82977e9f465528befc266ec42
sebturku <sebturku@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2008-05-17 06:35:52 +0000

    *** empty log message ***

--- org/mars_sim/msp/simulation/vehicle/LightUtilityVehicle.java
+++ org/mars_sim/msp/simulation/vehicle/LightUtilityVehicle.java
@@ -10,28 +10,48 @@ import java.util.Collection;
 
 import org.mars_sim.msp.simulation.CollectionUtils;
 import org.mars_sim.msp.simulation.Inventory;
+import org.mars_sim.msp.simulation.SimulationConfig;
 import org.mars_sim.msp.simulation.Unit;
 import org.mars_sim.msp.simulation.person.Person;
 import org.mars_sim.msp.simulation.resource.AmountResource;
+import org.mars_sim.msp.simulation.resource.Part;
 import org.mars_sim.msp.simulation.structure.Settlement;
 
 
 public class LightUtilityVehicle extends GroundVehicle implements Crewable{
     
+    private int crewCapacity = 0; // The LightUtilityVehicle's capacity for crewmembers.
+    private Collection<Part> attachments = null;
+    private int slotNumber  = 0;
+    
     public LightUtilityVehicle(String name, String description, Settlement settlement)
     throws Exception{
 	super(name,description,settlement);
+	
+	//Get vehicle configuration.
+	VehicleConfig config = SimulationConfig.instance().getVehicleConfiguration();
 	
 	//Add scope to malfunction manager.
 	malfunctionManager.addScopeString("LightUtilityVehicle");
 	malfunctionManager.addScopeString("Crewable");
 	malfunctionManager.addScopeString(description);
+	if (config.hasLab(description)) malfunctionManager.addScopeString("Laboratory");
+	if (config.hasSickbay(description)) malfunctionManager.addScopeString("Sickbay");
+	
+	if( config.hasPartAttachments(description)) {
+	    attachments = config.getAttachableParts(description);
+	    slotNumber = config.getPartAttachmentSlotNumber(description);
+	}
+	
+	crewCapacity = config.getCrewSize(description);
 	
 	Inventory inv = getInventory();
-	inv.addGeneralCapacity(200);
+	inv.addGeneralCapacity(config.getTotalCapacity(description));
+	
 	
 	//Set rover terrain modifier
 	setTerrainHandlingCapability(0D);
+	
     }
 
     /* 
@@ -63,7 +83,7 @@ public class LightUtilityVehicle extends GroundVehicle implements Crewable{
      * 
      */
     public int getCrewCapacity() {
-	return 1;
+	return crewCapacity;
     }
 
     /* 
@@ -80,6 +100,23 @@ public class LightUtilityVehicle extends GroundVehicle implements Crewable{
 	return getInventory().containsUnit(person);
     }
 
+    public Collection<Part> getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(Collection<Part> attachments) {
+        this.attachments = attachments;
+    }
+
+    public int getSlotNumber() {
+        return slotNumber;
+    }
+
+    public void setSlotNumber(int slotNumber) {
+        this.slotNumber = slotNumber;
+    }
+
+    
     
 
 }
