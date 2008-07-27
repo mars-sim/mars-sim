81987963fb4f1c4186ef64a6c9de0ad1edb443c8
sebturku <sebturku@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2008-07-27 12:33:15 +0000

    added fuel comsuption over time

--- org/mars_sim/msp/simulation/structure/building/function/FuelPowerSource.java
+++ org/mars_sim/msp/simulation/structure/building/function/FuelPowerSource.java
@@ -7,8 +7,12 @@
 package org.mars_sim.msp.simulation.structure.building.function;
 
 import java.io.Serializable;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.mars_sim.msp.simulation.Inventory;
+import org.mars_sim.msp.simulation.InventoryException;
 import org.mars_sim.msp.simulation.resource.AmountResource;
 import org.mars_sim.msp.simulation.structure.building.Building;
 
@@ -16,6 +20,12 @@ import org.mars_sim.msp.simulation.structure.building.Building;
 public class FuelPowerSource extends PowerSource implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    private static String CLASS_NAME = 
+    "org.mars_sim.msp.simulation.structure.building.function.FuelPowerSource";
+	
+    private static Logger logger = Logger.getLogger(CLASS_NAME);
+    
     private final static String TYPE = "Fuel Power Source";
     private boolean toggle = false;
     private double fuelCapacity; 
@@ -44,9 +54,7 @@ public class FuelPowerSource extends PowerSource implements Serializable {
      */
     @Override
     public double getCurrentPower(Building building) {
-	if(isToggleON()) {
-	    currentFuelLevel = currentFuelLevel- consumptionSpeed;
-	    
+	if(isToggleON()) {	    
 	    if(currentFuelLevel > 0) {
 		return getMaxPower();
 	    } else {
@@ -80,6 +88,25 @@ public class FuelPowerSource extends PowerSource implements Serializable {
     }
     
     public void consumeFuel(double time, Inventory inv) {
+	double consumedFuel = time  * consumptionSpeed;
+	 currentFuelLevel = currentFuelLevel - consumedFuel;
+	 
+	try {
+	    Set<AmountResource> resources = inv.getAllAmountResourcesStored();
+	    
+	    for(AmountResource res : resources) {
+		if(res == resource ) {
+		    Set<AmountResource> resAmounts = res.getAmountResources();
+		    for(AmountResource amountRes : resAmounts ) {
+			 double remainingAmount = inv.getAmountResourceStored(amountRes);
+			 if (consumedFuel > remainingAmount) consumedFuel = remainingAmount;
+			 inv.retrieveAmountResource(amountRes, consumedFuel);
+		    }   
+		}
+	    }
+	} catch (InventoryException e) {
+          logger.log(Level.SEVERE, "Issues when consuming fuel", e);
+	}
 	
     }
 }
