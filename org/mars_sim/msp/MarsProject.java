d71805a9a4cc62e3aa4bbe50be6aa194ce2680a2
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2008-08-31 18:29:37 +0000

    Formatting.

--- org/mars_sim/msp/MarsProject.java
+++ org/mars_sim/msp/MarsProject.java
@@ -1,6 +1,5 @@
 /**
- * Mars Simulation Project
- * MarsProject.java
+ * Mars Simulation Project MarsProject.java
  * @version 2.83 2008-02-26
  * @author Scott Davis
  */
@@ -16,99 +15,96 @@ import org.mars_sim.msp.simulation.Simulation;
 import org.mars_sim.msp.ui.standard.MainWindow;
 import org.mars_sim.msp.ui.standard.SplashWindow;
 
-/** 
- * MarsProject is the main class for the application. It creates both
- * Mars and the user interface.
+/**
+ * MarsProject is the main class for the application. It creates both Mars and
+ * the user interface.
  */
 public class MarsProject {
-    
+
     private static String CLASS_NAME = "org.mars_sim.msp.MarsProject";
-	
+
     private static Logger logger = Logger.getLogger(CLASS_NAME);
 
-
-    /** 
+    /**
      * Constructor
-     * @param args command line arguments. 
+     * 
+     * @param args command line arguments.
      */
     public MarsProject(String args[]) {
-	
-	logger.info("Starting Mars Simulation");
-	
+
+        logger.info("Starting Mars Simulation");
+
         // Create a splash window
         SplashWindow splashWindow = new SplashWindow();
-        
+
         // Create a simulation
         List argList = Arrays.asList(args);
-        
+
         if (argList.contains("-new")) {
-        	// If new argument, create new simulation.
-        	try {
-        		Simulation.createNewSimulation();
-        	}
-        	catch (Exception e) {
-        	    logger.log(Level.SEVERE, "Problem creating new simulation",e);
-        	    System.exit(0);
-        	}
+            // If new argument, create new simulation.
+            try {
+                Simulation.createNewSimulation();
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, "Problem creating new simulation", e);
+                System.exit(0);
+            }
         }
         else if (argList.contains("-load")) {
-        	// If load argument, load simulation from file.
-        	try {
-        		int index = argList.indexOf("-load");
-        		// Get the next argument as the filename.
-        		File loadFile = new File((String) argList.get(index + 1));
-        		if (loadFile.exists()) Simulation.instance().loadSimulation(loadFile);
-        		else {
-        		    logger.log(Level.SEVERE, "Problem loading simulation");
-        		    logger.log(Level.SEVERE, argList.get(index + 1) + " not found.");
-        		    System.exit(0);
-        		}
-        	}
-        	catch (Exception e) {
-        	    	logger.log(Level.SEVERE, "Problem loading existing simulation", e);
-        		System.exit(0);
-        	}
+            // If load argument, load simulation from file.
+            try {
+                int index = argList.indexOf("-load");
+                // Get the next argument as the filename.
+                File loadFile = new File((String) argList.get(index + 1));
+                if (loadFile.exists()) Simulation.instance().loadSimulation(loadFile);
+                else {
+                    logger.log(Level.SEVERE, "Problem loading simulation");
+                    logger.log(Level.SEVERE, argList.get(index + 1) + " not found.");
+                    System.exit(0);
+                }
+            } catch (Exception e) {
+                logger.log(Level.SEVERE, "Problem loading existing simulation", e);
+                System.exit(0);
+            }
         }
         else {
             try {
-            	// Load a the default simulation
-            	Simulation.instance().loadSimulation(null);
-            }
-            catch (Exception e) {
-        	if(!(e instanceof java.io.InvalidClassException)){
-            	   logger.log(Level.SEVERE, "Problem loading default simulation", e);
-        	}
-        	
-            	try {
-            		// If error reading default saved file, create new simulation.
-                	logger.warning("Creating new simulation");
-                	Simulation.createNewSimulation();
-            	}
-            	catch (Exception e2) {
-            		logger.log(Level.SEVERE, "Problem creating new simulation", e2);
-            		System.exit(0);
-            	}
+                // Load a the default simulation
+                Simulation.instance().loadSimulation(null);
+            } catch (Exception e) {
+                if (!(e instanceof java.io.InvalidClassException)) {
+                    logger.log(Level.SEVERE, "Problem loading default simulation", e);
+                }
+
+                try {
+                    // If error reading default saved file, create new
+                    // simulation.
+                    logger.warning("Creating new simulation");
+                    Simulation.createNewSimulation();
+                } catch (Exception e2) {
+                    logger.log(Level.SEVERE, "Problem creating new simulation", e2);
+                    System.exit(0);
+                }
             }
         }
-        
+
         // Start the simulation.
         Simulation.instance().start();
 
         // Create the main desktop window.
         new MainWindow();
-       
+
         // Dispose the splash window.
         splashWindow.dispose();
-       
     }
 
-    /** 
+    /**
      * The starting method for the application
+     * 
      * @param args the command line arguments
      */
-    public static void main(String args[]) {	
-	//starting the simulation
-        System.setProperty("swing.aatext","true");
+    public static void main(String args[]) {
+        // starting the simulation
+        System.setProperty("swing.aatext", "true");
         new MarsProject(args);
     }
 }
