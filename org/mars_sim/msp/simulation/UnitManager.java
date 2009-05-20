9a46f6671630ff863f2f8c3c0dd2db76d7033892
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2009-05-20 14:46:07 +0000

    Fixing code indention error.

--- org/mars_sim/msp/simulation/UnitManager.java
+++ org/mars_sim/msp/simulation/UnitManager.java
@@ -282,7 +282,7 @@ public class UnitManager implements Serializable {
 		SettlementConfig config = SimulationConfig.instance().getSettlementConfiguration();
     	
     	try {
-    	       Iterator<Settlement> i = getSettlements().iterator();
+    	    Iterator<Settlement> i = getSettlements().iterator();
     		while (i.hasNext()) {
     			Settlement settlement = i.next();
     			List vehicleTypes = config.getTemplateVehicleTypes(settlement.getTemplate());
