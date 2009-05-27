015950973222745dfab0e8bb191e49e7ef585000
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2009-05-27 09:40:08 +0000

    Changing all ogg sounds to use wav instead for now.

--- org/mars_sim/msp/ui/standard/sound/SoundConstants.java
+++ org/mars_sim/msp/ui/standard/sound/SoundConstants.java
@@ -1,7 +1,7 @@
 /**
  * Mars Simulation Project
  * SoundConstants.java
- * @version 2.85 2008-07-25
+ * @version 2.86 2009-05-27
  * @author Dima Stepanchuk
  */
 package org.mars_sim.msp.ui.standard.sound;
@@ -15,9 +15,9 @@ public interface SoundConstants {
 	public final static String SOUNDS_ROOT_PATH = "sounds/";
 	
 	// Unit window sounds for rovers.
-    public final static String SND_ROVER_MOVING = "rover_moving.ogg";
-    public final static String SND_ROVER_MALFUNCTION = "rover_malfunction.ogg";
-    public final static String SND_ROVER_MAINTENANCE = "rover_maintenance.ogg";
+    public final static String SND_ROVER_MOVING = "rover_moving.wav";
+    public final static String SND_ROVER_MALFUNCTION = "rover_malfunction.wav";
+    public final static String SND_ROVER_MAINTENANCE = "rover_maintenance.wav";
     public final static String SND_ROVER_PARKED = "";
     
     // Unit window sound for settlements.
@@ -28,10 +28,10 @@ public interface SoundConstants {
 
     // Unit window sounds for people.
     // TODO: Add additional sounds for people based on activity.
-    public final static String SND_PERSON_FEMALE1 = "female_person1.ogg";
-    public final static String SND_PERSON_FEMALE2 = "female_person2.ogg";
-    public final static String SND_PERSON_MALE1 = "male_person1.ogg";
-    public final static String SND_PERSON_MALE2 = "male_person2.ogg";
+    public final static String SND_PERSON_FEMALE1 = "female_person1.wav";
+    public final static String SND_PERSON_FEMALE2 = "female_person2.wav";
+    public final static String SND_PERSON_MALE1 = "male_person1.wav";
+    public final static String SND_PERSON_MALE2 = "male_person2.wav";
     public final static String SND_PERSON_DEAD = "person_dead.wav";
     
     // Unit window sounds for equipment.
