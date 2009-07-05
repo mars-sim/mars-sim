02416b2c0758dc35842d39c1f5feed0d96a249b2
sebturku <sebturku@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2009-07-05 09:48:59 +0000

    *** empty log message ***

--- org/mars_sim/msp/simulation/person/ai/task/ObserveAstronomicalObjects.java
+++ org/mars_sim/msp/simulation/person/ai/task/ObserveAstronomicalObjects.java
@@ -1,6 +1,7 @@
 package org.mars_sim.msp.simulation.person.ai.task;
 
 import java.io.Serializable;
+import java.util.logging.Logger;
 
 import org.mars_sim.msp.simulation.person.Person;
 import org.mars_sim.msp.simulation.person.ai.Skill;
@@ -9,10 +10,22 @@ public class ObserveAstronomicalObjects extends ResearchScience implements Seria
 
 	private static final long serialVersionUID = 1L;
 	
+	
+    private static String CLASS_NAME = 
+    "org.mars_sim.msp.simulation.person.ai.task.ObserveAstronomicalObjects";
+
+    private static Logger s_log = Logger.getLogger(CLASS_NAME);
+	
 	public ObserveAstronomicalObjects(Person person)
 			throws Exception {
 		super(Skill.ASTRONOMY, person);
 
+	}
+	
+	public static double getProbability(Person person) {
+		double result = 0D;
+	
+		return result;
 	}
 
 	
