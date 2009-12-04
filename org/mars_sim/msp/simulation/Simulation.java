a78d57b3e804cb9809c6a66da141cb0e8060e56d
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2009-12-04 16:56:18 +0000

    Removing unused import statement.

--- org/mars_sim/msp/simulation/Simulation.java
+++ org/mars_sim/msp/simulation/Simulation.java
@@ -10,7 +10,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
