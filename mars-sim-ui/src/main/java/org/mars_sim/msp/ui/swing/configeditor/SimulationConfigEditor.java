1c387333a4d195675bafdcc8ca8d551b5ff1bb1b
saga2 <saga2@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2010-08-30 15:17:40 +0000

    - added dialog for asking the user if he really wants to create a new simulation - config editor started   - simple dialog that is a template   - beginning of configuration changes     - change from dtd to schema validation: this will be needed to use castor as a xml binding framework, so that we will have proper configuration model objects and have a cleaner approach in that area

---
+++ mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/configeditor/SimulationConfigEditor.java
@@ -0,0 +1,95 @@
+/*
+ * $Id$
+ *
+ * Copyright 2010 Home Entertainment Systems.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mars_sim.msp.ui.swing.configeditor;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+import java.awt.Window;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import org.mars_sim.msp.core.SimulationConfig;
+
+/**
+ * DOCME: documentation is missing
+ *
+ * @version $Revision$ $Date$
+ * @author <a href="mailto:mail@landrus.de">Christian Domsch</a>
+ *
+ */
+public class SimulationConfigEditor extends JDialog {
+
+	/* ---------------------------------------------------------------------- *
+	 * Constructors
+	 * ---------------------------------------------------------------------- */
+
+	public SimulationConfigEditor(Window owner, SimulationConfig config) {
+		super(owner, "Simulation Configuration Editor", ModalityType.APPLICATION_MODAL);
+
+		initComponents(config);
+		pack();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setLocation((screenSize.width - getWidth()) / 2, (screenSize.height - getHeight()) / 2);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				Window window = e.getWindow();
+				window.setVisible(false);
+				window.dispose();
+			}
+		});
+	}
+
+	/* ---------------------------------------------------------------------- *
+	 * Initializations
+	 * ---------------------------------------------------------------------- */
+
+	private void initComponents(SimulationConfig config) {
+		JPanel contentPanel = new JPanel();
+		GridBagLayout gbl = new GridBagLayout();
+		contentPanel.setLayout(gbl);
+		Class<? extends SimulationConfig> type = config.getClass();
+		int row = 0;
+
+		for (Method method : type.getDeclaredMethods()) {
+			boolean isPublic = (method.getModifiers() & Modifier.PUBLIC) == 1;
+			String name = method.getName();
+
+			if (isPublic && name.startsWith("get") && name.endsWith("Configuration")) {
+				JLabel label = new JLabel(method.getName());
+				GridBagConstraints gbc = new GridBagConstraints();
+				gbc.gridx = 0;
+				gbc.gridy = row++;
+				gbl.setConstraints(label, gbc);
+				contentPanel.add(label);
+			}
+		}
+
+		setContentPane(contentPanel);
+	}
+
+}
