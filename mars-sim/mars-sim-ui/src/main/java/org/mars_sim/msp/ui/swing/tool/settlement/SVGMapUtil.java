7890c976750b5870661fb89baf00520de41a8ad9
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2010-10-18 15:20:18 +0000

    Adding SVG image for residential quarters building.

--- mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/tool/settlement/SVGMapUtil.java
+++ mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/tool/settlement/SVGMapUtil.java
@@ -49,6 +49,7 @@ public final class SVGMapUtil {
         buildingSVGMap.put("Mortar Storage Bin", "cement_storage_bin"); // same image for concrete, cement and mortar
         buildingSVGMap.put("Concrete Storage Bin", "cement_storage_bin"); // same image for concrete, cement and mortar
         buildingSVGMap.put("Atmospheric Processor", "atmospheric_processor");
+        buildingSVGMap.put("Residential Quarters", "residential_quarters");
         
         // TODO: Add more mapped entities.
     }
