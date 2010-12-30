f22ec9175c6239fdce4055efe7d12b69b6e41eff
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2010-12-30 04:04:54 +0000

    Adding new residential hab SVG image.

--- mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/tool/settlement/SVGMapUtil.java
+++ mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/tool/settlement/SVGMapUtil.java
@@ -60,6 +60,7 @@ public final class SVGMapUtil {
         buildingSVGMap.put("astronomy observatory", "astronomy_observatory");
         buildingSVGMap.put("md4 nuclear reactor", "md4_nuclear_reactor");
         buildingSVGMap.put("storage hab", "storage_hab");
+        buildingSVGMap.put("residential hab", "residential_hab");
         // TODO: Add more mapped entities.
     }
     
