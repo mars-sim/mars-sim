3d682ae57ae438913075f42bb54f0708e2d73656
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2012-06-15 06:51:25 +0000

    Adding StreamConsumer class for consuming output and error messages from Runtime.exec call to start sim in MarsProjectStarter.

--- mars-sim/mars-sim-main/src/main/java/org/mars_sim/msp/MarsProjectStarter.java
+++ mars-sim/mars-sim-main/src/main/java/org/mars_sim/msp/MarsProjectStarter.java
@@ -1,7 +1,7 @@
 /**
  * Mars Simulation Project
  * MarsProjectStarter.java
- * @version 3.00 2010-08-10
+ * @version 3.03 2012-06-14
  * @author Scott Davis
  */
 
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 /**
  * MarsProjectStarter is the default main class for the main executable JAR.
- * It creates a new virtual machine with 256MB memory and logging properties.
+ * It creates a new virtual machine with 512MB memory and logging properties.
  * It isn't used in the webstart release.
  */
 public class MarsProjectStarter {
@@ -20,32 +20,44 @@ public class MarsProjectStarter {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		
-            StringBuilder command = new StringBuilder();
-			
-			String javaHome = System.getenv("JAVA_HOME");
-			if (javaHome != null) {
-				if (javaHome.contains(" ")) javaHome = "\"" + javaHome;
-                command.append(javaHome).append(File.separator).append("bin").append(File.separator).append("java");
-				if (javaHome.contains(" ")) command.append("\"");
-			}
-			else command.append("java");
-			
-			command.append(" -Xms256m");
-			command.append(" -Xmx512m");
-			command.append(" -Djava.util.logging.config.file=logging.properties");
-            command.append(" -cp .").append(File.pathSeparator);
-            command.append("*").append(File.pathSeparator);
-            command.append("jars").append(File.separator).append("*");
-			command.append(" org.mars_sim.msp.MarsProject");
-			
-			String commandStr = command.toString();
-			System.out.println("Command: " + commandStr);
+    public static void main(String[] args) {
+
+        StringBuilder command = new StringBuilder();
+
+        String javaHome = System.getenv("JAVA_HOME");
+        if (javaHome != null) {
+            if (javaHome.contains(" ")) javaHome = "\"" + javaHome;
+            command.append(javaHome).append(File.separator).append("bin").append(File.separator).append("java");
+            if (javaHome.contains(" ")) command.append("\"");
+        }
+        else command.append("java");
+
+        command.append(" -Xms256m");
+        command.append(" -Xmx512m");
+        command.append(" -Djava.util.logging.config.file=logging.properties");
+        command.append(" -cp .").append(File.pathSeparator);
+        command.append("*").append(File.pathSeparator);
+        command.append("jars").append(File.separator).append("*");
+        command.append(" org.mars_sim.msp.MarsProject");
+
+        String commandStr = command.toString();
+        System.out.println("Command: " + commandStr);
         try {
             Process process = Runtime.getRuntime().exec(commandStr);
 
+            // Creating stream consumers for processes.
+            StreamConsumer errorConsumer = new StreamConsumer(process.getErrorStream(), "OUTPUT");            
+            StreamConsumer outputConsumer = new StreamConsumer(process.getInputStream(), "OUTPUT");
+
+            // Starting the stream consumers.
+            errorConsumer.start();
+            outputConsumer.start();
+
             process.waitFor();
+            
+            // Close stream consumers.
+            errorConsumer.join();
+            outputConsumer.join();
         } catch (IOException e) {
             throw new IllegalStateException(e);
         } catch (InterruptedException e) {
---
+++ mars-sim/mars-sim-main/src/main/java/org/mars_sim/msp/StreamConsumer.java
@@ -0,0 +1,46 @@
+/**
+ * Mars Simulation Project
+ * StreamConsumer.java
+ * @version 3.03 2012-06-14
+ * @author Scott Davis
+ */
+
+package org.mars_sim.msp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * A thread for consuming output from another stream.
+ */
+public class StreamConsumer extends Thread {
+
+    // Data members
+    InputStream in;
+    String type;
+
+    /**
+     * Constructor
+     * @param in the input stream to consume.
+     * @param type the stream type.
+     */
+    public StreamConsumer(InputStream in, String type) {
+        this.in = in;
+        this.type = type;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStreamReader isr = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(isr);
+            String line = null;
+            while ((line = br.readLine()) != null)
+                System.out.println(type + ">" + line);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
