d4af17a893ad6f1c72b45cd29fdc40fa2e867494
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2013-04-24 22:33:58 +0000

    Adding svg_image_mapping.properties file for mapping simulation items (buildings, construction stages, vehicles, etc) to SVG images to display on the settlement map tool.  Previously this was hard-coded in SVGMapUtil. Adding unit test suite to test that all required simulation items properly map to SVG images.

--- mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/tool/settlement/SVGMapUtil.java
+++ mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/tool/settlement/SVGMapUtil.java
@@ -1,13 +1,15 @@
 /**
  * Mars Simulation Project
  * SVGMapUtil.java
- * @version 3.04 2013-01-24
+ * @version 3.04 2013-04-23
  * @author Scott Davis
  */
 package org.mars_sim.msp.ui.swing.tool.settlement;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Properties;
 
 import org.apache.batik.gvt.GraphicsNode;
 import org.mars_sim.msp.ui.swing.SVGLoader;
@@ -20,12 +22,7 @@ import org.mars_sim.msp.ui.swing.SVGLoader;
 public final class SVGMapUtil {
 
     // Static members.
-    private static Map<String, String> BUILDING_SVG_MAP;
-    private static Map<String, String> CONSTRUCTION_SITE_MAP;
-    private static Map<String, String> VEHICLE_SVG_MAP;
-    private static Map<String, String> VEHICLE_MAINT_OVERLAY_SVG_MAP;
-    private static Map<String, String> VEHICLE_LOADING_OVERLAY_SVG_MAP;
-    private static Map<String, String> ATTACHMENT_PART_SVG_MAP;
+    private static Properties svgMapProperties;
     
     /**
      * Private constructor for utility class.
@@ -33,192 +30,68 @@ public final class SVGMapUtil {
     private SVGMapUtil() {}
     
     /**
-     * Initializes the building SVG map.
+     * Loads the SVG image mapping properties file.
      */
-    private static void initializeBuildingSVGMap() {
-        BUILDING_SVG_MAP = new HashMap<String, String>();
+    private static void loadSVGImageMappingPropertiesFile() {
         
-        // Add mapped building entries (do not include .svg suffix).
-        BUILDING_SVG_MAP.put("lander hab", "lander_hab");
-        BUILDING_SVG_MAP.put("inflatable greenhouse", "inflatable_greenhouse");
-        BUILDING_SVG_MAP.put("md1 nuclear reactor", "md1_nuclear_reactor");
-        BUILDING_SVG_MAP.put("starting erv base", "erv_base");
-        BUILDING_SVG_MAP.put("erv base", "erv_base");
-        BUILDING_SVG_MAP.put("regolith storage bin", "regolith_storage_bin");
-        BUILDING_SVG_MAP.put("wind turbine", "wind_turbine");
-        BUILDING_SVG_MAP.put("sand storage bin", "sand_storage_bin");
-        BUILDING_SVG_MAP.put("lime storage bin", "lime_storage_bin");
-        BUILDING_SVG_MAP.put("carbon storage bin", "carbon_storage_bin");
-        BUILDING_SVG_MAP.put("cement storage bin", "cement_storage_bin"); // same image for concrete, cement and mortar
-        BUILDING_SVG_MAP.put("mortar storage bin", "cement_storage_bin"); // same image for concrete, cement and mortar
-        BUILDING_SVG_MAP.put("concrete storage bin", "cement_storage_bin"); // same image for concrete, cement and mortar
-        BUILDING_SVG_MAP.put("atmospheric processor", "atmospheric_processor");
-        BUILDING_SVG_MAP.put("residential quarters", "residential_quarters");
-        BUILDING_SVG_MAP.put("lounge", "lounge");
-        BUILDING_SVG_MAP.put("command and control", "command_and_control");
-        BUILDING_SVG_MAP.put("infirmary", "infirmary");
-        BUILDING_SVG_MAP.put("laboratory", "laboratory");
-        BUILDING_SVG_MAP.put("workshop", "workshop");
-        BUILDING_SVG_MAP.put("garage", "garage");
-        BUILDING_SVG_MAP.put("large greenhouse", "large_greenhouse");
-        BUILDING_SVG_MAP.put("astronomy observatory", "astronomy_observatory");
-        BUILDING_SVG_MAP.put("md4 nuclear reactor", "md4_nuclear_reactor");
-        BUILDING_SVG_MAP.put("storage hab", "storage_hab");
-        BUILDING_SVG_MAP.put("residential hab", "residential_hab");
-        BUILDING_SVG_MAP.put("medical hab", "medical_hab");
-        BUILDING_SVG_MAP.put("research hab", "research_hab");
-        BUILDING_SVG_MAP.put("machinery hab", "machinery_hab");
-        BUILDING_SVG_MAP.put("solar photovoltaic array", "solar_photovoltaic_array");
-        BUILDING_SVG_MAP.put("solar thermal array", "solar_thermal_array");
-        BUILDING_SVG_MAP.put("small battery array", "small_battery_array");
-        BUILDING_SVG_MAP.put("methane power generator", "methane_power_generator");
-        BUILDING_SVG_MAP.put("small sabatier processor", "small_sabatier_processor");
-        BUILDING_SVG_MAP.put("bunkhouse", "bunkhouse");
-        BUILDING_SVG_MAP.put("manufacturing shed", "manufacturing_shed");
-        BUILDING_SVG_MAP.put("small areothermal well", "small_areothermal_well");
-        BUILDING_SVG_MAP.put("large areothermal well", "large_areothermal_well");
-        BUILDING_SVG_MAP.put("storage shed", "storage_shed");
-        BUILDING_SVG_MAP.put("outpost hub", "outpost_hub");
-        BUILDING_SVG_MAP.put("loading dock garage", "loading_dock_garage");
-        BUILDING_SVG_MAP.put("mining lab", "mining_lab");
-        BUILDING_SVG_MAP.put("inground greenhouse", "inground_greenhouse");
+        svgMapProperties = new Properties();
+
+        String fileName = SVGLoader.SVG_DIR + "svg_image_mapping.properties";
+        URL resource = SVGLoader.class.getResource(fileName);
+        InputStream inputStream = null;
+        try {
+            inputStream = resource.openStream();
+            svgMapProperties.load(inputStream);
+        }
+        catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+        finally {
+            try {
+                inputStream.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace(System.err);
+            }
+        }
     }
     
     /**
-     * Initializes the construction site SVG map.
+     * Gets an SVG graphics node for a given map item name.
+     * @param prefix the property key prefix (ex: building)
+     * @param name the name of the map item.
+     * @return the SVG graphics node.
      */
-    private static void initializeConstructionSiteSVGMap() {
-        CONSTRUCTION_SITE_MAP = new HashMap<String, String>();
+    private static GraphicsNode getSVGGraphicsNode(String prefix, String name) {
         
-        // Add mapped construction site entries (do not include .svg suffix).
-        CONSTRUCTION_SITE_MAP.put("residential quarters", "large_habitation_building_const");
-        CONSTRUCTION_SITE_MAP.put("lounge", "large_habitation_building_const");
-        CONSTRUCTION_SITE_MAP.put("command and control", "large_habitation_building_const");
-        CONSTRUCTION_SITE_MAP.put("infirmary", "large_habitation_building_const");
-        CONSTRUCTION_SITE_MAP.put("laboratory", "large_habitation_building_const");
-        CONSTRUCTION_SITE_MAP.put("workshop", "large_habitation_building_const");
-        CONSTRUCTION_SITE_MAP.put("large habitation frame", "large_habitation_frame_const");
-        CONSTRUCTION_SITE_MAP.put("unprepared surface foundation", "unprepared_surface_foundation_const");
-        CONSTRUCTION_SITE_MAP.put("garage", "garage_const");
-        CONSTRUCTION_SITE_MAP.put("garage frame", "garage_frame_const");
-        CONSTRUCTION_SITE_MAP.put("large greenhouse", "large_greenhouse_const");
-        CONSTRUCTION_SITE_MAP.put("large greenhouse frame", "large_greenhouse_frame_const");
-        CONSTRUCTION_SITE_MAP.put("astronomy observatory", "astronomy_observatory_const");
-        CONSTRUCTION_SITE_MAP.put("astronomy observatory frame", "astronomy_observatory_frame_const");
-        CONSTRUCTION_SITE_MAP.put("md4 nuclear reactor", "md4_nuclear_reactor_const");
-        CONSTRUCTION_SITE_MAP.put("md4 nuclear reactor frame", "md4_nuclear_reactor_frame_const");
-        CONSTRUCTION_SITE_MAP.put("md1 nuclear reactor", "md1_nuclear_reactor_const");
-        CONSTRUCTION_SITE_MAP.put("md1 nuclear reactor frame", "md1_nuclear_reactor_frame_const");
-        CONSTRUCTION_SITE_MAP.put("atmospheric processor", "atmospheric_processor_const");
-        CONSTRUCTION_SITE_MAP.put("atmospheric processor frame", "atmospheric_processor_frame_const");
-        CONSTRUCTION_SITE_MAP.put("erv base", "erv_base_const");
-        CONSTRUCTION_SITE_MAP.put("starting erv base", "erv_base_const");
-        CONSTRUCTION_SITE_MAP.put("erv base frame", "erv_base_frame_const");
-        CONSTRUCTION_SITE_MAP.put("inflatable greenhouse", "inflatable_greenhouse_const");
-        CONSTRUCTION_SITE_MAP.put("inflatable greenhouse frame", "inflatable_greenhouse_frame_const");
-        CONSTRUCTION_SITE_MAP.put("lander hab", "hab_const");
-        CONSTRUCTION_SITE_MAP.put("storage hab", "hab_const");
-        CONSTRUCTION_SITE_MAP.put("research hab", "hab_const");
-        CONSTRUCTION_SITE_MAP.put("residential hab", "hab_const");
-        CONSTRUCTION_SITE_MAP.put("medical hab", "hab_const");
-        CONSTRUCTION_SITE_MAP.put("machinery hab", "hab_const");
-        CONSTRUCTION_SITE_MAP.put("lander hab frame", "hab_frame_const");
-        CONSTRUCTION_SITE_MAP.put("solar photovoltaic array", "solar_photovoltaic_array_const");
-        CONSTRUCTION_SITE_MAP.put("array frame", "array_frame_const");
-        CONSTRUCTION_SITE_MAP.put("surface foundation 10m x 5m", "surface_foundation_10x5_const");
-        CONSTRUCTION_SITE_MAP.put("solar thermal array", "solar_thermal_array_const");
-        CONSTRUCTION_SITE_MAP.put("small battery array", "small_battery_array_const");
-        CONSTRUCTION_SITE_MAP.put("methane power generator", "methane_power_generator_const");
-        CONSTRUCTION_SITE_MAP.put("small steel frame", "small_steel_frame_const");
-        CONSTRUCTION_SITE_MAP.put("surface foundation 5m x 5m", "surface_foundation_5x5_const");
-        CONSTRUCTION_SITE_MAP.put("small sabatier processor", "small_sabatier_processor_const");
-        CONSTRUCTION_SITE_MAP.put("wind turbine", "wind_turbine_const");
-        CONSTRUCTION_SITE_MAP.put("steel frame tower", "steel_frame_tower_const");
-        CONSTRUCTION_SITE_MAP.put("carbon storage bin", "storage_bin_const");
-        CONSTRUCTION_SITE_MAP.put("cement storage bin", "storage_bin_const");
-        CONSTRUCTION_SITE_MAP.put("sand storage bin", "storage_bin_const");
-        CONSTRUCTION_SITE_MAP.put("lime storage bin", "storage_bin_const");
-        CONSTRUCTION_SITE_MAP.put("mortar storage bin", "storage_bin_const");
-        CONSTRUCTION_SITE_MAP.put("concrete storage bin", "storage_bin_const");
-        CONSTRUCTION_SITE_MAP.put("regolith storage bin", "storage_bin_const");
-        CONSTRUCTION_SITE_MAP.put("brick bin frame", "brick_bin_frame_const");
-        CONSTRUCTION_SITE_MAP.put("surface foundation 3m x 2m", "surface_foundation_3x2_const");
-        CONSTRUCTION_SITE_MAP.put("bunkhouse", "bunkhouse_const");
-        CONSTRUCTION_SITE_MAP.put("manufacturing shed", "manufacturing_shed_const");
-        CONSTRUCTION_SITE_MAP.put("small brick shed frame", "small_brick_shed_frame_const");
-        CONSTRUCTION_SITE_MAP.put("subsurface foundation 5m x 5m x 3m", "subsurface_foundation_5x5x3_const");
-        CONSTRUCTION_SITE_MAP.put("small areothermal well", "small_areothermal_well_const");
-        CONSTRUCTION_SITE_MAP.put("small areothermal well frame", "small_areothermal_well_frame_const");
-        CONSTRUCTION_SITE_MAP.put("shallow borehole drilling site", "shallow_borehole_drilling_site_const");
-        CONSTRUCTION_SITE_MAP.put("large areothermal well", "large_areothermal_well_const");
-        CONSTRUCTION_SITE_MAP.put("large areothermal well frame", "large_areothermal_well_frame_const");
-        CONSTRUCTION_SITE_MAP.put("deep borehole drilling site", "deep_borehole_drilling_site_const");
-        CONSTRUCTION_SITE_MAP.put("storage shed", "storage_shed_const");
-        CONSTRUCTION_SITE_MAP.put("outpost hub", "outpost_hub_const");
-        CONSTRUCTION_SITE_MAP.put("small vaulted brick frame", "small_vaulted_brick_frame_const");
-        CONSTRUCTION_SITE_MAP.put("subsurface foundation 10m x 10m x 3m", "subsurface_foundation_10x10x3_const");
-        CONSTRUCTION_SITE_MAP.put("loading dock garage", "loading_dock_garage_const");
-        CONSTRUCTION_SITE_MAP.put("garage_brick_frame", "garage_brick_frame_const");
-        CONSTRUCTION_SITE_MAP.put("ramped subsurface foundation 15m x 18m x 5m", "ramped_subsurface_foundation_15x18x5_const");
-        CONSTRUCTION_SITE_MAP.put("mining log", "mining_lab_const");
-        CONSTRUCTION_SITE_MAP.put("inground greenhouse", "inground_greenhouse_const");
-        CONSTRUCTION_SITE_MAP.put("vaulted glass brick frame", "vaulted_glass_brick_frame_const");
-        CONSTRUCTION_SITE_MAP.put("subsurface foundation 5m x 10m x 3m", "subsurface_foundation_5x10x3_const");
-    }
-    
-    /**
-     * Initializes the vehicle SVG map.
-     */
-    private static void initializeVehicleSVGMap() {
-        VEHICLE_SVG_MAP = new HashMap<String, String>();
+        GraphicsNode result = null;
         
-        // Add mapped vehicle entries (do not include .svg suffix).
-        VEHICLE_SVG_MAP.put("explorer rover", "explorer_rover");
-        VEHICLE_SVG_MAP.put("cargo rover", "cargo_rover");
-        VEHICLE_SVG_MAP.put("transport rover", "transport_rover");
-        VEHICLE_SVG_MAP.put("light utility vehicle", "light_utility_vehicle");
-    }
-    
-    /**
-     * Initializes the vehicle maintenance/repair overlay SVG map.
-     */
-    private static void initializeVehicleMaintOverlaySVGMap() {
-        VEHICLE_MAINT_OVERLAY_SVG_MAP = new HashMap<String, String>();
+        // Load svgMapProperties from file if necessary.
+        if (svgMapProperties == null) {
+            loadSVGImageMappingPropertiesFile();
+        }
         
-        // Add mapped vehicle entries (do not include .svg suffix).
-        VEHICLE_MAINT_OVERLAY_SVG_MAP.put("explorer rover", "explorer_rover_maint");
-        VEHICLE_MAINT_OVERLAY_SVG_MAP.put("cargo rover", "cargo_rover_maint");
-        VEHICLE_MAINT_OVERLAY_SVG_MAP.put("transport rover", "transport_rover_maint");
-        VEHICLE_MAINT_OVERLAY_SVG_MAP.put("light utility vehicle", "light_utility_vehicle_maint");
-    }
-    
-    /**
-     * Initializes the vehicle loading/unloading overlay SVG map.
-     */
-    private static void initializeVehicleLoadingOverlaySVGMap() {
-        VEHICLE_LOADING_OVERLAY_SVG_MAP = new HashMap<String, String>();
+        // Append property prefix.
+        StringBuffer propertyNameBuff = new StringBuffer("");
+        if (prefix != null) {
+            propertyNameBuff.append(prefix);
+            propertyNameBuff.append(".");
+        }
         
-        // Add mapped vehicle entries (do not include .svg suffix).
-        VEHICLE_LOADING_OVERLAY_SVG_MAP.put("explorer rover", "explorer_rover_load");
-        VEHICLE_LOADING_OVERLAY_SVG_MAP.put("cargo rover", "cargo_rover_load");
-        VEHICLE_LOADING_OVERLAY_SVG_MAP.put("transport rover", "transport_rover_load");
-        // Note: not including a loading/unloading overlay for the light utility vehicle yet.  
-        // May add it in the future.
-    }
-    
-    /**
-     * Initializes the attachment part SVG map.
-     */
-    private static void initializeAttachmentPartSVGMap() {
-        ATTACHMENT_PART_SVG_MAP = new HashMap<String, String>();
+        // Append name.
+        if (name != null) {
+            String prepName = name.trim().toLowerCase();
+            propertyNameBuff.append(prepName);
+        }
         
-        // Add mapped attachment part entries (do not include .svg suffix).
-        ATTACHMENT_PART_SVG_MAP.put("bulldozer blade", "bulldozer_blade");
-        ATTACHMENT_PART_SVG_MAP.put("soil compactor", "soil_compactor");
-        ATTACHMENT_PART_SVG_MAP.put("backhoe", "backhoe");
-        ATTACHMENT_PART_SVG_MAP.put("pneumatic drill", "pneumatic_drill");
-        ATTACHMENT_PART_SVG_MAP.put("drilling rig", "drilling_rig");
-        ATTACHMENT_PART_SVG_MAP.put("crane boom", "crane_boom");
+        String propertyName = propertyNameBuff.toString();
+        
+        String svgFileName = svgMapProperties.getProperty(propertyName);
+        if (svgFileName != null) {
+            result = SVGLoader.getSVGImage(svgFileName);
+        }
+        
+        return result;
     }
     
     /**
@@ -227,14 +100,8 @@ public final class SVGMapUtil {
      * @return SVG node or null if none found.
      */
     public static GraphicsNode getBuildingSVG(String buildingName) {
-        if (BUILDING_SVG_MAP == null) initializeBuildingSVGMap();
         
-        GraphicsNode result = null;
-        
-        String svgFileName = BUILDING_SVG_MAP.get(buildingName);
-        if (svgFileName != null) result = SVGLoader.getSVGImage(svgFileName);
-        
-        return result;
+        return getSVGGraphicsNode("building", buildingName);
     }
     
     /**
@@ -243,14 +110,8 @@ public final class SVGMapUtil {
      * @return SVG node or null if none found.
      */
     public static GraphicsNode getConstructionSiteSVG(String constructionSiteStageName) {
-        if (CONSTRUCTION_SITE_MAP == null) initializeConstructionSiteSVGMap();
         
-        GraphicsNode result = null;
-        
-        String svgFileName = CONSTRUCTION_SITE_MAP.get(constructionSiteStageName);
-        if (svgFileName != null) result = SVGLoader.getSVGImage(svgFileName);
-        
-        return result;
+        return getSVGGraphicsNode("construction_stage", constructionSiteStageName);
     }
     
     /**
@@ -259,14 +120,8 @@ public final class SVGMapUtil {
      * @return SVG node or null if none found.
      */
     public static GraphicsNode getVehicleSVG(String vehicleType) {
-        if (VEHICLE_SVG_MAP == null) initializeVehicleSVGMap();
         
-        GraphicsNode result = null;
-        
-        String svgFileName = VEHICLE_SVG_MAP.get(vehicleType);
-        if (svgFileName != null) result = SVGLoader.getSVGImage(svgFileName);
-        
-        return result;
+        return getSVGGraphicsNode("vehicle", vehicleType);
     }
     
     /**
@@ -275,14 +130,8 @@ public final class SVGMapUtil {
      * @return SVG node of null if none found.
      */
     public static GraphicsNode getMaintenanceOverlaySVG(String vehicleType) {
-        if (VEHICLE_MAINT_OVERLAY_SVG_MAP == null) initializeVehicleMaintOverlaySVGMap();
         
-        GraphicsNode result = null;
-        
-        String svgFileName = VEHICLE_MAINT_OVERLAY_SVG_MAP.get(vehicleType);
-        if (svgFileName != null) result = SVGLoader.getSVGImage(svgFileName);
-        
-        return result;
+        return getSVGGraphicsNode("vehicle.maintenance", vehicleType);
     }
     
     /**
@@ -291,14 +140,8 @@ public final class SVGMapUtil {
      * @return SVG node of null if none found.
      */
     public static GraphicsNode getLoadingOverlaySVG(String vehicleType) {
-        if (VEHICLE_LOADING_OVERLAY_SVG_MAP == null) initializeVehicleLoadingOverlaySVGMap();
         
-        GraphicsNode result = null;
-        
-        String svgFileName = VEHICLE_LOADING_OVERLAY_SVG_MAP.get(vehicleType);
-        if (svgFileName != null) result = SVGLoader.getSVGImage(svgFileName);
-        
-        return result;
+        return getSVGGraphicsNode("vehicle.loading", vehicleType);
     }
     
     /**
@@ -307,15 +150,7 @@ public final class SVGMapUtil {
      * @return SVG node or null if none found.
      */
     public static GraphicsNode getAttachmentPartSVG(String partType) {
-        if (ATTACHMENT_PART_SVG_MAP == null) {
-            initializeAttachmentPartSVGMap();
-        }
         
-        GraphicsNode result = null;
-        
-        String svgFileName = ATTACHMENT_PART_SVG_MAP.get(partType);
-        if (svgFileName != null) result = SVGLoader.getSVGImage(svgFileName);
-        
-        return result;
+        return getSVGGraphicsNode("vehicle.attachment_part", partType);
     }
 }
