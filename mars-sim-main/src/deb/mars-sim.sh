9b250f7077deaeb2e0883b4bc0769543439ac371
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2013-07-29 21:59:28 +0000

    Fixing problems with the debian start bash script executable permission. Adding image icon and gnome/unity desktop configuration.

--- mars-sim-main/src/deb/mars-sim.sh
+++ mars-sim-main/src/deb/mars-sim.sh
@@ -1,1 +1,3 @@
-java -Xms256m -Xmx512m -Djava.util.logging.config.file=logging.properties -cp .:* org.mars_sim.msp.MarsProject
+#!/bin/sh
+
+java -Xms256m -Xmx512m -Djava.util.logging.config.file=logging.properties -cp /usr/lib/mars-sim-main/* org.mars_sim.msp.MarsProject
