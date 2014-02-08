47fd8eadbbda8f33821fd9fa0630092fb71a21f0
Lars Næsbye Christensen <lars@naesbye.dk>
2014-02-08 00:21:23 +0000

    Updated version imports feature type

--- tools/landmarks/tsv2xml.py
+++ tools/landmarks/tsv2xml.py
@@ -8,14 +8,15 @@
 #
 # This script requires Python 2.4 or later and the 'SearchResults.tsv' file to be in the same
 # directory as the script file.
-# Generate TSV file at http://planetarynames.wr.usgs.gov/SearchResults?target=MARS
-# Scroll down and click 'TSV (tab separated values) for importing into other spread sheets', and save the file as SearchResults.tsv
-# Usage : 'python tsv2xml.py'
+#
+# Generate TSV file at http://planetarynames.wr.usgs.gov/AdvancedSearch
+# Select Target: Mars; Add column 'Feature Type Code'; Run Search
+# Scroll down and click 'TSV (tab separated values) for importing into other spread sheets' 
+# save the file as SearchResults.tsv in the same directory as this script.
+# Run : 'python tsv2xml.py' and the result file landmarks.xml is fit for use with MSP
 
 # TODO: 
-# Insert doctype - <!DOCTYPE landmark-list SYSTEM "conf/dtd/landmarks.dtd"> - doesn't seem to be necessary, though
-# Parse into MSP's native coordinate format
-# Sort according to name or type
+# Insert doctype - <!DOCTYPE landmark-list SYSTEM "conf/dtd/landmarks.dtd"> 
 
 from xml.dom.minidom import Document
 from decimal import Decimal
@@ -26,17 +27,18 @@ diameter_threshold = 750.0 #skip landmarks smaller than this size in km
 # Initialize types 
 xmldoc = Document()
 # in case we don't have a key line, assume some defaults
-index_feature = 0
+index_feature_name = 0
 index_target = 1
 index_diameter = 2
 index_lat = 3
 index_long = 4
-index_approval = 7
-index_origin = 8
+index_feature_type = 6
+index_approval = 8
+index_origin = 9
 
 # Add introductory comments 
 introcomment1 = xmldoc.createComment("Landmark coordinates from USGS Astrogeology Research Program")
-introcomment2 = xmldoc.createComment("http://planetarynames.wr.usgs.gov/SearchResults?target=MARS")
+introcomment2 = xmldoc.createComment("http://planetarynames.wr.usgs.gov/AdvancedSearch")
 introcomment3 = xmldoc.createComment("Landmarks to be displayed in the user interface. ")
 xmldoc.appendChild(introcomment1)
 xmldoc.appendChild(introcomment2)
@@ -44,9 +46,9 @@ xmldoc.appendChild(introcomment3)
  
 # Create the base XML element
 landmarks = xmldoc.createElement("landmark-list")
-landmarks.setAttribute("xmlns", "http://mars-sim.sourceforge.net/landmarks")
-landmarks.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
-landmarks.setAttribute("xsi:schemaLocation", "http://mars-sim.sourceforge.net/landmarks schema/landmarks.xsd")
+#landmarks.setAttribute("xmlns", "http://mars-sim.sourceforge.net/landmarks")
+#landmarks.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+#landmarks.setAttribute("xsi:schemaLocation", "http://mars-sim.sourceforge.net/landmarks schema/landmarks.xsd")
 
 xmldoc.appendChild(landmarks)
 
@@ -62,11 +64,12 @@ for tsvline in tsvlinelist:
 	elif "Feature_Name" in tsvline:
 		print "Found key line, parsing..." # first line of TSV, names of keys
 		paramlist = tsvline.split("\t") # explode into a list of keys
-		index_feature = paramlist.index('Feature_Name')
+		index_feature_name = paramlist.index('Feature_Name')
 		index_diameter = paramlist.index('Diameter')
 		index_lat = paramlist.index('Center_Latitude')
 		index_long = paramlist.index('Center_Longitude')
 		index_approval = paramlist.index('Approval_Date')
+		index_feature_type = paramlist.index('Feature_Type_Code')
 		index_origin = paramlist.index('Origin')
 	elif "Dropped" in tsvline:
 		print "Skipped dropped name" # ignore dropped (unapproved) names
@@ -79,7 +82,7 @@ for tsvline in tsvlinelist:
 		if float(valuelist[index_diameter]) > float(diameter_threshold): # threshold
 			landmark = xmldoc.createElement("landmark")
 		
-			namestring = valuelist[index_feature]
+			namestring = valuelist[index_feature_name]
 			namestring = namestring.replace('[', '')
 			namestring = namestring.replace(']', '')
 			landmark.setAttribute("name", namestring) # Feature_Name
@@ -98,6 +101,7 @@ for tsvline in tsvlinelist:
 
 			landmark.setAttribute("approvaldate", valuelist[index_approval]) # Approval Date string
 			landmark.setAttribute("origin", valuelist[index_origin]) # Origin of name
+			landmark.setAttribute("type", valuelist[index_feature_type]) # Type of feature
 			landmarks.appendChild(landmark)
 
 
@@ -123,9 +127,10 @@ for artobj in artobjarray:
 	landmark.setAttribute("name", artobj[0]) 
 	landmark.setAttribute("longitude", artobj[1]) 
 	landmark.setAttribute("latitude", artobj[2]) 
-	landmark.setAttribute("diameter", artobj[3]) #absurd?
-	landmark.setAttribute("approvaldate", "N/A") #absurd?
-	landmark.setAttribute("origin", "N/A") #absurd?
+	landmark.setAttribute("diameter", artobj[3]) 
+	landmark.setAttribute("approvaldate", "N/A") 
+	landmark.setAttribute("type", "AO") # artificial object
+	landmark.setAttribute("origin", "N/A") 
 
 	landmarks.appendChild(landmark)
 
