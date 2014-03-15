a78ab1dbdba4d4136b9435206fbc726287431ec5
st-pa <st-pa@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2014-03-15 11:42:02 +0000

    fixed cutoff of version string in splash screen

--- mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/SplashWindow.java
+++ mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/SplashWindow.java
@@ -9,29 +9,34 @@ package org.mars_sim.msp.ui.swing;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 import org.mars_sim.msp.core.Msg;
-import org.mars_sim.msp.core.RandomUtil;
 import org.mars_sim.msp.core.Simulation;
 
 /**
  * The SplashWindow class is a splash screen shown when the project
  * is loading.
  */
-public class SplashWindow {
+public class SplashWindow
+extends JComponent {
+
+	/** default serial id. */
+	private static final long serialVersionUID = 1L;
 
 //	private Window window;
 	private JFrame window;
 
 	// Constant data member
 	private static final String VERSION_STRING = Simulation.VERSION;
-	private static final String MSP_STRING = Msg.getString("SplashWindow.title");
+	private static final String MSP_STRING = Msg.getString("SplashWindow.title"); //$NON-NLS-1$
 
 	private static String IMAGE_NAME = "splash.png";
 	
@@ -39,19 +44,28 @@ public class SplashWindow {
 	private int width;
 	private int height;
 
+	/** stores the font for displaying {@link #VERSION_STRING}. */
+	private Font versionStringFont = new Font("SansSerif", Font.PLAIN, 11);
+	/** measures the pixels needed to display text. */
+	private FontMetrics positionMetrics = getFontMetrics(versionStringFont);
+	/** stores the displayed length of {@link #VERSION_STRING} in pixels. */
+	int versionStringWidth = positionMetrics.stringWidth(VERSION_STRING);
+
+
 	public SplashWindow() {
 		window = new JFrame() {
+			/** default serial id. */
+			private static final long serialVersionUID = 1L;
 			@Override
 			public void paint(Graphics g) {
 //				window.paint(g);
-
 				// Draw splash image and superimposed text
 				g.drawImage(splashImage, 0, 0, this);
 				window.setForeground(Color.white);
 				g.setFont(new Font("SansSerif", Font.PLAIN, 35));
 				g.drawString(MSP_STRING, 30, 60);
-				g.setFont(new Font("SansSerif", Font.PLAIN, 11));
-				g.drawString(VERSION_STRING, 930, 20);
+				g.setFont(versionStringFont);
+				g.drawString(VERSION_STRING, splashImage.getWidth(this) - versionStringWidth - 10, 20);
 			}
 		};
 
