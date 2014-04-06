5c279a8099f389d8c147e75143615e84ae66a9f0
st-pa <st-pa@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2014-04-06 20:21:31 +0000

    beautifications, renamed TowTabPanel to TabPanelTow

--- mars-sim-core/src/main/java/org/mars_sim/msp/core/mars/Mars.java
+++ mars-sim-core/src/main/java/org/mars_sim/msp/core/mars/Mars.java
@@ -12,33 +12,39 @@ import java.io.Serializable;
  * Mars represents the planet Mars in the simulation.
  */
 public class Mars implements Serializable {
-	
-	// Mars average radius in km.
+
+	/** default serial id. */
+	private static final long serialVersionUID = 1L;
+
+	/** Mars average radius in km. */
 	public static final double MARS_RADIUS_KM = 3393D;
-	
+
 	public static final double MARS_CIRCUMFERENCE = MARS_RADIUS_KM * 2D * Math.PI;
 
-    // Data members
-    private SurfaceFeatures surfaceFeatures; // Surface features
-    private OrbitInfo orbitInfo; // Orbital information
-    private Weather weather; // Martian weather
+	// Data members
+	/** Surface features. */
+	private SurfaceFeatures surfaceFeatures;
+	/** Orbital information. */
+	private OrbitInfo orbitInfo;
+	/** Martian weather. */
+	private Weather weather;
 
-    /** 
-     * Constructor
-     * @throws Exception if Mars could not be constructed.
-     */
-    public Mars() {
+	/** 
+	 * Constructor.
+	 * @throws Exception if Mars could not be constructed.
+	 */
+	public Mars() {
 
 		// Initialize surface features
 		surfaceFeatures = new SurfaceFeatures(); 
 
-        // Initialize orbit info
-        orbitInfo = new OrbitInfo();
+		// Initialize orbit info
+		orbitInfo = new OrbitInfo();
 
-	    // Initialize weather
-	    weather = new Weather();
-    }
-    
+		// Initialize weather
+		weather = new Weather();
+	}
+
 	/**
 	 * Initialize transient data in the simulation.
 	 * @throws Exception if transient data could not be constructed.
@@ -48,43 +54,46 @@ public class Mars implements Serializable {
 		surfaceFeatures.initializeTransientData();
 	}
 
-    /** Returns the orbital information
-     *  @return orbital information
-     */
-    public OrbitInfo getOrbitInfo() {
-        return orbitInfo;
-    }
+	/**
+	 * Returns the orbital information
+	 * @return orbital information
+	 */
+	public OrbitInfo getOrbitInfo() {
+		return orbitInfo;
+	}
 
-    /** Returns surface features
-     *  @return surfaces features
-     */
-    public SurfaceFeatures getSurfaceFeatures() {
-        return surfaceFeatures;
-    }
+	/**
+	 * Returns surface features
+	 * @return surfaces features
+	 */
+	public SurfaceFeatures getSurfaceFeatures() {
+		return surfaceFeatures;
+	}
 
-    /** Returns Martian weather
-     *  @return weather
-     */
-    public Weather getWeather() {
-        return weather;
-    }
-    
-    /**
-     * Time passing in the simulation.
-     * @param time time in millisols
-     * @throws Exception if error during time.
-     */
-    public void timePassing(double time) {
-    	orbitInfo.addTime(time);
-    	surfaceFeatures.timePassing(time);
-    }
-    
-    /**
-     * Prepare object for garbage collection.
-     */
-    public void destroy() {
-        surfaceFeatures.destroy();
-        orbitInfo.destroy();
-        weather.destroy();
-    }
+	/**
+	 * Returns Martian weather
+	 * @return weather
+	 */
+	public Weather getWeather() {
+		return weather;
+	}
+
+	/**
+	 * Time passing in the simulation.
+	 * @param time time in millisols
+	 * @throws Exception if error during time.
+	 */
+	public void timePassing(double time) {
+		orbitInfo.addTime(time);
+		surfaceFeatures.timePassing(time);
+	}
+
+	/**
+	 * Prepare object for garbage collection.
+	 */
+	public void destroy() {
+		surfaceFeatures.destroy();
+		orbitInfo.destroy();
+		weather.destroy();
+	}
 }
--- mars-sim-core/src/main/java/org/mars_sim/msp/core/mars/Weather.java
+++ mars-sim-core/src/main/java/org/mars_sim/msp/core/mars/Weather.java
@@ -5,7 +5,6 @@
  * @author Scott Davis
  * @author Hartmut Prochaska
  */
- 
 package org.mars_sim.msp.core.mars;
 
 import java.io.Serializable;
@@ -14,7 +13,8 @@ import org.mars_sim.msp.core.Coordinates;
 import org.mars_sim.msp.core.Simulation;
 
 /** Weather represents the weather on Mars */
-public class Weather implements Serializable {
+public class Weather
+implements Serializable {
 
 	/** default serial id. */
 	private static final long serialVersionUID = 1L;
@@ -32,83 +32,83 @@ public class Weather implements Serializable {
 	/** Constructs a Weather object */
 	public Weather() {}
 
-    /**
-     * Gets the air pressure at a given location.
-     * @return air pressure in atm.
-     */
-    public double getAirPressure(Coordinates location) {
-	    
-        // Get local elevation in meters.
+	/**
+	 * Gets the air pressure at a given location.
+	 * @return air pressure in atm.
+	 */
+	public double getAirPressure(Coordinates location) {
+
+		// Get local elevation in meters.
 		Mars mars = Simulation.instance().getMars();
-        TerrainElevation terrainElevation = mars.getSurfaceFeatures().getSurfaceTerrain();
-        double elevation = terrainElevation.getElevation(location);
-        
-        // p = pressure0 * e(-((density0 * gravitation) / pressure0) * h)
-        // P = 0.009 * e(-(0.0155 * 3.0 / 0.009) * elevation)
-        double pressure = SEA_LEVEL_AIR_PRESSURE * Math.exp(-1D *
-               SEA_LEVEL_AIR_DENSITY * SEA_LEVEL_GRAVITY / (SEA_LEVEL_AIR_PRESSURE * 1000)
-               * elevation);
+		TerrainElevation terrainElevation = mars.getSurfaceFeatures().getSurfaceTerrain();
+		double elevation = terrainElevation.getElevation(location);
 
-        return pressure;
-    }
+		// p = pressure0 * e(-((density0 * gravitation) / pressure0) * h)
+		// P = 0.009 * e(-(0.0155 * 3.0 / 0.009) * elevation)
+		double pressure = SEA_LEVEL_AIR_PRESSURE * Math.exp(-1D *
+				SEA_LEVEL_AIR_DENSITY * SEA_LEVEL_GRAVITY / (SEA_LEVEL_AIR_PRESSURE * 1000)
+				* elevation);
 
-    /**
-     * Gets the surface temperature at a given location.
-     * @return temperature in celsius.
-     */
-    public double getTemperature(Coordinates location) {
-        // easy implementation, needs revision for phi
-        // We can change this later.
-        
-        // standard -120D if extreme cold
-        
-        double temperature = EXTREME_COLD;
+		return pressure;
+	}
+
+	/**
+	 * Gets the surface temperature at a given location.
+	 * @return temperature in celsius.
+	 */
+	public double getTemperature(Coordinates location) {
+		// easy implementation, needs revision for phi
+		// We can change this later.
+
+		// standard -120D if extreme cold
+
+		double temperature = EXTREME_COLD;
 		Mars mars = Simulation.instance().getMars();
-        SurfaceFeatures surfaceFeatures = mars.getSurfaceFeatures();
-        
-        if (surfaceFeatures.inDarkPolarRegion(location)){
-            
-            //known temperature for cold days at the pole
-            
-            temperature = -150D;
-            
-        } else {
-           
-            // + getSurfaceSunlight * (80D / 127D (max sun))
-            // if sun full we will get -40D the avg, if night or twilight we will get 
-            // a smoth temperature change and in the night -120D
-        
-            temperature = temperature + surfaceFeatures.getSurfaceSunlight(location) * 0.629921D;
-        
-            // not correct but guess: - (elevation * 5)
-        
-            TerrainElevation terrainElevation = surfaceFeatures.getSurfaceTerrain();
-            temperature = temperature - (terrainElevation.getElevation(location) * 5D);
-        
-            // - ((math.pi/2) / (phi of location)) * 20
-            // guess, but could work, later we can implement real physic
-        
-            double piHalf = Math.PI / 2.0;
-            double degrees = 0;
-            double phi = location.getPhi();
-        
-            if (phi < piHalf) {
-                degrees = ((piHalf - phi) / piHalf);
-            } else if (phi > piHalf){
-                degrees = ((phi - piHalf) / piHalf);
-            }
+		SurfaceFeatures surfaceFeatures = mars.getSurfaceFeatures();
 
-            temperature = temperature - (20 * degrees) ;
-        
-        }
-	
-        return temperature;
-    }
-    
-    /**
-     * Prepare object for garbage collection.
-     */
-    public void destroy() {
-        // Do nothing
-    }
+		if (surfaceFeatures.inDarkPolarRegion(location)){
+
+			//known temperature for cold days at the pole
+
+			temperature = -150D;
+
+		} else {
+
+			// + getSurfaceSunlight * (80D / 127D (max sun))
+			// if sun full we will get -40D the avg, if night or twilight we will get 
+			// a smoth temperature change and in the night -120D
+
+			temperature = temperature + surfaceFeatures.getSurfaceSunlight(location) * 0.629921D;
+
+			// not correct but guess: - (elevation * 5)
+
+			TerrainElevation terrainElevation = surfaceFeatures.getSurfaceTerrain();
+			temperature = temperature - (terrainElevation.getElevation(location) * 5D);
+
+			// - ((math.pi/2) / (phi of location)) * 20
+			// guess, but could work, later we can implement real physic
+
+			double piHalf = Math.PI / 2.0;
+			double degrees = 0;
+			double phi = location.getPhi();
+
+			if (phi < piHalf) {
+				degrees = ((piHalf - phi) / piHalf);
+			} else if (phi > piHalf){
+				degrees = ((phi - piHalf) / piHalf);
+			}
+
+			temperature = temperature - (20 * degrees) ;
+
+		}
+
+		return temperature;
+	}
+
+	/**
+	 * Prepare object for garbage collection.
+	 */
+	public void destroy() {
+		// Do nothing
+	}
 }
