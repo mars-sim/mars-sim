749e29a8679021b6d9ae0a5191de677ceacfe273
scud1 <scud1@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2014-09-24 02:09:28 +0000

    Overriding equals methods for MissionPhase and TaskPhase classes. Fixing incorrect name reference in Sleep task.

--- mars-sim/mars-sim-core/src/main/java/org/mars_sim/msp/core/person/ai/mission/MissionPhase.java
+++ mars-sim/mars-sim-core/src/main/java/org/mars_sim/msp/core/person/ai/mission/MissionPhase.java
@@ -1,7 +1,7 @@
 /**
  * Mars Simulation Project
  * MissionPhase.java
- * @version 3.07 2014-09-15
+ * @version 3.07 2014-09-23
  * @author Scott Davis
  */
 package org.mars_sim.msp.core.person.ai.mission;
@@ -13,6 +13,9 @@ import java.io.Serializable;
  */
 public final class MissionPhase implements Serializable {
 
+    /** default serial id. */
+    private static final long serialVersionUID = 1L;
+    
     // The phase name.
     private String name;
     
@@ -35,5 +38,15 @@ public final class MissionPhase implements Serializable {
     @Override
     public String toString() {
         return getName();
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = false;
+        if ((obj != null) && (obj instanceof MissionPhase) && 
+                obj.toString().equals(toString())) {
+            result = true;
+        }
+        return result;
     }
 }
