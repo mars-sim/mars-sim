932db30090e222c072418d87b0ec390103b589d2
st-pa <st-pa@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2014-10-17 13:47:01 +0000

    indentation beautification and reduction of warnings

--- mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/tool/resupply/ResupplyWindow.java
+++ mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/tool/resupply/ResupplyWindow.java
@@ -12,13 +12,11 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JDialog;
-import javax.swing.JOptionPane;
-
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -31,189 +29,196 @@ import org.mars_sim.msp.ui.swing.tool.ToolWindow;
 
 /**
  * Window for the resupply tool.
+ * TODO externalize strings
  */
-public class ResupplyWindow extends ToolWindow implements ListSelectionListener {
+public class ResupplyWindow
+extends ToolWindow
+implements ListSelectionListener {
 
-    // Tool name
-    public static final String NAME = "Resupply Tool";
+	/** default serial id. */
+	private static final long serialVersionUID = 1L;
 
-    // Data members
-    private IncomingListPanel incomingListPane;
-    private ArrivedListPanel arrivedListPane;
-    private TransportDetailPanel detailPane;
-    private JButton modifyButton;
-    private JButton cancelButton;
+	/** Tool name. */
+	public static final String NAME = "Resupply Tool";
 
-    /**
-     * Constructor
-     * @param desktop the main desktop panel.
-     */
-    public ResupplyWindow(MainDesktopPane desktop) {
+	// Data members
+	private IncomingListPanel incomingListPane;
+	private ArrivedListPanel arrivedListPane;
+	private TransportDetailPanel detailPane;
+	private JButton modifyButton;
+	private JButton cancelButton;
 
-        // Use the ToolWindow constructor.
-        super(NAME, desktop);
+	/**
+	 * Constructor.
+	 * @param desktop the main desktop panel.
+	 */
+	public ResupplyWindow(MainDesktopPane desktop) {
 
-        // Create main panel.
-        JPanel mainPane = new JPanel(new BorderLayout());
-        mainPane.setBorder(MainDesktopPane.newEmptyBorder());
-        setContentPane(mainPane);
+		// Use the ToolWindow constructor.
+		super(NAME, desktop);
 
-        // Create list panel.
-        JPanel listPane = new JPanel(new GridLayout(2, 1));
-        mainPane.add(listPane, BorderLayout.WEST);
+		// Create main panel.
+		JPanel mainPane = new JPanel(new BorderLayout());
+		mainPane.setBorder(MainDesktopPane.newEmptyBorder());
+		setContentPane(mainPane);
 
-        // Create incoming list panel.
-        incomingListPane = new IncomingListPanel();
-        incomingListPane.getIncomingList().addListSelectionListener(this);
-        listPane.add(incomingListPane);
+		// Create list panel.
+		JPanel listPane = new JPanel(new GridLayout(2, 1));
+		mainPane.add(listPane, BorderLayout.WEST);
 
-        // Create arrived list panel.
-        arrivedListPane = new ArrivedListPanel();
-        listPane.add(arrivedListPane);
+		// Create incoming list panel.
+		incomingListPane = new IncomingListPanel();
+		incomingListPane.getIncomingList().addListSelectionListener(this);
+		listPane.add(incomingListPane);
 
-        // Set incoming and arrived list panels to listen to each other's list selections.
-        incomingListPane.getIncomingList().addListSelectionListener(arrivedListPane);
-        arrivedListPane.getArrivedList().addListSelectionListener(incomingListPane);
+		// Create arrived list panel.
+		arrivedListPane = new ArrivedListPanel();
+		listPane.add(arrivedListPane);
 
-        // Create detail panel.
-        detailPane = new TransportDetailPanel();
-        incomingListPane.getIncomingList().addListSelectionListener(detailPane);
-        arrivedListPane.getArrivedList().addListSelectionListener(detailPane);
-        mainPane.add(detailPane, BorderLayout.CENTER);
+		// Set incoming and arrived list panels to listen to each other's list selections.
+		incomingListPane.getIncomingList().addListSelectionListener(arrivedListPane);
+		arrivedListPane.getArrivedList().addListSelectionListener(incomingListPane);
 
-        // Create button panel.
-        JPanel buttonPane = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        mainPane.add(buttonPane, BorderLayout.SOUTH);
+		// Create detail panel.
+		detailPane = new TransportDetailPanel();
+		incomingListPane.getIncomingList().addListSelectionListener(detailPane);
+		arrivedListPane.getArrivedList().addListSelectionListener(detailPane);
+		mainPane.add(detailPane, BorderLayout.CENTER);
 
-        // Create new button.
+		// Create button panel.
+		JPanel buttonPane = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		mainPane.add(buttonPane, BorderLayout.SOUTH);
+
+		// Create new button.
 		// 9/29/2014 modified by mk: Changed button text from "New"  to "New Mission"
-        JButton newButton = new JButton("New Mission");
-        newButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent evt) {
-                createNewTransportItem();
-            }
-        });
-        buttonPane.add(newButton);
+		JButton newButton = new JButton("New Mission");
+		newButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				createNewTransportItem();
+			}
+		});
+		buttonPane.add(newButton);
 
-        // Create modify button.
+		// Create modify button.
 		// 9/29/2014 modified by mkung: Changed button text from "Modify"  to "Modify Mission"
-        modifyButton = new JButton("Modify Mission");
-        modifyButton.setEnabled(false);
-        modifyButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent evt) {
-                modifyTransportItem();
-            }
-        });
-        buttonPane.add(modifyButton);
+		modifyButton = new JButton("Modify Mission");
+		modifyButton.setEnabled(false);
+		modifyButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				modifyTransportItem();
+			}
+		});
+		buttonPane.add(modifyButton);
 
-        // Create cancel button.
+		// Create cancel button.
 		// 9/29/2014 modified by mkung: Changed button text from "Discard"  to "Discard Mission"
-        cancelButton = new JButton("Discard Mission");
-        cancelButton.setEnabled(false);
-        cancelButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent evt) {
-                cancelTransportItem();
-            }
-        });
-        buttonPane.add(cancelButton);
+		cancelButton = new JButton("Discard Mission");
+		cancelButton.setEnabled(false);
+		cancelButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				cancelTransportItem();
+			}
+		});
+		buttonPane.add(cancelButton);
 
-        pack();
-    }
+		pack();
+	}
 
-    /**
-     * Opens a create dialog.
-     */
-    private void createNewTransportItem() {
-        // Pause simulation.
-        desktop.getMainWindow().pauseSimulation();
+	/**
+	 * Opens a create dialog.
+	 */
+	private void createNewTransportItem() {
+		// Pause simulation.
+		desktop.getMainWindow().pauseSimulation();
 
-        // Create new transportItem dialog.
-        new NewTransportItemDialog(desktop.getMainWindow().getFrame());
+		// Create new transportItem dialog.
+		new NewTransportItemDialog(desktop.getMainWindow().getFrame());
 
-        // Unpause simulation.
-        desktop.getMainWindow().unpauseSimulation();
-    }
+		// Unpause simulation.
+		desktop.getMainWindow().unpauseSimulation();
+	}
 
-    /**
-     * Opens a modify dialog for the currently selected transport item.
-     */
-    private void modifyTransportItem() {
-        // Pause simulation.
-        desktop.getMainWindow().pauseSimulation();
+	/**
+	 * Opens a modify dialog for the currently selected transport item.
+	 */
+	private void modifyTransportItem() {
+		// Pause simulation.
+		desktop.getMainWindow().pauseSimulation();
 
-        // Get currently selected incoming transport item.
-        Transportable transportItem = (Transportable) incomingListPane.getIncomingList().getSelectedValue();
+		// Get currently selected incoming transport item.
+		Transportable transportItem = (Transportable) incomingListPane.getIncomingList().getSelectedValue();
 
-        if ((transportItem != null)) {
-            if (transportItem instanceof Resupply) {
-                // Create modify resupply mission dialog.
-                Resupply resupply = (Resupply) transportItem;
-                String title = "Modify Resupply Mission";
-                new ModifyTransportItemDialog(desktop.getMainWindow().getFrame(), title, resupply);
-            }
-            else if (transportItem instanceof ArrivingSettlement) {
-                // Create modify arriving settlement dialog.
-                ArrivingSettlement settlement = (ArrivingSettlement) transportItem;
-                String title = "Modify Arriving Settlement";
-                new ModifyTransportItemDialog(desktop.getMainWindow().getFrame(), title, settlement);
-            }
-        }
- 
-        // Unpause simulation.
-        desktop.getMainWindow().unpauseSimulation();
-    }
+		if ((transportItem != null)) {
+			if (transportItem instanceof Resupply) {
+				// Create modify resupply mission dialog.
+				Resupply resupply = (Resupply) transportItem;
+				String title = "Modify Resupply Mission";
+				new ModifyTransportItemDialog(desktop.getMainWindow().getFrame(), title, resupply);
+			}
+			else if (transportItem instanceof ArrivingSettlement) {
+				// Create modify arriving settlement dialog.
+				ArrivingSettlement settlement = (ArrivingSettlement) transportItem;
+				String title = "Modify Arriving Settlement";
+				new ModifyTransportItemDialog(desktop.getMainWindow().getFrame(), title, settlement);
+			}
+		}
 
-    /**
-     * Cancels the currently selected transport item.
-     */
-    private void cancelTransportItem() {     
-     // 2014-10-04 by mkung -- added a dialog box asking the user to confirm "discarding" the mission
-        JDialog.setDefaultLookAndFeelDecorated(true);
-           int response = JOptionPane.showConfirmDialog(null, "Are you sure you want to discard the highlighted mission?", "Confirm",
-               JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-           if (response == JOptionPane.NO_OPTION) {
-             // "No" button click, do nothing
-           } else if (response == JOptionPane.YES_OPTION) {
-             // "Yes" button clicked and go ahead with discarding this mission 
-        	   Transportable transportItem = (Transportable) incomingListPane.getIncomingList().getSelectedValue();
-               if (transportItem != null) {
-               	// call cancelTransportItem() in TransportManager Class to cancel the selected transport item.
-                   Simulation.instance().getTransportManager().cancelTransportItem(transportItem);
-               }
-           } else if (response == JOptionPane.CLOSED_OPTION) {
-        	   // Close the dialogbox, do nothing
-           }	  
-    }
+		// Unpause simulation.
+		desktop.getMainWindow().unpauseSimulation();
+	}
 
-    @Override
-    public void valueChanged(ListSelectionEvent evt) {
-        if (!evt.getValueIsAdjusting()) {
-            JList incomingList = (JList) evt.getSource();
-            Object selected = incomingList.getSelectedValue();
-            if (selected != null) {
-                // Incoming transport item is selected, 
-                // so enable modify and cancel buttons.
-                modifyButton.setEnabled(true);
-                cancelButton.setEnabled(true);
-            }
-            else {
-                // Incoming transport item is unselected,
-                // so disable modify and cancel buttons.
-                modifyButton.setEnabled(false);
-                cancelButton.setEnabled(false);
-            }
-        }
-    }
+	/**
+	 * Cancels the currently selected transport item.
+	 */
+	private void cancelTransportItem() {     
+		// 2014-10-04 by mkung -- added a dialog box asking the user to confirm "discarding" the mission
+		JDialog.setDefaultLookAndFeelDecorated(true);
+		int response = JOptionPane.showConfirmDialog(null, "Are you sure you want to discard the highlighted mission?", "Confirm",
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+		if (response == JOptionPane.NO_OPTION) {
+			// "No" button click, do nothing
+		} else if (response == JOptionPane.YES_OPTION) {
+			// "Yes" button clicked and go ahead with discarding this mission 
+			Transportable transportItem = (Transportable) incomingListPane.getIncomingList().getSelectedValue();
+			if (transportItem != null) {
+				// call cancelTransportItem() in TransportManager Class to cancel the selected transport item.
+				Simulation.instance().getTransportManager().cancelTransportItem(transportItem);
+			}
+		} else if (response == JOptionPane.CLOSED_OPTION) {
+			// Close the dialogbox, do nothing
+		}	  
+	}
 
-    /**
-     * Prepare this window for deletion.
-     */
-    public void destroy() {
-        incomingListPane.destroy();
-        arrivedListPane.destroy();
-        detailPane.destroy();
-    }
+	@Override
+	public void valueChanged(ListSelectionEvent evt) {
+		if (!evt.getValueIsAdjusting()) {
+			JList<?> incomingList = (JList<?>) evt.getSource();
+			Object selected = incomingList.getSelectedValue();
+			if (selected != null) {
+				// Incoming transport item is selected, 
+				// so enable modify and cancel buttons.
+				modifyButton.setEnabled(true);
+				cancelButton.setEnabled(true);
+			}
+			else {
+				// Incoming transport item is unselected,
+				// so disable modify and cancel buttons.
+				modifyButton.setEnabled(false);
+				cancelButton.setEnabled(false);
+			}
+		}
+	}
+
+	/**
+	 * Prepare this window for deletion.
+	 */
+	@Override
+	public void destroy() {
+		incomingListPane.destroy();
+		arrivedListPane.destroy();
+		detailPane.destroy();
+	}
 }
