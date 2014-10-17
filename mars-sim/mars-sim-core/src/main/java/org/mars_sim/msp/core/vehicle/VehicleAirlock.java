bb39b84d6164476d95eda7a8cbeeb58df5bdcc26
st-pa <st-pa@5831e110-d9be-4583-8d6f-8bfb9cd01620>
2014-10-17 10:11:15 +0000

    i18n

--- mars-sim/mars-sim-core/src/main/java/org/mars_sim/msp/core/vehicle/VehicleAirlock.java
+++ mars-sim/mars-sim-core/src/main/java/org/mars_sim/msp/core/vehicle/VehicleAirlock.java
@@ -4,7 +4,6 @@
  * @version 3.06 2014-04-26
  * @author Scott Davis
  */
-
 package org.mars_sim.msp.core.vehicle;
 
 import java.awt.geom.Point2D;
@@ -14,136 +13,135 @@ import org.mars_sim.msp.core.Airlock;
 import org.mars_sim.msp.core.Inventory;
 import org.mars_sim.msp.core.LifeSupport;
 import org.mars_sim.msp.core.LocalAreaUtil;
+import org.mars_sim.msp.core.Msg;
 import org.mars_sim.msp.core.person.LocationSituation;
 import org.mars_sim.msp.core.person.Person;
 
 /** 
- * The VehicleAirlock class represents an airlock for a vehicle.
+ * This class represents an airlock for a vehicle.
  */
-public class VehicleAirlock extends Airlock {
+public class VehicleAirlock
+extends Airlock {
 
-    /** default serial id. */
-    private static final long serialVersionUID = 1L;
-    
-    private static Logger logger = Logger.getLogger(VehicleAirlock.class.getName());
+	/** default serial id. */
+	private static final long serialVersionUID = 1L;
 
-    // Data members.
-    private Vehicle vehicle; // The vehicle this airlock is for.
-    private Point2D airlockInsidePos;
-    private Point2D airlockInteriorPos;
-    private Point2D airlockExteriorPos;
+	/** default logger. */
+	private static Logger logger = Logger.getLogger(VehicleAirlock.class.getName());
 
-    /**
-     * Constructor
-     * 
-     * @param vehicle the vehicle this airlock of for.
-     * @param capacity number of people airlock can hold.
-     */
-    public VehicleAirlock(Vehicle vehicle, int capacity, double xLoc, double yLoc, 
-            double interiorXLoc, double interiorYLoc, double exteriorXLoc, 
-            double exteriorYLoc) {
-        // User Airlock constructor
-        super(capacity);
+	// Data members.
+	/** The vehicle this airlock is for. */
+	private Vehicle vehicle;
+	private Point2D airlockInsidePos;
+	private Point2D airlockInteriorPos;
+	private Point2D airlockExteriorPos;
 
-        if (vehicle == null) {
-            throw new IllegalArgumentException("vehicle is null.");
-        }
-        else if (!(vehicle instanceof Crewable)) {
-            throw new IllegalArgumentException("vehicle not crewable.");
-        }
-        else if (!(vehicle instanceof LifeSupport)) {
-            throw new IllegalArgumentException("vehicle has no life support.");
-        }
-        else {
-            this.vehicle = vehicle;
-        }
-        
-        // Determine airlock interior position.
-        airlockInteriorPos = new Point2D.Double(interiorXLoc, interiorYLoc);
-        
-        // Determine airlock exterior position.
-        airlockExteriorPos = new Point2D.Double(exteriorXLoc, exteriorYLoc);
-        
-        // Determine airlock inside position.
-        airlockInsidePos = new Point2D.Double(xLoc, yLoc); 
-    }
+	/**
+	 * Constructor.
+	 * @param vehicle the vehicle this airlock of for.
+	 * @param capacity number of people airlock can hold.
+	 */
+	public VehicleAirlock(
+		Vehicle vehicle, int capacity, double xLoc, double yLoc, 
+		double interiorXLoc, double interiorYLoc, double exteriorXLoc, 
+		double exteriorYLoc
+	) {
+		// User Airlock constructor
+		super(capacity);
 
-    /**
-     * Causes a person within the airlock to exit either inside or outside.
-     *
-     * @param person the person to exit.
-     * @throws Exception if person is not in the airlock.
-     */
-    protected void exitAirlock(Person person) {
+		if (vehicle == null) {
+			throw new IllegalArgumentException(Msg.getString("VehicleAirlock.error.null")); //$NON-NLS-1$
+		}
+		else if (!(vehicle instanceof Crewable)) {
+			throw new IllegalArgumentException(Msg.getString("VehicleAirlock.error.notCrewable")); //$NON-NLS-1$
+		}
+		else if (!(vehicle instanceof LifeSupport)) {
+			throw new IllegalArgumentException(Msg.getString("VehicleAirlock.error.noLifeSupport")); //$NON-NLS-1$
+		}
+		else {
+			this.vehicle = vehicle;
+		}
 
-        if (inAirlock(person)) {
-            if (PRESSURIZED.equals(getState())) {
-                if (LocationSituation.OUTSIDE == person.getLocationSituation()) {
-                    // Exit person to inside vehicle.
-                    vehicle.getInventory().storeUnit(person);
-                }
-                else if (LocationSituation.BURIED != person.getLocationSituation()) {
-                    throw new IllegalStateException(person + " is entering " + getEntityName() + 
-                            " from an airlock but is not outside.");
-                }
-            }
-            else if (DEPRESSURIZED.equals(getState())) {
-                if (LocationSituation.IN_VEHICLE == person.getLocationSituation()) {
-                    // Exit person outside vehicle.  
-                    vehicle.getInventory().retrieveUnit(person);
-                }
-                else if (LocationSituation.BURIED != person.getLocationSituation()) {
-                    throw new IllegalStateException(person + " is exiting " + getEntityName() + 
-                            " from an airlock but is not inside.");
-                }
-            }
-            else {
-                logger.severe("Vehicle airlock in incorrect state for exiting: " + getState());
-            }
-        }
-        else {
-            throw new IllegalStateException(person.getName() + " not in airlock of " + getEntityName());
-        }
-    }
+		// Determine airlock interior position.
+		airlockInteriorPos = new Point2D.Double(interiorXLoc, interiorYLoc);
 
-    /**
-     * Gets the name of the entity this airlock is attached to.
-     *
-     * @return name
-     */
-    public String getEntityName() {
-        return vehicle.getName();
-    }
+		// Determine airlock exterior position.
+		airlockExteriorPos = new Point2D.Double(exteriorXLoc, exteriorYLoc);
 
-    /**
-     * Gets the inventory of the entity this airlock is attached to.
-     *
-     * @return inventory
-     */
-    public Inventory getEntityInventory() {
-        return vehicle.getInventory();
-    }
-    
-    @Override
-    public Object getEntity() {
-        return vehicle;
-    }
+		// Determine airlock inside position.
+		airlockInsidePos = new Point2D.Double(xLoc, yLoc); 
+	}
 
-    @Override
-    public Point2D getAvailableInteriorPosition() {
-        return LocalAreaUtil.getLocalRelativeLocation(airlockInteriorPos.getX(), 
-                airlockInteriorPos.getY(), vehicle);
-    }
+	/**
+	 * Causes a person within the airlock to exit either inside or outside.
+	 *
+	 * @param person the person to exit.
+	 * @throws Exception if person is not in the airlock.
+	 */
+	protected void exitAirlock(Person person) {
 
-    @Override
-    public Point2D getAvailableExteriorPosition() {
-        return LocalAreaUtil.getLocalRelativeLocation(airlockExteriorPos.getX(), 
-                airlockExteriorPos.getY(), vehicle);
-    }
+		if (inAirlock(person)) {
+			if (PRESSURIZED.equals(getState())) {
+				if (LocationSituation.OUTSIDE == person.getLocationSituation()) {
+					// Exit person to inside vehicle.
+					vehicle.getInventory().storeUnit(person);
+				}
+				else if (LocationSituation.BURIED != person.getLocationSituation()) {
+					throw new IllegalStateException(Msg.getString("VehicleAirlock.error.notOutside",person.getName(),getEntityName())); //$NON-NLS-1$
+				}
+			}
+			else if (DEPRESSURIZED.equals(getState())) {
+				if (LocationSituation.IN_VEHICLE == person.getLocationSituation()) {
+					// Exit person outside vehicle.  
+					vehicle.getInventory().retrieveUnit(person);
+				}
+				else if (LocationSituation.BURIED != person.getLocationSituation()) {
+					throw new IllegalStateException(Msg.getString("VehicleAirlock.error.notInside",person.getName(),getEntityName())); //$NON-NLS-1$
+				}
+			}
+			else {
+				logger.severe(Msg.getString("VehicleAirlock.error.badState",getState())); //$NON-NLS-1$
+			}
+		}
+		else {
+			throw new IllegalStateException(Msg.getString("VehicleAirlock.error.notInAirlock",person.getName(),getEntityName())); //$NON-NLS-1$
+		}
+	}
 
-    @Override
-    public Point2D getAvailableAirlockPosition() {
-        return LocalAreaUtil.getLocalRelativeLocation(airlockInsidePos.getX(), 
-                airlockInsidePos.getY(), vehicle);
-    }
+	/**
+	 * Gets the name of the entity this airlock is attached to.
+	 * @return name {@link String}
+	 */
+	public String getEntityName() {
+		return vehicle.getName();
+	}
+
+	/**
+	 * Gets the inventory of the entity this airlock is attached to.
+	 * @return inventory {@link Inventory}
+	 */
+	@Override
+	public Inventory getEntityInventory() {
+		return vehicle.getInventory();
+	}
+
+	@Override
+	public Object getEntity() {
+		return vehicle;
+	}
+
+	@Override
+	public Point2D getAvailableInteriorPosition() {
+		return LocalAreaUtil.getLocalRelativeLocation(airlockInteriorPos.getX(),airlockInteriorPos.getY(),vehicle);
+	}
+
+	@Override
+	public Point2D getAvailableExteriorPosition() {
+		return LocalAreaUtil.getLocalRelativeLocation(airlockExteriorPos.getX(),airlockExteriorPos.getY(),vehicle);
+	}
+
+	@Override
+	public Point2D getAvailableAirlockPosition() {
+		return LocalAreaUtil.getLocalRelativeLocation(airlockInsidePos.getX(),airlockInsidePos.getY(),vehicle);
+	}
 }
