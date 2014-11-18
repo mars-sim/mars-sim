2d8a0be35d45190ebde3dec59fe32d3d364b481a
Lars Næsbye Christensen <lars@naesbye.dk>
2014-11-18 21:44:22 +0000

    Now we import landmark type and origin strings for further sorting/filtering. Filtering not added yet.

--- mars-sim-core/src/main/java/org/mars_sim/msp/core/mars/Landmark.java
+++ mars-sim-core/src/main/java/org/mars_sim/msp/core/mars/Landmark.java
@@ -1,7 +1,7 @@
 /**
  * Mars Simulation Project
  * Landmark.java
- * @version 3.06 2014-01-29
+ * @version 3.07 2014-11-18
  * @author Dalen Kruse
  */
 
@@ -27,16 +27,26 @@ public class Landmark implements Serializable {
 	private Coordinates landmarkLocation;
 	/** Diameter of the landmark, rounded to int. */
 	private int landmarkDiameter;
+	/** Origin of the designation. */
+	private String landmarkOrigin;
+	/** Type of feature, e.g. Mons (MO). */
+	private String landmarkType;
 
+	
+	
+	
     /** Constructs a landmark object with the given name at the given location.
      *  @param name name of the landmark
      *  @param location location of the landmark
      */
-    public Landmark(String name, Coordinates location, int diameter) {
+    public Landmark(String name, Coordinates location, int diameter, String origin, String type) {
 
         this.landmarkName = name;
         this.landmarkLocation = location;
         this.landmarkDiameter = diameter;
+        this.landmarkOrigin = origin;
+        this.landmarkType = type;
+
     }
 
     /**
@@ -62,7 +72,23 @@ public class Landmark implements Serializable {
     public void setLandmarkDiameter(int diameter) {
         this.landmarkDiameter = diameter;
     }
-
+    
+    /**
+     *  Sets the origin.
+     *  @param origin origin of feature name
+     */
+    public void setLandmarkDiameter(String origin) {
+        this.landmarkOrigin = origin;
+    }
+    
+    /**
+     *  Sets the type.
+     *  @param type type of feature 
+     */
+    public void setLandmarkType(String type) {
+        this.landmarkType = type;
+    }
+       
     /**
      *  Gets the landmark name.
      *  @return name of the landmark
@@ -85,5 +111,21 @@ public class Landmark implements Serializable {
      */
     public int getLandmarkDiameter() {
         return landmarkDiameter;
+    }
+    
+    /**
+     *  Gets the origin.
+     *  @return origin of the landmark name
+     */
+    public String getLandmarkOrigin() {
+        return landmarkOrigin;
+    }
+    
+    /**
+     *  Gets the type.
+     *  @return type of the landmark
+     */
+    public String getLandmarkTyper() {
+        return landmarkType;
     }
 }
