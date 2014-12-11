15735b3e77f4f1a35eb323409828bb1e380c0067
manny kung <mannykung@gmail.com>
2014-12-11 00:54:17 +0000

    2014-12-10; (A.)  Added a button in notification menu setting that "Purge All Msgs in Queue Now"; (B.) Refactored NotificationWindow to safely remove excessive queue messages. Added a timer to hold off incoming messages for a few seconds while a user is changing notification display msg setting; (C.) Changed queue type from LinkedList to ConcurrentLinkedQueue in TelegraphQueue.java. (D.) Added crop/food item/food processes.
    2014-12-10; (A.)  Added a button in notification menu setting that "Purge All Msgs in Queue Now"; (B.) Refactored NotificationWindow to safely remove excessive queue messages. Added a timer to hold off incoming messages for a few seconds while a user is changing notification display msg setting; (C.) Changed queue type from LinkedList to ConcurrentLinkedQueue in TelegraphQueue.java. (D.) Added crop/food item/food processes.
    
    
    2014-12-10; (A.)  Added a button in notification menu setting that "Purge All Msgs in Queue Now"; (B.) Refactored NotificationWindow to safely remove excessive queue messages. Added a timer to hold off incoming messages for a few seconds while a user is changing notification display msg setting; (C.) Changed queue type from LinkedList to ConcurrentLinkedQueue in TelegraphQueue.java. (D.) Added crop/food item/food processes.

--- mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/notification/NotificationMenu.java
+++ mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/notification/NotificationMenu.java
@@ -10,11 +10,16 @@ package org.mars_sim.msp.ui.swing.notification;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JSeparator;
 import javax.swing.event.MenuEvent;
@@ -33,32 +38,37 @@ public class NotificationMenu implements ActionListener, MenuListener {
 	// 2014-12-04 Added notification related items
 	private JCheckBoxMenuItem medicalMenuItem;
 	private JCheckBoxMenuItem malfunctionMenuItem;	
+	private JCheckBoxMenuItem emptyMenuItem;
+	private JButton btn;
 	private JRadioButtonMenuItem threeMenuItem;
 	private JRadioButtonMenuItem twoMenuItem;
 	private JRadioButtonMenuItem oneMenuItem;
 	private JRadioButtonMenuItem showAllMenuItem ;
-	private JRadioButtonMenuItem showLastTwoMenuItem ;
+	private JRadioButtonMenuItem showLastThreeMenuItem ;
 	private JRadioButtonMenuItem showLastOneMenuItem ;
 	
 	private boolean showMedical = true;
 	private boolean showMalfunction = true;
+	private boolean isSetQueueToEmpty = false;
 	private int maxNumMsg = 99;
 	private int displayTime = 2;
 
+	private Timer timer;
 	/** The main window frame. */
 	private MainWindow mainWindow;
 	
 	public NotificationMenu(MainWindowMenu mainWindowMenu) {
 		this.mainWindowMenu = mainWindowMenu;
 		this.mainWindow = mainWindowMenu.getMainWindow();
-
-		
+	
 		JMenu notificationMenu = new JMenu(Msg.getString("mainMenu.notification")); //$NON-NLS-1$
 		notificationMenu.addActionListener(this);
 		notificationMenu.setMnemonic(KeyEvent.VK_N);
 		//notificationMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_DOWN_MASK, false));
 		notificationMenu.setToolTipText(Msg.getString("mainMenu.tooltip.notification")); //$NON-NLS-1$
 		mainWindowMenu.add(notificationMenu);
+		
+		//notificationMenu.add(new JSeparator());
 		
 		JMenu typeItem = new JMenu("Message Type");
 		notificationMenu.add(typeItem);
@@ -72,7 +82,6 @@ public class NotificationMenu implements ActionListener, MenuListener {
 		malfunctionMenuItem.setSelected(true);
 		malfunctionMenuItem.addActionListener(this);
 		typeItem.add(malfunctionMenuItem);
-		
 
 		//notificationMenu.addSeparator();
 			
@@ -104,7 +113,6 @@ public class NotificationMenu implements ActionListener, MenuListener {
 		
 		JMenu queueItem = new JMenu("Queue Size");
 		notificationMenu.add(queueItem);
-
 		
 		ButtonGroup group2 = new ButtonGroup();
 		showAllMenuItem = new JRadioButtonMenuItem("Unlimited");
@@ -113,22 +121,42 @@ public class NotificationMenu implements ActionListener, MenuListener {
 		showAllMenuItem.addActionListener(this);
 		queueItem.add(showAllMenuItem);
 
-		showLastTwoMenuItem = new JRadioButtonMenuItem("Two");
-		showLastTwoMenuItem.setSelected(true);
-		group2.add(showLastTwoMenuItem);
-		showLastTwoMenuItem.addActionListener(this);
-		queueItem.add(showLastTwoMenuItem);
+		showLastThreeMenuItem = new JRadioButtonMenuItem("Three");
+		showLastThreeMenuItem.setSelected(true);
+		group2.add(showLastThreeMenuItem);
+		showLastThreeMenuItem.addActionListener(this);
+		queueItem.add(showLastThreeMenuItem);
 
 		showLastOneMenuItem = new JRadioButtonMenuItem("One");
 		//lastOne.setSelected(true);
 		group2.add(showLastOneMenuItem);
 		showLastOneMenuItem.addActionListener(this);
 		queueItem.add(showLastOneMenuItem);
-
-		notificationMenu.add(new JSeparator());
 		
-		JMenuItem noteItem = new JMenuItem("<html><i><h5>Note: Expect delay for changes to take effect</h5></i></html>");
+		notificationMenu.add(new JSeparator());	
+		
+		// 2014-12-10 Added Empty Queue option
+		JButton b = new JButton(
+				"<html><font size=-4>Purge</font></html>");
+		JPanel p = new JPanel();
+		JLabel l = new JLabel("<html><font size=-2><font color=red> All Msgs in Queue Now</font></font></html>");
+		l.setOpaque(false);
+		p.setOpaque(false);
+		p.add(b);
+		p.add(l);
+			b.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent evt) {
+		    	isSetQueueToEmpty = true;
+		    }
+		});
+		
+		notificationMenu.add(p);
+		
+		notificationMenu.add(new JSeparator());
+		JMenuItem noteItem = new JMenuItem("<html><font size=-2>"
+		+ "Note: May Take Time for <br>Changes to Take Effect</font></html>");
 		notificationMenu.add(noteItem);
+
 
 	}
 	
@@ -148,6 +176,22 @@ public class NotificationMenu implements ActionListener, MenuListener {
 	public boolean getShowMalfunction() {
 		return showMalfunction;
 	}
+	
+	/**
+	 * Gets the value of isQueueEmpty.
+	 * @return isQueueEmpty
+	 */
+	public boolean getIsSetQueueEmpty() {
+		boolean result = false;
+		if (isSetQueueToEmpty) {
+			result = true;
+			//emptyMenuItem.setSelected(false);
+			// reset it back to false once it calls this method
+			isSetQueueToEmpty = false;
+		}
+		return result; 
+	}
+	
 	
 	/**
 	 * Gets the value of maxNumMsg.
@@ -171,6 +215,7 @@ public class NotificationMenu implements ActionListener, MenuListener {
 	/** ActionListener method overriding. */
 	@Override
 	public final void actionPerformed(ActionEvent event) {
+		
 		JMenuItem selectedItem = (JMenuItem) event.getSource();
 		//MainDesktopPane desktop = mainWindow.getDesktop();
 
@@ -197,9 +242,38 @@ public class NotificationMenu implements ActionListener, MenuListener {
 		if (selectedItem ==  twoMenuItem) displayTime = 2;
 		if (selectedItem ==  oneMenuItem) displayTime = 1;
 		if (selectedItem ==  showAllMenuItem) maxNumMsg = 99;
-		if (selectedItem ==  showLastTwoMenuItem) maxNumMsg = 2;
+		if (selectedItem ==  showLastThreeMenuItem) maxNumMsg = 3;
 		if (selectedItem ==  showLastOneMenuItem) maxNumMsg = 1;
+		/*
+		if (selectedItem ==  emptyMenuItem) {
+			//System.out.println("selectedItem ==  emptyMenuItem");
+			if (emptyMenuItem.isSelected()) {
+				//System.out.println("isSelected() = true");
+				// doClick(int pressTime)
+				//emptyMenuItem.setText("<html><i><font size=-2><font color=red>Purging...</font></font></i></html>");
+				isSetQueueToEmpty = true;
+				// wait for a few second
+				// reset it to default
+				//int seconds = 5;
+				//timer = new Timer();
+				//timer = new Timer(displayTime, mainWindowMenu);
+				//timer.schedule(new CancelTask(), seconds * 1000);			
+				//isSetQueueToEmpty = false;
+				//emptyMenuItem.setText("<html><font size=-2><font color=red>Purge Message Queue Now</font></font></html>");
+				//emptyMenuItem.setSelected(false);
+			} else {
+				System.out.println(".isSelected()= false");
+				isSetQueueToEmpty = false;
+			}
+		} // end of if (selectedItem ==  emptyMenuItem)
+		*/
+	} // end of public final void actionPerformed(ActionEvent event) 
 		
+	public class CancelTask extends TimerTask {
+		public void run() {
+			System.out.println("Time's up!");
+			timer.cancel(); // Terminate the thread
+		}
 	}
 	
 	/** MenuListener method overriding. */
