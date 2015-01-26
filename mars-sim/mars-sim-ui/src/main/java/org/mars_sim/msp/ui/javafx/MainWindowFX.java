153f00b10e5ae5b885e1c3e06a9b744f7643fcfc
manny kung <mannykung@gmail.com>
2015-01-26 18:02:06 +0000

    2015-01-26 Testing the use of JFXPanel in MainWindowFX.java for embedding JavaFX object in Swing based environment.

--- mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/javafx/MainWindowFX.java
+++ mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/javafx/MainWindowFX.java
@@ -12,6 +12,15 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
 import javafx.scene.text.Font;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 /**
  * The MainWindowFX class is the primary JavaFX frame for the project.
@@ -24,7 +33,7 @@ public class MainWindowFX {
 
 	/**
 	 * Constructor.
-	 */
+	 
 	public MainWindowFX() {
         Text text = new Text(10, 40, "Mars Simulation Project "+WINDOW_TITLE);
         text.setFont(new Font(40));
@@ -37,4 +46,52 @@ public class MainWindowFX {
         stage.show(); 
 
 	}
+	*/
+
+    private static void initAndShowGUI() {
+        // This method is invoked on the EDT thread
+        JFrame frame = new JFrame("Mars Simulation Project " + WINDOW_TITLE);
+        final JFXPanel fxPanel = new JFXPanel();
+        frame.add(fxPanel);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                initFX(fxPanel);
+            }
+       });
+    }
+
+    private static void initFX(JFXPanel fxPanel) {
+        // This method is invoked on the JavaFX thread
+        Scene scene = createScene();
+        fxPanel.setScene(scene);
+    }
+
+    private static Scene createScene() {
+        Group  root  =  new  Group();
+        Scene  scene  =  new  Scene(root, Color.ALICEBLUE);
+        Text  text  =  new  Text();
+        
+        text.setX(40);
+        text.setY(100);
+        text.setFont(new Font(25));
+        text.setText("JavaFX MSP testbed!");
+
+        root.getChildren().add(text);
+
+        return (scene);
+    }
+    
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                initAndShowGUI();
+            }
+        });
+    }
 }
