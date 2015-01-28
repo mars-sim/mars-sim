3e05db7bc67058430ebeb2a8416e2c52fb3c6ec6
Lars Næsbye Christensen <lars@naesbye.dk>
2015-01-28 22:40:03 +0000

    Made three Scenes to be our three 'modes' (can't switch between them for now). Also added two font families I intend to build the UI from (Jura for big type sci fi lettering, Noto Sans for common UI elements). These are of course referenced by the stylesheets.

---
+++ mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/javafx/MainScene.java
@@ -0,0 +1,34 @@
+/**
+ * Mars Simulation Project
+ * MainWindowFX.java
+ * @version 3.08 2015-01-28
+ * @author Lars Næsbye Christensen
+ */
+
+package org.mars_sim.msp.ui.javafx;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class MainScene {
+
+    public Scene createMainScene() {
+        Group  root  =  new  Group();
+        Scene  scene  =  new  Scene(root);
+		scene.getStylesheets().addAll("/fxui/css/mainskin.css");		
+
+        MainWindowFXMenu menuBar = new MainWindowFXMenu(null);
+        ImageView bg1 = new ImageView();
+        bg1.setImage(new Image("/images/background.png"));        
+        root.getChildren().add(bg1);
+
+        root.getChildren().add(menuBar); 
+//        root.getChildren().add(new Button("Press me to go to Settlement Scene"));
+
+        return (scene);
+    }
+	
+	
+}
