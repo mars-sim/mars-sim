ef75a7697cb8bdbad2b7ca91120ae353caa0fbae
manny kung <mannykung@gmail.com>
2015-03-21 03:56:05 +0000

    2015-03-20d Attempts in speeding up the day-night effect in Settlement Map Tool.

--- mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/tool/settlement/DayNightMapLayer.java
+++ mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/tool/settlement/DayNightMapLayer.java
@@ -16,21 +16,25 @@ import org.mars_sim.msp.core.structure.Settlement;
 import org.mars_sim.msp.core.structure.building.Building;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.awt.image.MemoryImageSource;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+import javax.swing.SwingUtilities;
 
 /**
  * The DayNightMapLayer is a graphics layer to display twilight and night time shading of the settlement
  */
 public class DayNightMapLayer implements SettlementMapLayer {
-    
+
     private static String CLASS_NAME = "org.mars_sim.msp.ui.swing.tool.settlement.ShadingMapLayer";
 	
     private static Logger logger = Logger.getLogger(CLASS_NAME);
      
     private int[] shadingArray;
-    
+   
     private SurfaceFeatures surfaceFeatures;
 	private SettlementMapPanel mapPanel;
 
@@ -46,75 +50,85 @@ public class DayNightMapLayer implements SettlementMapLayer {
 			Building building, double xPos, double yPos, int mapWidth,
 			int mapHeight, double rotation, double scale) {
 
-		boolean isOn = mapPanel.isShowDayNightLayer();
-		
-		if (isOn) {
+
+		if (mapPanel.isShowDayNightLayer()) {
+			int width = SettlementWindow.HORIZONTAL;
+			int height = SettlementWindow.VERTICAL;
+
 			// Get the map center point.
-	        int centerX =  mapWidth / 2;
-	        int centerY =  mapHeight / 2;
-			//Dimension parentSize = mapPanel.getSettlementWindow().getDesktop().getSize();
-	        //Insets insets = mapPanel.getSettlementWindow().getDesktop().getInsets();
-	        //int width = parentSize.width - (insets.left + insets.right);
-	        //int height = parentSize.height - (insets.top + insets.bottom);
-			int width = 2500; //1920;
-			int height = 1500; //1080;
-
-			Coordinates mapCenter = mapPanel.getSettlement().getCoordinates();		
-
+	        int centerX =  width / 2;
+	        int centerY =  height / 2;
 			
 	        shadingArray = new int[width * height];
-	        
+
 	        // Coordinates sunDirection = orbitInfo.getSunDirection();
-
-	        double rho =  (double) height / Math.PI;
-
+	        double rho =  (double) height * 3 / Math.PI; // the arc needs to be at least 3 times the height to be rendered realistically
+	        // TODO: should take into consideration the direction of the sun when rendering the direction of the "normal" vector of the shadow wavefront
+	        
 	        boolean nightTime = true;
 	        boolean dayTime = true;
 	        Coordinates location = new Coordinates(0D, 0D);
-	        for (int x = 0; x <width; x+=2) {
-	            for (int y = 0; y <height; y+=2) {
-	                mapCenter.convertRectToSpherical(x - centerX, y - centerY, rho, location);
+	        
+	        // TODO: make the sahdingArray sensitive to the zooming in and out
+        
+	        for (int x = 0; x < width; x += 2) {
+	        	
+	            for (int y = 0; y < height; y += 2) {
+	            	
+	            	mapPanel.getSettlement().getCoordinates().convertRectToSpherical(x - centerX, y - centerY, rho, location);
 	                double sunlight = surfaceFeatures.getSurfaceSunlight(location);
 	                int sunlightInt = (int) (127 * sunlight);
 	                int shadeColor = ((127 - sunlightInt) << 24) & 0xFF000000;
 	               
-	                shadingArray[x + (y *width)] = shadeColor;
-	                shadingArray[x + 1 + (y *width)] = shadeColor;
-	                if (y <height -1) {
-	                    shadingArray[x + ((y + 1) *width)] = shadeColor;
-	                    shadingArray[x + 1 + ((y + 1) *width)] = shadeColor;
+	                shadingArray[x + (y * width)] = shadeColor;
+	                shadingArray[x + 1 + (y * width)] = shadeColor;
+	                
+	                if (y < height - 1) {
+	                    shadingArray[x + ((y + 1) * width)] = shadeColor;
+	                    shadingArray[x + 1 + ((y + 1) * width)] = shadeColor;
 	                }
 	       
 	                if (sunlight > 0) nightTime = false;
 	                if (sunlight < 127) dayTime = false;
 	            }
 	        }
-	        
+	            
 	        if (nightTime) {
 	            g2d.setColor(new Color(0, 0, 0, 128));
-	            g2d.fillRect(0, 0,width,height);
+	            g2d.fillRect(0, 0, width, height);
 	        }
 	        else if (!dayTime) {
 	            // Create shading image for map
 	            Image shadingMap = mapPanel.createImage(
-	            	new MemoryImageSource(width,height,shadingArray, 0,width));
+	            	new MemoryImageSource(width, height, shadingArray, 0, width));
+	            // TODO : use BufferedImage instead of MemoryImageSource. which is faster ?            
+	            //BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_USHORT_GRAY);
 
 	            MediaTracker mt = new MediaTracker(mapPanel);
 	            mt.addImage(shadingMap, 0);
+	            
+	            //Thread t = new Thread(this);            
+	            //t.start();
+
+		        // TODO: what is causing the slowness? the for-loop algorithm ?
+	        	
 	            try {
 	                mt.waitForID(0);
+	    	        //System.out.println("mt.waitForID(0)");	     
+	                Thread.sleep(40);
 	            }
 	            catch (InterruptedException e) {
 	                logger.log(Level.SEVERE,"ShadingMapLayer interrupted: " + e);
+	                mt.removeImage(shadingMap, 0);
 	            }
-
-	            // Draw the shading image
-	            g2d.drawImage(shadingMap, 0, 0, mapPanel);
+	            if (!mt.isErrorID(0) )
+	            	// Draw the shading image
+	            	g2d.drawImage(shadingMap, 0, 0, mapPanel);
+	            
 	        }
-	        
+
 		}
-		else
-			;
+
 	}
 
 	@Override
