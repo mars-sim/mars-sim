1498383cf5c6204d8f690d896f4a10c4b5da983a
Lars Næsbye Christensen <lars@naesbye.dk>
2015-04-18 16:51:24 +0000

    Removed WAV versions of sound files. Changed the sound constants accordingly.

--- mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/sound/SoundConstants.java
+++ mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/swing/sound/SoundConstants.java
@@ -16,26 +16,26 @@ public interface SoundConstants {
 	public final static String SOUNDS_ROOT_PATH = "audio/";
 
 	// Unit window sounds for rovers.
-	public final static String SND_ROVER_MOVING = "rover_moving.wav";
-	public final static String SND_ROVER_MALFUNCTION = "rover_malfunction.wav";
-	public final static String SND_ROVER_MAINTENANCE = "rover_maintenance.wav";
+	public final static String SND_ROVER_MOVING = "rover_moving.ogg";
+	public final static String SND_ROVER_MALFUNCTION = "rover_malfunction.ogg";
+	public final static String SND_ROVER_MAINTENANCE = "rover_maintenance.ogg";
 	public final static String SND_ROVER_PARKED = "";
 
 	/** Unit window sound for settlements. */
-	public final static String SND_SETTLEMENT = "settlement.wav";
+	public final static String SND_SETTLEMENT = "settlement.ogg";
 
 	/** Sound for splash. */
-	public final static String SND_SPLASH = "splash.wav";
+	public final static String SND_SPLASH = "splash.ogg";
 
 	// Unit window sounds for people.
-	public final static String SND_PERSON_FEMALE1 = "female_person1.wav";
-	public final static String SND_PERSON_FEMALE2 = "female_person2.wav";
-	public final static String SND_PERSON_MALE1 = "male_person1.wav";
-	public final static String SND_PERSON_MALE2 = "male_person2.wav";
-	public final static String SND_PERSON_DEAD = "person_dead.wav";
+	public final static String SND_PERSON_FEMALE1 = "female_person1.ogg";
+	public final static String SND_PERSON_FEMALE2 = "female_person2.ogg";
+	public final static String SND_PERSON_MALE1 = "male_person1.ogg";
+	public final static String SND_PERSON_MALE2 = "male_person2.ogg";
+	public final static String SND_PERSON_DEAD = "person_dead.ogg";
 
 	/** Unit window sounds for equipment. */
-	public final static String SND_EQUIPMENT = "equipment.wav";
+	public final static String SND_EQUIPMENT = "equipment.ogg";
 
 	// Supported sound formats
 	public final static String SND_FORMAT_WAV  = ".wav";
