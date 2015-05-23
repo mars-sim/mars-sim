cdeff6588fd6663969eab15a078aca9386b77c97
manny kung <mannykung@gmail.com>
2015-05-23 23:39:54 +0000

    2015-05-23 (1). Replaced the Eager Initialization Singleton Pattern with Bill Pugh Singleton Pattern (using a inner static helper class called SingletonHelper) in Simulation.java.
    (2). Added readResolve() to prevent the singleton pattern from being destroyed at the time of serialization
    
    See reference : http://www.journaldev.com/1377/java-singleton-design-pattern-best-practices-with-examples
    
    (3). Added new class Training.java, TrainingType.java, and HealthRisk.java (not implemented yet).

---
+++ mars-sim/mars-sim-core/src/main/java/org/mars_sim/msp/core/person/Training.java
@@ -0,0 +1,24 @@
+/**
+ * Mars Simulation Project
+ * Training.java
+ * @version 3.08 2015-05-22
+ * @author Manny Kung
+ */
+
+
+package org.mars_sim.msp.core.person;
+
+public class Training {
+
+	Person person;
+	String trainingName;
+	String area;
+	int score;
+	int like;
+
+	public Training(Person person, String trainingName, String area, int score, int like) {
+
+
+	}
+
+}
