7027b51c3e11fb705d7009ea824e7d51d2113cf3
manny kung <mannykung@gmail.com>
2015-06-12 00:54:48 +0000

    2015-06-11 Added mavenized version of jME3 (3.0.10) in mars-sim-ui's pom.xml
    (1).  Created a new source folder called org.mars_sim.msp.ui.jme3 inside mars-sim-ui 
    
    (2).  Temporarily embedded a few jME3 tutorial class files for testing jME3 codes in msp development environment. 
     
    (3).  Temporarily embedded some resource assets (Textures, Materials, Models, Scenes, Interfaces,..,etc.) for test-runing those tutorial files iin mars-sim-ui's resources folder  
    
    NOTE: most of these assets can be deleted before the next release if not used in msp.

---
+++ mars-sim/mars-sim-ui/src/main/java/org/mars_sim/msp/ui/jme3/HelloJME3.java
@@ -0,0 +1,31 @@
+package org.mars_sim.msp.ui.jme3;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.material.Material;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
+import com.jme3.math.ColorRGBA;
+
+/** Sample 1 - how to get started with the most simple JME 3 application.
+ * Display a blue 3D cube and view from all sides by
+ * moving the mouse and pressing the WASD keys. */
+public class HelloJME3 extends SimpleApplication {
+
+	String UNSHADED = "Common/MatDefs/Misc/Unshaded.j3md";
+
+    public static void main(String[] args){
+    	HelloJME3 app = new HelloJME3();
+        app.start(); // start the game
+    }
+
+    @Override
+    public void simpleInitApp() {
+        Box b = new Box(1, 1, 1); // create cube shape
+        Geometry geom = new Geometry("Box", b);  // create cube geometry from the shape
+        Material mat = new Material(assetManager, UNSHADED);  // create a simple material
+        mat.setColor("Color", ColorRGBA.Blue);   // set color of material to blue
+        geom.setMaterial(mat);                   // set the cube's material
+        rootNode.attachChild(geom);              // make the cube appear in the scene
+    }
+}
