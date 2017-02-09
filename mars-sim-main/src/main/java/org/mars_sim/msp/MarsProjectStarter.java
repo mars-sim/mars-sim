1e6846e5ff0b813c171ff065efe388ad99d01301
manny kung <mannykung@gmail.com>
2017-02-09 17:08:05 +0000

    2017-02-09 Fix minor CSS errors

--- mars-sim-main/src/main/java/org/mars_sim/msp/MarsProjectStarter.java
+++ mars-sim-main/src/main/java/org/mars_sim/msp/MarsProjectStarter.java
@@ -19,16 +19,20 @@ import java.util.List;
  */
 public class MarsProjectStarter {
 
-	
+
     public static void main(String[] args) {
-    	
+
         StringBuilder command = new StringBuilder();
-			
+
         String javaHome = System.getenv("JAVA_HOME");
         if (javaHome != null) {
-            if (javaHome.contains(" ")) javaHome = "\"" + javaHome;
+            if (javaHome.contains(" "))
+            	javaHome = "\"" + javaHome;
+
             command.append(javaHome).append(File.separator).append("bin").append(File.separator).append("java");
-            if (javaHome.contains(" ")) command.append("\"");
+
+            if (javaHome.contains(" "))
+            	command.append("\"");
         }
         else command.append("java");
 
@@ -36,27 +40,30 @@ public class MarsProjectStarter {
         //command.append(" -Dswing.plaf.metal.controlFont=Tahoma"); // the compiled jar won't run
         //command.append(" -Dswing.plaf.metal.userFont=Tahoma"); // the compiled jar won't run
         //command.append(" -generateHelp");
-        //command.append(" -new");   
-        
-        command.append(" -Djava.util.logging.config.file=logging.properties");
-        command.append(" -cp .").append(File.pathSeparator);
-        command.append("*").append(File.pathSeparator);
-        command.append("jars").append(File.separator).append("*");
-        //command.append(" org.mars_sim.msp.MarsProject");
-        command.append(" org.mars_sim.msp.javafx.MarsProjectFX");
+        //command.append(" -new");
+
+        command.append(" -Djava.util.logging.config.file=logging.properties")
+        	.append(" -cp .")
+        	.append(File.pathSeparator)
+        	.append("*")
+        	.append(File.pathSeparator)
+        	.append("jars")
+        	.append(File.separator)
+        	.append("*")
+        	.append(" org.mars_sim.msp.javafx.MarsProjectFX"); // OR .append(" org.mars_sim.msp.MarsProject");
 
         // 2016-05-28 Added checking for input args
         List<String> argList = Arrays.asList(args);
-        
+
         if (argList.isEmpty()) {
         	// by default, use gui and 1GB
             command.append(" -Xms256m");
             command.append(" -Xmx1024m");
         	command.append(" -new");
         }
-        
-        else { 
-        	
+
+        else {
+
 	        if (argList.contains("5")) {// || argList.contains("5 ")) {
 	            command.append(" -Xms256m");
 	            command.append(" -Xmx2048m");
@@ -68,46 +75,46 @@ public class MarsProjectStarter {
 	        else if (argList.contains("3")) {
 	            command.append(" -Xms256m");
 	            command.append(" -Xmx1024m");
-	        }        	
+	        }
 	        else if (argList.contains("2")) {
 	            command.append(" -Xms256m");
 	            command.append(" -Xmx768m");
-	        }   
+	        }
 	        else if (argList.contains("1")) {
 	            command.append(" -Xms256m");
 	            command.append(" -Xmx512m");
-	        }    
+	        }
 	        else if (argList.contains("0")) {
 	            command.append(" -Xms256m");
 	            command.append(" -Xmx1024m");
-	        }    
+	        }
 	        else {
 	        	//  use 1GB by default
 	            command.append(" -Xms256m");
 	            command.append(" -Xmx1024m");
 	        }
-	        
+
 	        if (argList.contains("help")) {
 	        	command.append(" -help");
 	        	//System.out.println(manpage);
 	        }
-	        
+
 	        else if (argList.contains("html")) {
-	        	//command.append(" -new");	        	
+	        	//command.append(" -new");
 	        	command.append(" -html");
 	        }
-	        
+
 	        else {
-	        
+
 		        if (argList.contains("headless"))
 		        	command.append(" -headless");
-		        
+
 		        if (argList.contains("new"))
 		        	command.append(" -new");
-		        	
+
 		        else if (argList.contains("load")) {
 		        	command.append(" -load");
-		        
+
 		        	// 2016-10-06 Appended the name of loadFile to the end of the command stream so that MarsProjectFX can read it.
 		        	int index = argList.indexOf("load");
 		        	int size = argList.size();
@@ -118,19 +125,19 @@ public class MarsProjectStarter {
 		        		command.append(" " + fileName);
 		        	}
 		        }
-		        
+
 		        else {
 		        	//System.out.println("Note: it's missing 'new' or 'load'. Assume you want to start a new sim now.");
 		        	command.append(" -new");
 		        }
-	        
+
 	        }
 
-        }     
-        
+        }
+
         String commandStr = command.toString();
         System.out.println("Command: " + commandStr);
-        
+
         try {
             Process process = Runtime.getRuntime().exec(commandStr);
 
@@ -147,7 +154,7 @@ public class MarsProjectStarter {
             // Close stream consumers.
             errorConsumer.join();
             outputConsumer.join();
-            
+
         } catch (IOException e) {
             throw new IllegalStateException(e);
         } catch (InterruptedException e) {
