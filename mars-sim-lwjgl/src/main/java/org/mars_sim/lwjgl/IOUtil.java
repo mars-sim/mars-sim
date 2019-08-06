e5c373e6ee31899cf6b80111b94cba775ac8c0d4
mokun <mannykung@gmail.com>
2019-08-05 22:06:21 -0700

    Test LWJGL. Fix Maven compile
    r4959
    2019-08-05
    
    ## DISCUSSION
    1. https://github.com/mars-sim/mars-sim/issues/187
    
    ## NEW
    1. Add lwjgl maven submodule for testing.
       - Test the use of nuklear UI.
         - Run GLFWDemo to construct a calculator in a glfw window.
    
    ## CHANGES
    1. Fix compiling under Maven
       - Revert back maven-compiler-plugin to 3.8.0 for
         compatibility reason.

---
+++ mars-sim-lwjgl/src/main/java/org/mars_sim/lwjgl/IOUtil.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+package org.mars_sim.lwjgl;
+
+import org.lwjgl.*;
+
+import java.io.*;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.file.*;
+
+import static org.lwjgl.BufferUtils.*;
+
+public final class IOUtil {
+
+    private IOUtil() {
+    }
+
+    private static ByteBuffer resizeBuffer(ByteBuffer buffer, int newCapacity) {
+        ByteBuffer newBuffer = BufferUtils.createByteBuffer(newCapacity);
+        buffer.flip();
+        newBuffer.put(buffer);
+        return newBuffer;
+    }
+
+    /**
+     * Reads the specified resource and returns the raw data as a ByteBuffer.
+     *
+     * @param resource   the resource to read
+     * @param bufferSize the initial buffer size
+     *
+     * @return the resource data
+     *
+     * @throws IOException if an IO error occurs
+     */
+    public static ByteBuffer ioResourceToByteBuffer(String resource, int bufferSize) throws IOException {
+        ByteBuffer buffer;
+
+        Path path = Paths.get(resource);
+        if (Files.isReadable(path)) {
+            try (SeekableByteChannel fc = Files.newByteChannel(path)) {
+                buffer = BufferUtils.createByteBuffer((int)fc.size() + 1);
+                while (fc.read(buffer) != -1) {
+                    ;
+                }
+            }
+        } else {
+            try (
+                InputStream source = IOUtil.class.getClassLoader().getResourceAsStream(resource);
+                ReadableByteChannel rbc = Channels.newChannel(source)
+            ) {
+                buffer = createByteBuffer(bufferSize);
+
+                while (true) {
+                    int bytes = rbc.read(buffer);
+                    if (bytes == -1) {
+                        break;
+                    }
+                    if (buffer.remaining() == 0) {
+                        buffer = resizeBuffer(buffer, buffer.capacity() * 3 / 2); // 50%
+                    }
+                }
+            }
+        }
+
+        buffer.flip();
+        return buffer;
+    }
+
+}
