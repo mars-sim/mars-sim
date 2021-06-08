92966b2fc272c74ddc57c192567dd6dc52ae7420
bevans2000 <20720166+bevans2000@users.noreply.github.com>
2021-06-08 21:38:26 +0100

    Correct activity spots that fall outside Building
    Also BuildingConfig checks that the activity spots are within the bounds
    of the Building when loading the xml. In not the loading ia aborted.
    
    fixes#358

--- mars-sim-core/src/main/java/org/mars_sim/msp/core/science/ScientificStudyManager.java
+++ mars-sim-core/src/main/java/org/mars_sim/msp/core/science/ScientificStudyManager.java
@@ -54,14 +54,16 @@ public class ScientificStudyManager // extends Thread
 			throw new IllegalArgumentException("Science cannot be null");
 		if (difficultyLevel < 0)
 			throw new IllegalArgumentException("difficultyLevel must be positive value");
-		int id = studies.size() + 1;
-		String name = science.getName() + " #" + id;
-		ScientificStudy study = new ScientificStudy(id, name, researcher, science, difficultyLevel);
+		
+		ScientificStudy study = null;
 		synchronized (studies) {
+			int id = studies.size() + 1;
+			String name = science.getName() + " #" + id;
+			study = new ScientificStudy(id, name, researcher, science, difficultyLevel);
 			studies.add(study);
 		}
 
-		logger.fine(researcher.getName() + " began writing proposal for " + name);
+		logger.fine(researcher.getName() + " began writing proposal for " + study.getName());
 
 		return study;
 	}
