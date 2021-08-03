77d18b4bb911f934cc1d44233b6ec8619a4e78a2
Barry Evans <20720166+bevans2000@users.noreply.github.com>
2021-08-03 21:32:44 +0100

    Tidy up the GovernanceConfig XML parsing
    #363

--- mars-sim-core/src/main/java/org/mars_sim/msp/core/reportingAuthority/MissionAgenda.java
+++ mars-sim-core/src/main/java/org/mars_sim/msp/core/reportingAuthority/MissionAgenda.java
@@ -14,6 +14,10 @@ import java.util.List;
 import org.mars_sim.msp.core.logging.SimLogger;
 import org.mars_sim.msp.core.person.ai.task.utils.Worker;
 
+/**
+ * Mission agenda for a ReportingAuthority to follow. It defines a set
+ * of SubAgendas that specific actual targets for the RA.
+ */
 public class MissionAgenda implements Serializable {
 	/** default serial id. */
 	private static final long serialVersionUID = 1L;
@@ -35,18 +39,34 @@ public class MissionAgenda implements Serializable {
 		this.samples = samples;
 	}
 
+	/**
+	 * Get the agendas for this mission.
+	 * @return
+	 */
 	public List<MissionSubAgenda> getAgendas() {
 		return subs;
 	}
 
+	/**
+	 * Get the overall objective.
+	 * @return
+	 */
 	public String getObjectiveName() {
 		return objective;
 	}
 
+	/**
+	 * Reporting some findings by a Worker. This may adjust charactirtics of the RA.
+	 * @param unit
+	 */
 	public void reportFindings(Worker unit) {
 		logger.info(unit, 20_000L, name + ":" + findings);
 	}
 
+	/**
+	 * Gather some samples as part of this agenda.
+	 * @param unit
+	 */
 	public void gatherSamples(Worker unit) {
 		logger.info(unit, 20_000L, name + ":" + samples);
 	}
