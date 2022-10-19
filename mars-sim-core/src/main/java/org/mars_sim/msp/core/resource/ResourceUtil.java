68eb4f52e553d653c565252273ca55297e4ff6fb
bevans2000 <20720166+bevans2000@users.noreply.github.com>
2022-10-19 21:33:57 +0100

    Change the names of items in the XML configuration files.
    Part of #723

--- mars-sim-core/src/main/java/org/mars_sim/msp/core/resource/ResourceUtil.java
+++ mars-sim-core/src/main/java/org/mars_sim/msp/core/resource/ResourceUtil.java
@@ -15,15 +15,12 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Objects;
 import java.util.Set;
 
 import org.mars_sim.msp.core.SimulationConfig;
 import org.mars_sim.msp.core.food.Food;
 import org.mars_sim.msp.core.food.FoodUtil;
 import org.mars_sim.msp.core.logging.SimLogger;
-import org.mars_sim.msp.core.tool.Conversion;
 
 public class ResourceUtil implements Serializable {
 
