1b092a7e7cad8aa3058d04637efa15c335c740f7
mokun <mannykung@gmail.com>
2022-10-25 12:13:37 -0700

    Redo batch files
    10.25.2022
    
    - change: rework batch files

--- mars-sim-headless/src/main/resources/mars-sim.bat
+++ mars-sim-headless/src/main/resources/mars-sim.bat
@@ -1,6 +1,6 @@
-:: Please replace the [$CURRENT_VERSION] with the correct build version in your jarfile 
-:: e.g. [$CURRENT_VERSION] such as 3.4.0 or a build number such as 7545
+:: This batch file will run the command "java -jar ./[$VERSION_OR_BUILD]_headless_java11.jar new" 
+:: Ensure you replace the [$VERSION_OR_BUILD] with the correct version (such as 3.4.0) or build number (such as 7553) in the jarfile
 @echo off
-call java -jar ./[$CURRENT_VERSION]_headless_java11.jar new
+call java -jar ./3.4.0_headless_java11.jar new
 echo Exit Code = %ERRORLEVEL%
 if "%ERRORLEVEL%" == "1" exit /B 1
