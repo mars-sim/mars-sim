60471dabfbc3e4212e2de7230269974d4026e035
mokun <mannykung@gmail.com>
2023-08-08 09:19:04 -0700

    Correct maven test
    08.08.2023
    
    - fix: correct maven test
    
    Relates to #901

--- mars-sim-mapdata/src/test/java/org/mars/sim/mapdata/MapDataFactoryTest.java
+++ mars-sim-mapdata/src/test/java/org/mars/sim/mapdata/MapDataFactoryTest.java
@@ -9,11 +9,11 @@ public class MapDataFactoryTest extends TestCase {
 
     public void testMEGDRReader() {
         MEGDRMapReader memoryReader = MapDataFactory.createReader(MapDataFactory.MEMORY_READER
-                                    + "," + MEGDRMapReader.DEFAULT_MEGDR_FILE);
+                                    + ", " + MEGDRMapReader.DEFAULT_MEGDR_FILE);
         MEGDRMapReader directReader = MapDataFactory.createReader(MapDataFactory.DIRECT_READER
-                                    + "," + MEGDRMapReader.DEFAULT_MEGDR_FILE);
+                                    + ", " + MEGDRMapReader.DEFAULT_MEGDR_FILE);
         MEGDRMapReader arrayReader = MapDataFactory.createReader(MapDataFactory.ARRAY_READER
-                                    + "," + MEGDRMapReader.DEFAULT_MEGDR_FILE);
+                                    + ", " + MEGDRMapReader.DEFAULT_MEGDR_FILE);
         for(int i = 0; i < 1000; i++) {
             double phi = RandomUtil.getRandomDouble(Math.PI);
             double theta = RandomUtil.getRandomDouble(Math.PI * 2);
