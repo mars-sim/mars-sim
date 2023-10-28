b20b7b1fbc76580e5f903232ca9d5af9877f25bc
bevans2000 <20720166+bevans2000@users.noreply.github.com>
2023-10-28 22:32:33 +0100

    Convert job assignment review to  SettlementMetaTask
    Changes:
    - ReviewJobReassignmentMeta implements SettlementMetaTask and not FactoryMetaTask
    - The SettlementTasks are identified by scannng the citizens for pending job changes; this is only done once
    - The age of the job assignment is used as an overdue modifier to increase the score
    - The highest Role seniority of people changing job is used to defined the base score
    - The number of pending assignments is used as the demand attribute in the SettlementTasks
    - Person suitability is based on the standard assessment plus the callers Role
    
    Part of #1084

--- mars-sim-core/src/main/java/com/mars_sim/core/person/ai/task/meta/ReviewJobReassignmentMeta.java
+++ mars-sim-core/src/main/java/com/mars_sim/core/person/ai/task/meta/ReviewJobReassignmentMeta.java
@@ -6,28 +6,54 @@
  */
 package com.mars_sim.core.person.ai.task.meta;
 
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.mars_sim.core.data.History.HistoryItem;
+import com.mars_sim.core.data.RatingScore;
 import com.mars_sim.core.person.Person;
 import com.mars_sim.core.person.ai.job.util.Assignment;
 import com.mars_sim.core.person.ai.job.util.AssignmentType;
 import com.mars_sim.core.person.ai.role.RoleType;
 import com.mars_sim.core.person.ai.task.ReviewJobReassignment;
-import com.mars_sim.core.person.ai.task.util.FactoryMetaTask;
+import com.mars_sim.core.person.ai.task.util.MetaTask;
+import com.mars_sim.core.person.ai.task.util.SettlementMetaTask;
+import com.mars_sim.core.person.ai.task.util.SettlementTask;
 import com.mars_sim.core.person.ai.task.util.Task;
 import com.mars_sim.core.person.ai.task.util.TaskTrait;
+import com.mars_sim.core.robot.Robot;
+import com.mars_sim.core.structure.Settlement;
 import com.mars_sim.core.structure.building.Building;
 import com.mars_sim.core.structure.building.BuildingManager;
 import com.mars_sim.core.structure.building.function.FunctionType;
-import com.mars_sim.core.vehicle.Vehicle;
 import com.mars_sim.tools.Msg;
 
 /**
  * The Meta task for the ReviewJobReassignment task.
  */
-public class ReviewJobReassignmentMeta extends FactoryMetaTask {
+public class ReviewJobReassignmentMeta extends MetaTask
+	implements SettlementMetaTask
+{
+
+	private static class ReviewJobTaskJob extends SettlementTask {
+		
+		private static final long serialVersionUID = 1L;
+
+        public ReviewJobTaskJob(SettlementMetaTask owner, RatingScore score, int jobAssignments) {
+            super(owner, "Review Job Assignment", null, score);
+			setDemand(jobAssignments);
+        }
+
+        @Override
+        public Task createTask(Person person) {
+            return new ReviewJobReassignment(person);
+        }
+
+        @Override
+        public Task createTask(Robot robot) {
+            throw new UnsupportedOperationException("Robots cannot review job assignments");
+        }
+    }
 
     /** Task name */
     private static final String NAME = Msg.getString(
@@ -38,105 +64,86 @@ public class ReviewJobReassignmentMeta extends FactoryMetaTask {
 		setTrait(TaskTrait.LEADERSHIP);
 
 	}
+
+    /**
+     * Gets the score for a Settlement task for a person. 
+     * Considers the condition of the person and their current role.
+     * 
+	 * @return The factor to adjust task score; 0 means task is not applicable
+     */
     @Override
-    public Task constructInstance(Person person) {
-        return new ReviewJobReassignment(person);
-    }
+	public RatingScore assessPersonSuitability(SettlementTask t, Person p) {
+		RoleType roleType = p.getRole().getType();
+		RatingScore factor = RatingScore.ZERO_RATING;
 
-    @Override
-    public double getProbability(Person person) {
-
-        double result = 0D;
-    
-        if (person.isInSettlement()) {
-
-            // Probability affected by the person's stress and fatigue.
-            if (!person.getPhysicalCondition().isFitByLevel(1000, 70, 1000))
-            	return 0;
-            
-        	//if (roleType == null)
-        	//NOTE: sometimes enum is null. sometimes it is NOT. why?
-        	RoleType roleType = person.getRole().getType();
-
-            if (roleType != null && (roleType.isCouncil()
+        if (p.isInSettlement() && p.getPhysicalCondition().isFitByLevel(1000, 70, 1000)
+			&& (roleType != null)
+			&& (roleType.isCouncil()
         	        || roleType.isChief()     			
-        			|| (roleType == RoleType.MISSION_SPECIALIST && person.getAssociatedSettlement().getNumCitizens() <= 4))) {
-
-	        	    Iterator<Person> i = person.getAssociatedSettlement().getAllAssociatedPeople().iterator();
-	                while (i.hasNext()) {
-	                	// Get the job history of the candidate not the caller
-	                    Person p = i.next();
-	                    List<HistoryItem<Assignment>> list = p.getJobHistory().getJobAssignmentList();
-	                    Assignment ja = list.get(list.size()-1).getWhat();
-	                    
-	                    AssignmentType status = ja.getStatus();
-
-	                    if (status != null && status == AssignmentType.PENDING) {
-
-	                    	result += 100D;
-	                    	
-	                        if (person.isInVehicle()) {	
-	                	        // Check if person is in a moving rover.
-	                	        if (Vehicle.inMovingRover(person)) {
-	                		        // the bonus inside a vehicle
-	                	        	result += 30;
-	                	        } 	       
-	                	        else
-	                		        // the bonus inside a vehicle
-	                	        	result += 10;
-	                        }
-	                        
-		                    RoleType role2 = p.getRole().getType();
-		                    
-		                    // Adjust the probability with penalty if approving his/her own job reassignment 
-		                    if (roleType == RoleType.SUB_COMMANDER
-			                    && role2 != null && role2 == RoleType.SUB_COMMANDER)
-		    		            result /= 2D;
-
-		                    else if (roleType == RoleType.COMMANDER
-				                    && role2 != null && role2 == RoleType.COMMANDER)
-		    		            result /= 2.5D;
-
-		                    else if (roleType == RoleType.MAYOR
-				                    && role2 != null && role2 == RoleType.MAYOR)
-		                    	result /= 3D;
-
-			                else if (roleType == RoleType.PRESIDENT
-					                 && role2 != null && role2 == RoleType.PRESIDENT)
-			    		        result /= 4D;
-		                    
-	                    	//result = result + result * preference / 10D ;
-	                    	
-	                    	// Add adjustment based on how many sol the request has since been submitted
-                            // if the job assignment submitted date is > 1 sol
-                            int sol = getMarsTime().getMissionSol();
-                            int solRequest = list.get(list.size()-1).getWhen().getMissionSol();
-                            if (sol - solRequest == 1)
-                                result += 50D;
-                            else if (sol - solRequest == 2)
-                                result += 100D;
-                            else if (sol - solRequest == 3)
-                                result += 150D;
-                            else if (sol - solRequest > 3)
-                                result += 200D;
-	                    }
-	                }
-	                
-	                if (result > 0D) {
-	                    // Get an available office space.
-	                    Building building = BuildingManager.getAvailableFunctionTypeBuilding(person, FunctionType.ADMINISTRATION);
-						result += 100D;
-						result *= getBuildingModifier(building, person);
-
-	                    result *= getPersonModifier(person);
-	                }
-                    
-                    if (result < 0) {
-                        result = 0;
-                    }
+        			|| (roleType == RoleType.MISSION_SPECIALIST
+							&& p.getAssociatedSettlement().getNumCitizens() <= 4))) {
+			factor = super.assessPersonSuitability(t, p);
+            if (factor.getScore() == 0) {
+                return factor;
             }
+			
+			// Get an available office space.
+	        Building building = BuildingManager.getAvailableFunctionTypeBuilding(p, FunctionType.ADMINISTRATION);
+			factor = assessBuildingSuitability(factor, building, p);
         }
 
-        return result;
+        return factor;
     }
+
+	/**
+	 * Find any JobAssignments that are needed at this Settlement
+	 * @param settlement Being reviewed
+	 * @return
+	 */
+	@Override
+	public List<SettlementTask> getSettlementTasks(Settlement settlement) {
+		double base = 0D;
+		int numOfAssignments = 0;
+		int oldestRequest = 0;
+		int sol = getMarsTime().getMissionSol();
+
+	    // Get the job history of the candidate not the caller
+	    for(Person p : settlement.getAllAssociatedPeople()) {
+	        List<HistoryItem<Assignment>> list = p.getJobHistory().getJobAssignmentList();
+	        Assignment ja = list.get(list.size()-1).getWhat();
+	                    
+	        AssignmentType status = ja.getStatus();
+	        if (status == AssignmentType.PENDING) {
+				numOfAssignments++;
+	            
+				// Take the base score based on the higher seniority
+				RoleType newRole = p.getRole().getType();
+				double newBase = switch(newRole) {
+					case PRESIDENT -> 600;
+					case MAYOR -> 550;
+					case COMMANDER -> 500;
+					case SUB_COMMANDER -> 450;
+					default -> 400;
+				};
+				base = Math.max(base, newBase);
+	
+	                    	
+				// Add adjustment based on how many sol the request has since been submitted
+				// if the job assignment submitted date is > 1 sol
+				int solRequest = sol - list.get(list.size()-1).getWhen().getMissionSol();
+				oldestRequest = Math.max(solRequest, oldestRequest);
+			}
+		}
+
+		List<SettlementTask> result = new ArrayList<>();
+		if (numOfAssignments > 0) {
+			RatingScore score = new RatingScore(base);
+
+			// Addmodifier based on the oldest request; 0.3 per day to a maximum of 3
+			score.addModifier("due", 1D + (0.33D * Math.min(3, oldestRequest)));
+
+			result.add(new ReviewJobTaskJob(this, score, numOfAssignments));
+		}
+		return result;
+	}
 }
