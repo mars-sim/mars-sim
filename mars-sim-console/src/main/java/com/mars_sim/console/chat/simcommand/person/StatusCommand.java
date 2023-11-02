fa2fa6b96fcfc626017419f55558b2ac11a82857
mokun <mannykung@gmail.com>
2023-11-01 19:47:19 -0700

    Refine death reporting
    11.01.2023
    
    - change: revise StatusCommand to always show the general heath status
              regardless the person has died or not
    - change: switch to reporting in 3rd person perspective if a person
              has died
    
    Relates to
    https://github.com/mars-sim/mars-sim/discussions/324#discussioncomment-7450726

--- mars-sim-console/src/main/java/com/mars_sim/console/chat/simcommand/person/StatusCommand.java
+++ mars-sim-console/src/main/java/com/mars_sim/console/chat/simcommand/person/StatusCommand.java
@@ -8,6 +8,7 @@ package com.mars_sim.console.chat.simcommand.person;
 
 import com.mars_sim.console.chat.ChatCommand;
 import com.mars_sim.console.chat.Conversation;
+import com.mars_sim.core.person.GenderType;
 import com.mars_sim.core.person.Person;
 import com.mars_sim.core.person.PhysicalCondition;
 import com.mars_sim.core.person.health.DeathInfo;
@@ -18,6 +19,8 @@ import com.mars_sim.core.structure.Settlement;
  */
 public class StatusCommand extends AbstractPersonCommand {
 	public static final ChatCommand STATUS = new StatusCommand();
+	
+	static String vowels = "aeiou";
 	
 	private StatusCommand() {
 		super("sts", "status", "Status report");
@@ -33,59 +36,124 @@ public class StatusCommand extends AbstractPersonCommand {
 	public static String getStatus(Person person) {
 		StringBuilder buffer = new StringBuilder();
 		buffer.append(System.lineSeparator());
-		buffer.append("Hi, My name is ");
-		buffer.append(person.getName());
-		buffer.append(" and I am a ");
-		buffer.append(person.getMind().getJob().getName().toLowerCase()).append(".");
-		buffer.append(System.lineSeparator());
 		
-		Settlement home = person.getAssociatedSettlement();
-		if (home != null) {
-			buffer.append("My home base is ");
-			buffer.append(person.getAssociatedSettlement().getName()).append(",");
-			buffer.append(System.lineSeparator());
-			buffer.append("where I am a/an ");
-			buffer.append(person.getRole().getType().getName()).append(".");
-		}
-		buffer.append(System.lineSeparator());
-		
-		PhysicalCondition condition = person.getPhysicalCondition();
 		if (person.isDeclaredDead()) {
+			buffer.append("It's reported that ");
+			buffer.append(person.getName());
+			buffer.append(" had passed away. ");
+			
+			String genderNoun0 = "He was ";	
+			String genderNoun1 = "His ";
+			String genderNoun2 = "He ";	
+			
+			GenderType gender = person.getGender();
+			if (gender == GenderType.FEMALE) {
+				genderNoun0 = "She was";
+				genderNoun1 = "Her ";
+				genderNoun2 = "She ";	
+			}
+			
+			buffer.append(genderNoun0.toLowerCase());
+			
+			String job = person.getMind().getJob().getName().toLowerCase();
+			if (vowels.indexOf(Character.toLowerCase(job.charAt(0))) != -1) {	
+				buffer.append(" an ");
+			}
+			else {
+				buffer.append(" a ");
+			}
+			buffer.append(job).append(".");
+			buffer.append(System.lineSeparator());
+			
+			Settlement home = person.getAssociatedSettlement();
+			if (home != null) {
+				buffer.append(genderNoun1);
+				buffer.append(" home base was ");
+				buffer.append(person.getAssociatedSettlement().getName()).append(", ");
+				buffer.append(System.lineSeparator());
+				buffer.append("where ");
+				buffer.append(genderNoun0.toLowerCase());
+				String role = person.getRole().getType().getName().toLowerCase();
+				if (vowels.indexOf(Character.toLowerCase(role.charAt(0))) != -1) {	
+					buffer.append(" an ");
+				}
+				else {
+					buffer.append(" a ");
+				}
+				buffer.append(role).append(". ");
+			}
+			buffer.append(System.lineSeparator());
+			
+			PhysicalCondition condition = person.getPhysicalCondition();
 			DeathInfo death = condition.getDeathDetails();
-			buffer.append("I died on ");
+			buffer.append(genderNoun2);
+			buffer.append(" died on ");
 			buffer.append(death.getTimeOfDeath());
-			buffer.append(" doing ");
+			buffer.append(" while ");
 			buffer.append(death.getTask());
-			buffer.append(". Cause : ");
+			buffer.append(System.lineSeparator());
+			buffer.append(". Cause of Death: ");
 			buffer.append(death.getCause());
 		}
+		
 		else {
+			buffer.append("Hi, my name is ");
+			buffer.append(person.getName());
+			buffer.append(" and I am ");
+			String job = person.getMind().getJob().getName().toLowerCase();
+			if (vowels.indexOf(Character.toLowerCase(job.charAt(0))) != -1) {	
+				buffer.append("a ");
+			}
+			else {
+				buffer.append("an ");
+			}
+			buffer.append(job).append(".");
+			buffer.append(System.lineSeparator());
+			
+			Settlement home = person.getAssociatedSettlement();
+			if (home != null) {
+				buffer.append("My home base is ");
+				buffer.append(person.getAssociatedSettlement().getName()).append(",");
+				buffer.append(System.lineSeparator());
+				buffer.append("where I am ");
+				String role = person.getRole().getType().getName().toLowerCase();
+				if (vowels.indexOf(Character.toLowerCase(role.charAt(0))) != -1) {	
+					buffer.append("a ");
+				}
+				else {
+					buffer.append("an ");
+				}
+				buffer.append(role).append(".");
+			}
+			buffer.append(System.lineSeparator());		
+			
 			buffer.append("At the moment, I am ");
 			buffer.append(person.getTaskDescription().toLowerCase()).append(".");
-
-			double p = condition.getPerformanceFactor();
-			double h = condition.getHunger();
-			double e = condition.getHunger();
-			double t = condition.getThirst();
-			double s = condition.getStress();
-			double f = condition.getFatigue();
-
-			buffer.append(System.lineSeparator());
-			buffer.append(System.lineSeparator());
-			buffer.append("Hunger: ").append(PhysicalCondition.getHungerStatus(h, e));
-			buffer.append(System.lineSeparator());
-			buffer.append("Thirst: ").append(PhysicalCondition.getThirstyStatus(t));
-			buffer.append(System.lineSeparator());
-			buffer.append("Stress: ").append(PhysicalCondition.getStressStatus(s));
-			buffer.append(System.lineSeparator());
-			buffer.append("Fatigue: ").append(PhysicalCondition.getFatigueStatus(f));
-			buffer.append(System.lineSeparator());
-			buffer.append(System.lineSeparator());
-			buffer.append("I am ");
-			buffer.append(PhysicalCondition.getPerformanceStatus(p).toLowerCase());
-			buffer.append(" in performance.");
-			buffer.append(System.lineSeparator());
 		}
+
+		PhysicalCondition condition = person.getPhysicalCondition();
+
+		double p = condition.getPerformanceFactor();
+		double h = condition.getHunger();
+		double e = condition.getEnergy();
+		double t = condition.getThirst();
+		double s = condition.getStress();
+		double f = condition.getFatigue();
+
+		buffer.append(System.lineSeparator());
+		buffer.append(System.lineSeparator());
+		buffer.append("Hunger: ").append(PhysicalCondition.getHungerStatus(h, e));
+		buffer.append(System.lineSeparator());
+		buffer.append("Thirst: ").append(PhysicalCondition.getThirstyStatus(t));
+		buffer.append(System.lineSeparator());
+		buffer.append("Stress: ").append(PhysicalCondition.getStressStatus(s));
+		buffer.append(System.lineSeparator());
+		buffer.append("Fatigue: ").append(PhysicalCondition.getFatigueStatus(f));
+		buffer.append(System.lineSeparator());
+		buffer.append("Performance: ")
+			.append(PhysicalCondition.getPerformanceStatus(p));
+		buffer.append(System.lineSeparator());
+
 		return buffer.toString();
 	}
 
