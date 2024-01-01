eeb940ed33bbdd0b6d369db2c55290d3f0af9724
bevans2000 <20720166+bevans2000@users.noreply.github.com>
2024-01-01 20:04:23 +0000

    Correct missed file from previous commit

--- mars-sim-core/src/main/java/com/mars_sim/core/time/MasterClock.java
+++ mars-sim-core/src/main/java/com/mars_sim/core/time/MasterClock.java
@@ -20,7 +20,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
-import java.util.logging.Level;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.mars_sim.core.Simulation;
@@ -760,7 +759,7 @@ public class MasterClock implements Serializable {
 					listener.clockPulse(activePulse);
 				}
 				catch (Exception e) {
-					logger.log(Level.SEVERE, "Can't send out clock pulse: ", e);
+					logger.severe( "Can't send out clock pulse: ", e);
 				}
 			}
 			return "done";
@@ -847,8 +846,8 @@ public class MasterClock implements Serializable {
 		// Execute all listener concurrently and wait for all to complete before advancing
 		// Ensure that Settlements stay synch'ed and some don't get ahead of others as tasks queue
 		// May use parallelStream() after it's proven to be safe
-//		Collections.synchronizedSet(new HashSet<>(clockListenerTasks)).stream().forEach(this::executeClockListenerTask);
-		clockListenerTasks.stream().forEach(this::executeClockListenerTask);
+		Collections.synchronizedSet(new HashSet<>(clockListenerTasks)).stream().forEach(this::executeClockListenerTask);
+//		clockListenerTasks.stream().forEach(this::executeClockListenerTask);
 	}
 
 	/**
@@ -863,16 +862,16 @@ public class MasterClock implements Serializable {
 			// Wait for it to complete so the listeners doesn't get queued up if the MasterClock races ahead
 			result.get();
 		} catch (ExecutionException ee) {
-			logger.log(Level.SEVERE, "ExecutionException. Problem with clock listener tasks: ", ee);
+			logger.severe( "ExecutionException. Problem with clock listener tasks: ", ee);
 		} catch (RejectedExecutionException ree) {
 			// Application shutting down
 			Thread.currentThread().interrupt();
 			// Executor is shutdown and cannot complete queued tasks
-			logger.log(Level.SEVERE, "RejectedExecutionException. Problem with clock listener tasks: ", ree);
+			logger.severe( "RejectedExecutionException. Problem with clock listener tasks: ", ree);
 		} catch (InterruptedException ie) {
 			// Program closing down
 			Thread.currentThread().interrupt();
-			logger.log(Level.SEVERE, "InterruptedException. Problem with clock listener tasks: ", ie);
+			logger.severe( "InterruptedException. Problem with clock listener tasks: ", ie);
 		}
 	}
 
