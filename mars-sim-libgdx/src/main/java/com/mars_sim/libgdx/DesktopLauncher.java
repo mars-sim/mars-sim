61758b2f29c7fe5166f8e074ffa46a91ad4c2a55
mokun <mannykung@gmail.com>
2024-09-12 12:30:09 -0700

    Fix title
    09.12.2024

--- mars-sim-libgdx/src/main/java/com/mars_sim/libgdx/DesktopLauncher.java
+++ mars-sim-libgdx/src/main/java/com/mars_sim/libgdx/DesktopLauncher.java
@@ -13,7 +13,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.title = SimulationRuntime.TITLE;
+        config.title = SimulationRuntime.SHORT_TITLE;
         config.width = 990;
         config.height = 1000;
         new LwjglApplication(new MarsProjectLibGDX(), config);
