7ad9dca83715e1b3141d32035cf16d2ffd3fd4ce
Milosz Rataj <miloszrataj2@gmail.com>
2024-09-16 18:23:57 +0200

    add mockito dependency to pom.xml

---
+++ mars-sim-core/src/test/java/com/mars_sim/core/person/ai/CacheCreatorTest.java
@@ -0,0 +1,13 @@
+package com.mars_sim.core.person.ai;
+
+import com.mars_sim.core.data.Rating;
+import com.mars_sim.core.time.MarsTime;
+import org.mockito.Mock;
+
+class CacheCreatorTest {
+
+    private CacheCreator<Rating> cacheCreator;
+    private MarsTime marsTime;
+
+    }
+}
