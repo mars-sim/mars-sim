a111c8172a1addbf4c49d303be40f2f01e667bc0
Barry Evans <bevans2000@users.noreply.github.com>
2025-10-05 09:45:15 +0100

    Upgrade OutsidePathFinderTest to JUnit5
    Changes:
    - Use @BeforeEach instead of @Before in teh MarsSimUnitTest
    - OutsidePathFinderTest uses JUnit5 assertions
    
    Ref #1767

--- mars-sim-core/src/test/java/com/mars_sim/core/person/ai/task/walk/OutsidePathFinderTest.java
+++ mars-sim-core/src/test/java/com/mars_sim/core/person/ai/task/walk/OutsidePathFinderTest.java
@@ -7,20 +7,25 @@
 
 package com.mars_sim.core.person.ai.task.walk;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
-import com.mars_sim.core.AbstractMarsSimUnitTest;
+import org.junit.jupiter.api.Test;
+
 import com.mars_sim.core.LocalAreaUtil;
 import com.mars_sim.core.map.location.LocalPosition;
 import com.mars_sim.core.person.Person;
 import com.mars_sim.core.structure.Settlement;
+import com.mars_sim.core.test.MarsSimUnitTest;
 
 /**
  * A unit test suite for the CollectResources task class.
  */
-public class OutsidePathFinderTest extends AbstractMarsSimUnitTest {
+class OutsidePathFinderTest extends MarsSimUnitTest {
 
 	/**
 	 * Factory method to create the path finder.
@@ -38,22 +43,23 @@ public class OutsidePathFinderTest extends AbstractMarsSimUnitTest {
 	/**
 	 * Check the clearPathToDestination method.
 	 */
-	public void testClearPath() {
+	@Test
+	void testClearPath() {
 
 		// Create test settlement.
-		Settlement settlement = buildSettlement();
+		Settlement settlement = buildSettlement("Outside");
 
 		// Create test person.
 		Person person = buildPerson("Outsider", settlement);
-		person.transfer(surface);
+		person.transfer(getMarsSurface());
 		person.setPosition(LocalPosition.DEFAULT_POSITION);
-		assertTrue("Person starts outside", person.isOutside());
+		assertTrue(person.isOutside(), "Person starts outside");
 
 		LocalPosition target = new LocalPosition(-10D, 10D);
 		var outsideWalk = createPathFinder(person, person.getPosition());
 		var solution = outsideWalk.determineWalkingPath(target);
-		assertFalse("No obstacles", solution.obstaclesInPath());
-		assertEquals("Waypoints direct", 2, solution.path().size());
+		assertFalse(solution.obstaclesInPath(), "No obstacles");
+		assertEquals(2, solution.path().size(), "Waypoints direct");
 
 		assertClearPath("Clear path", person, solution.path());
 	}
@@ -62,8 +68,8 @@ public class OutsidePathFinderTest extends AbstractMarsSimUnitTest {
 		LocalPosition from = path.get(0);
 		for(int idx = 1; idx < path.size(); idx++) {
 			var to = path.get(idx);
-			assertTrue(context + " segment " + idx + " clear", 
-						LocalAreaUtil.isLinePathCollisionFree(from, to, person.getCoordinates()));
+			assertTrue(LocalAreaUtil.isLinePathCollisionFree(from, to, person.getCoordinates()),
+						context + " segment " + idx + " clear");
 			from = to;
 		}
 	}
@@ -71,24 +77,25 @@ public class OutsidePathFinderTest extends AbstractMarsSimUnitTest {
 	/**
 	 * Test the determineObstacleAvoidancePath method.
 	 */
-	public void testAvoidBuilding() {
+	@Test
+	void testAvoidBuilding() {
 		// Create test settlement.
-		Settlement settlement = buildSettlement();
+		Settlement settlement = buildSettlement("AvoidBuilding");
 	
 		// Create test person.
 		Person person = buildPerson("Outsider", settlement);
-		person.transfer(surface);
+		person.transfer(getMarsSurface());
 		person.setPosition(LocalPosition.DEFAULT_POSITION);
-		assertTrue("Person starts outside", person.isOutside());
+		assertTrue(person.isOutside(), "Person starts outside");
 	
 		// Add a Building in the way
-		buildBuilding(settlement.getBuildingManager(), new LocalPosition(1D + BUILDING_WIDTH, -BUILDING_LENGTH/2), 0D, 0);
+		buildBuilding(settlement.getBuildingManager(), new LocalPosition(15, -5));
 		
 		LocalPosition target = new LocalPosition(20D, 0D);
 		var outsideWalk = createPathFinder(person, person.getPosition());
 		var solution = outsideWalk.determineWalkingPath(target);
-		assertEquals("Waypoints has a path", 3, solution.path().size());
-		assertFalse("Has no obstacles in calculated path", solution.obstaclesInPath());
+		assertEquals(2, solution.path().size(), "Waypoints has a path");
+		assertFalse(solution.obstaclesInPath(), "Has no obstacles in calculated path");
 
 		assertClearPath("Avoid building", person, solution.path());
 
@@ -97,23 +104,24 @@ public class OutsidePathFinderTest extends AbstractMarsSimUnitTest {
 	/**
 	 * Test the determineObstacleAvoidancePath method.
 	 */
-	public void testAvoidVehicle() {
+	@Test
+	void testAvoidVehicle() {
 		// Create test settlement.
-		Settlement settlement = buildSettlement();
+		Settlement settlement = buildSettlement("AvoidVehicle");
 	
 		// Create test person.
 		Person person = buildPerson("Outsider", settlement);
-		person.transfer(surface);
+		person.transfer(getMarsSurface());
 		person.setPosition(LocalPosition.DEFAULT_POSITION);
-		assertTrue("Person starts outside", person.isOutside());
-	
+		assertTrue(person.isOutside(), "Person starts outside");
+
 		// Add a Building in the way
-		buildRover(settlement, "Rover", new LocalPosition(10D, -1D));
+		buildRover(settlement, "Rover", new LocalPosition(10D, -1D), EXPLORER_ROVER);
 		
 		LocalPosition target = new LocalPosition(20D, 0D);
 		var outsideWalk = createPathFinder(person, person.getPosition());
 		var solution = outsideWalk.determineWalkingPath(target);
-		assertFalse("Has no obstacles in calculated path", solution.obstaclesInPath());
+		assertFalse(solution.obstaclesInPath(), "Has no obstacles in calculated path");
 		assertNotEquals(2, solution.path().size(), "Waypoints has a path");
 		
 		assertClearPath("Avoid Vehicle", person, solution.path());
@@ -122,25 +130,26 @@ public class OutsidePathFinderTest extends AbstractMarsSimUnitTest {
 	/**
 	 * Test the determineObstacleAvoidancePath method.
 	 */
-	public void testAvoidBuildingVehicle() {
+	@Test
+	 void testAvoidBuildingVehicle() {
 		// Create test settlement.
-		Settlement settlement = buildSettlement();
+		Settlement settlement = buildSettlement("BuildingVehicle");
 	
 		// Create test person.
 		Person person = buildPerson("Outsider", settlement);
-		person.transfer(surface);
-		person.setPosition(LocalPosition.DEFAULT_POSITION);
-		assertTrue("Person starts outside", person.isOutside());
-	
+		person.transfer(getMarsSurface());
+		person.setPosition(new LocalPosition(-10, -10));
+		assertTrue(person.isOutside(), "Person starts outside");
+
 		// Add a Building in the way
-		buildBuilding(settlement.getBuildingManager(), new LocalPosition(10D + BUILDING_WIDTH, -BUILDING_LENGTH/2), 0D, 0);
-		buildRover(settlement, "Rover", new LocalPosition(10D, -1D));
-		
-		LocalPosition target = new LocalPosition(30D, 0D);
+		buildBuilding(settlement.getBuildingManager(), new LocalPosition(20D, -5));
+		buildRover(settlement, "Rover", new LocalPosition(10D, -1D), EXPLORER_ROVER);
+
+		LocalPosition target = new LocalPosition(30D, 20D);
 		var outsideWalk = createPathFinder(person, person.getPosition());
 		var solution = outsideWalk.determineWalkingPath(target);
-		assertFalse("Has no obstacles in calculated path", solution.obstaclesInPath());
-		assertNotEquals(2, solution.path().size(), "Waypoints has a path");
+		assertFalse(solution.obstaclesInPath(), "Has no obstacles in calculated path");
+		assertEquals(2, solution.path().size(), "Waypoints has a path");
 
 		assertClearPath("Avoid building & Vehicle", person, solution.path());
 	}
