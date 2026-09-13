a76877486b5cf9befbb954b3c73cde70972fabdc
Barry Evans <20720166+bevans2000@users.noreply.github.com>
2026-09-13 21:56:22 +0000

    task: Correct life resoruce filtering on Goodstable (#2099)
    Changes:
    - CategoryTableModel now supports changing of the displayed categories
    - During category change; updates are ignores because table is changing
    - TradeTableModel only has a single on/off option to display LifeResource
    - Applying the filter will change the displayed categories
    
    ref #2097
    
    Co-authored-by: Barry Evans <bevans2000@users.noreply.github.com>

--- mars-sim-ui/src/main/java/com/mars_sim/ui/swing/tool/monitor/TradeTableModel.java
+++ mars-sim-ui/src/main/java/com/mars_sim/ui/swing/tool/monitor/TradeTableModel.java
@@ -7,7 +7,6 @@
 package com.mars_sim.ui.swing.tool.monitor;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -86,16 +85,14 @@ class TradeTableModel extends CategoryTableModel<Good> implements FilteredTableM
 		COLUMNS[REPAIR_COL] = new ColumnSpec ("Repair", Double.class, ColumnSpec.STYLE_DIGIT2);
 
 	}
-	private static final String ALL = "All";
-	private static final String LIFE_RESOURCES = "Life Resources";
+	private static final String LIFE_RESOURCES = "Only Life Resources";
 	
-	private boolean allCB = true;
 	private boolean lifeResourcesCB = false;
 
 	/** The market manager instance. */
 	private static MarketManager marketManager = Simulation.instance().getMarketManager();
 	
-	private static Set<Good> lifeResources = new HashSet<>();
+	private List<Good> lifeResources = new ArrayList<>();
 
 	/**
 	 * Constructor 1.
@@ -113,48 +110,6 @@ class TradeTableModel extends CategoryTableModel<Good> implements FilteredTableM
 			lifeResources.add(g);
 		}
 	}
-	
-	/**
-	 * Finds the relevant Person by apply filters.
-	 */
-	protected void reapplyFilter() {
-//		Collection<Good> entities = settlements.stream()
-//					.map(Settlement::getAllAssociatedPeople)
-//					.flatMap(Collection::stream)
-//					.filter(this::isGoodDisplayable)
-//					.toList();
-//		
-//		setEntities(entities);
-	}
-
-	/**
-	 * Applies the settlement filter to the model. 
-	 * 
-	 * @param selectedSettlement Settlements to filter by.
-	 * @return true if the filter was applied.
-	 */
-	@Override
-	public boolean applySettlementFilter(Set<Settlement> selectedSettlement) {
-        return super.applySettlementFilter(selectedSettlement);
-	}
-	
-	/**
-	 * Is the good displayable based on filters ?
-	 * 
-	 * @param p
-	 * @return
-	 */
-	private boolean isGoodDisplayable(Good g) {
-		if (!allCB && !lifeResourcesCB) {
-			return false;
-		}
-
-		if (lifeResources.contains(g)) {
-			return lifeResourcesCB;
-		}
-
-		return allCB;
-	}
 
 	/**
 	 * Gets a list of the supported filters and their active state based on the alive state of persons.
@@ -164,7 +119,6 @@ class TradeTableModel extends CategoryTableModel<Good> implements FilteredTableM
 	@Override
 	public List<FilteredTableModel.Filter> getActiveFilters() {
 		var filters = new ArrayList<FilteredTableModel.Filter>();
-		filters.add(new Filter(ALL, ALL, allCB));
 		filters.add(new Filter(LIFE_RESOURCES, LIFE_RESOURCES, lifeResourcesCB));
 
 		return filters;
@@ -178,16 +132,12 @@ class TradeTableModel extends CategoryTableModel<Good> implements FilteredTableM
 	 */
 	@Override
 	public void setFilter(String name, boolean isDisplayed) {
-		switch (name) {
-			case ALL -> allCB = isDisplayed;
-			case LIFE_RESOURCES -> lifeResourcesCB = isDisplayed;
-			default -> {
-				// Do nothing
-			}
-		}
+		if (LIFE_RESOURCES.equals(name)) {
+			lifeResourcesCB = isDisplayed;
 
-		// Reload
-		reapplyFilter();
+			// Change categories based on the filter
+			changeCategories(lifeResourcesCB ? lifeResources : GoodsUtil.getGoodsList());
+		}
 	}
 	
 	/**
